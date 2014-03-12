a1f3ec4496a8e2b473b9f9ea0162dc122d8502ef
Yablokov Aleksey <a.yablokov@crystals.ru>
2014-03-12 16:03:55 +0400

    Добавил вызов javac

--- Building/Ant/junit/run/src/pack1/BuildMe.java
+++ Building/Ant/junit/run/src/pack1/BuildMe.java
@@ -1,6 +1,10 @@
 package pack1;
 
 public class BuildMe {
+    public static void main(String[] args) {
+        System.out.println("do it");
+    }
+
     public String getEmptyString() {
         return "";
     }
