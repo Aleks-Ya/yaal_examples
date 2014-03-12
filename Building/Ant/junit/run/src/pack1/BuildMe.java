33647722ee6f3ba69d10c15fefc91ca0a63ff559
Yablokov Aleksey <a.yablokov@crystals.ru>
2014-03-12 16:27:23 +0400

    Добавил зависимость проекта ant от либы

--- Building/Ant/junit/run/src/pack1/BuildMe.java
+++ Building/Ant/junit/run/src/pack1/BuildMe.java
@@ -1,8 +1,10 @@
 package pack1;
 
+import org.apache.commons.lang3.RandomUtils;
+
 public class BuildMe {
     public static void main(String[] args) {
-        System.out.println("do it");
+        System.out.println(RandomUtils.nextInt(0, 1000));
     }
 
     public String getEmptyString() {
