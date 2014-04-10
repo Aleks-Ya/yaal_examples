bd688ddd37f8cb2ec8e51b25396dce78d83c71a5
Aleks Yablokov <ya_al@bk.ru>
2014-04-10 07:45:38 +0400

    MVC: NumberFormatter

---
+++ Pattern/mvc/NumberFormatter/src/ru/yaal/examples/pattern/mvc/numberformatter/facade/Facade.java
@@ -0,0 +1,25 @@
+package ru.yaal.examples.pattern.mvc.numberformatter.facade;
+
+import ru.yaal.examples.pattern.mvc.numberformatter.controller.ControllerImpl;
+import ru.yaal.examples.pattern.mvc.numberformatter.controller.IController;
+import ru.yaal.examples.pattern.mvc.numberformatter.model.IModel;
+import ru.yaal.examples.pattern.mvc.numberformatter.model.ModelImpl;
+import ru.yaal.examples.pattern.mvc.numberformatter.view.DecimalView;
+import ru.yaal.examples.pattern.mvc.numberformatter.view.IView;
+import ru.yaal.examples.pattern.mvc.numberformatter.view.IntegerView;
+
+/**
+ * Фасад: инициализирует систему MVC.
+ */
+public class Facade {
+    public static void main(String[] args) {
+        IView integerView = new IntegerView();
+        IView decimalView = new DecimalView();
+
+        IModel model = new ModelImpl();
+        model.addChangedListener(integerView);
+        model.addChangedListener(decimalView);
+
+        IController controller = new ControllerImpl(model);
+    }
+}
---
+++ Pattern/mvc/NumberFormatter/src/ru/yaal/examples/pattern/mvc/numberformatter/model/IModel.java
@@ -0,0 +1,14 @@
+package ru.yaal.examples.pattern.mvc.numberformatter.model;
+
+/**
+ * Интерфейс моделей.
+ */
+public interface IModel {
+    double getNumber();
+
+    void setNumber(double number);
+
+    void changed();
+
+    void addChangedListener(IChangeListener listener);
+}
