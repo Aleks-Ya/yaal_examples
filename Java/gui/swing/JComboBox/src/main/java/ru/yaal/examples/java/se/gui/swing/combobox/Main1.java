390ef1694c41ecd2673aca24bcd64aef323042dd
Yablokov Aleksey <a.yablokov@crystals.ru>
2014-04-17 13:11:04 +0400

    [JComboBox] Main1 работа через модель выпадающего списка готова

---
+++ Java/gui/swing/JComboBox/src/main/java/ru/yaal/examples/java/se/gui/swing/combobox/Main1.java
@@ -0,0 +1,45 @@
+package ru.yaal.examples.java.se.gui.swing.combobox;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Добавление элементов в выпадающий список через DefaultComboBoxModel.
+ */
+public class Main1 {
+    public static void main(String[] args) {
+        JComboBox<String> comboBox = new JComboBox<String>();
+        comboBox.setEditable(true);
+
+        DefaultComboBoxModel<String> comboBoxModel = (DefaultComboBoxModel<String>) comboBox.getModel();
+
+        JButton okButton = new JButton("OK");
+        okButton.addActionListener(new OkButtonActionListener(comboBoxModel));
+
+        JFrame frame = new JFrame();
+        frame.setLayout(new BorderLayout());
+        frame.setSize(300, 50);
+        frame.setVisible(true);
+        frame.add(comboBox, BorderLayout.CENTER);
+        frame.add(okButton, BorderLayout.EAST);
+    }
+
+    static class OkButtonActionListener implements ActionListener {
+        private DefaultComboBoxModel<String> comboBoxModel;
+
+        OkButtonActionListener(DefaultComboBoxModel<String> comboBoxModel) {
+            this.comboBoxModel = comboBoxModel;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            comboBoxModel.addElement((String) comboBoxModel.getSelectedItem());
+        }
+    }
+}
+
