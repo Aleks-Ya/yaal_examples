c02f470674a1140a26339144a463a17c2b013665
Aleks Yablokov <ya_al@bk.ru>
2014-04-28 08:28:41 +0400

    Некомпилирующиеся изменения в Font

--- Java/JSE/gui/swing/Font/src/Main.java
+++ Java/JSE/gui/swing/Font/src/Main.java
@@ -1,8 +1,25 @@
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.WindowConstants;
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+import java.awt.font.TextAttribute;
+import java.text.AttributedCharacterIterator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+        Font[] availableFonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
+        System.out.printf("Available fonts: %d\n", availableFonts.length);
+
+        Map<? extends AttributedCharacterIterator.Attribute, Object> fontAttrs = new HashMap<>();
+        fontAttrs.put(TextAttribute.FONT, TextAttribute.WEIGHT_HEAVY);
+
+        Font font = new Font(fontAttrs);
+
         JLabel label = new JLabel("Съешь этих французских булок да выпей чаю, мудак");
+        label.setFont(font);
 
         JFrame frame = new JFrame();
         frame.setSize(600, 100);
