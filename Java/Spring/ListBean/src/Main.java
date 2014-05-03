b5a6875fd9263d892a83c140a3abba091507f262
Aleks Yablokov <ya_al@bk.ru>
2014-05-03 12:59:48 +0400

    Добавил инициализацию set

--- Java/Spring/ListBean/src/Main.java
+++ Java/Spring/ListBean/src/Main.java
@@ -2,12 +2,19 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+
         List<MyClass> myList = (List<MyClass>) context.getBean("myList");
         for (MyClass my : myList) {
+            System.out.println(my.getValue());
+        }
+
+        Set<MyClass> mySet = (Set<MyClass>) context.getBean("mySet");
+        for (MyClass my : mySet) {
             System.out.println(my.getValue());
         }
     }
