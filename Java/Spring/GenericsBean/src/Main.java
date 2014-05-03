56f4805cddc6b3926ca4679ae8d91e4fbd64ecdc
Aleks Yablokov <ya_al@bk.ru>
2014-05-03 12:54:22 +0400

    GenericsBean Красивое решение

--- Java/Spring/GenericsBean/src/Main.java
+++ Java/Spring/GenericsBean/src/Main.java
@@ -1,14 +1,14 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
-
-        MyClass my1 = (MyClass) context.getBean("my1");
-        MyClass my2 = (MyClass) context.getBean("my2");
-
-        System.out.println(my1.getValue());
-        System.out.println(my2.getValue());
+        List<MyClass> myList = (List<MyClass>) context.getBean("myList");
+        for (MyClass my : myList) {
+            System.out.println(my.getValue());
+        }
     }
 }
