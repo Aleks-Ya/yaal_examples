795f19a57ed48c1aa4fab79ae5ddc0ae3cf53b83
Aleks Yablokov <ya_al@bk.ru>
2014-05-03 12:35:14 +0400

    GenericsBean easy beans creation

---
+++ Java/Spring/GenericsBean/src/MyClass.java
@@ -0,0 +1,14 @@
+/**
+ * Класс бина для инициализации.
+ */
+public class MyClass {
+    private String value;
+
+    public MyClass(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
