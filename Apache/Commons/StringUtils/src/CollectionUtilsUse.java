d2b960a932a58232925140d2d4f197309f82f72a
Aleks Yablokov <ya_al@bk.ru>
2014-05-04 03:05:06 +0400

    Добавил gradle в Apachecommons

--- Apache/Commons/StringUtils/src/CollectionUtilsUse.java
+++ Apache/Commons/StringUtils/src/CollectionUtilsUse.java
@@ -1,6 +1,5 @@
-package ru.yaal.examples.java.apache.commons.lang3.stringutils;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 
 import java.util.ArrayList;
 
--- Apache/Commons/StringUtils/test/StringUtilsUseTest.java
+++ Apache/Commons/StringUtils/test/StringUtilsUseTest.java
@@ -1,16 +1,9 @@
-package ru.yaal.examples.java.apache.commons.lang3.stringutils;
-
 import org.apache.commons.lang3.StringUtils;
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-/**
- * Created with IntelliJ IDEA.
- * User: a.yablokov
- * Date: 14.11.13
- */
 public class StringUtilsUseTest {
     @Test
     public void testMain() throws Exception {
