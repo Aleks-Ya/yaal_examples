786786e3c70b48c585449c9e6f106ea6b79e3a4a
Aleks Yablokov <ya_al@bk.ru>
2014-05-04 17:25:23 +0400

    add ArrayUtils

---
+++ Apache/Commons/ApacheCommonsUtils/src/ArrayUtilsUse.java
@@ -0,0 +1,9 @@
+import org.apache.commons.lang3.ArrayUtils;
+
+public class ArrayUtilsUse {
+    public static void main(String[] args) {
+        int[] arr = {100, 200};
+        boolean isEmpty = ArrayUtils.isEmpty(arr);
+        System.out.println(isEmpty);
+    }
+}
