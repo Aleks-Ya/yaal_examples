7a18b83cbd694cf4da0b5ed656d04cffab0f5c08
Yablokov Aleksey <a.yablokov@crystals.ru>
2014-05-05 21:41:52 +0400

    DateBean Упростил инициализацию бина

--- Java/Spring/XmlCofig/DateBean/src/Main.java
+++ Java/Spring/XmlCofig/DateBean/src/Main.java
@@ -2,14 +2,15 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
-        Date date = (Date) context.getBean("date");
+
+        Date date = context.getBean("date", Date.class);
         System.out.println(date);
+
+        Date dateEasy = context.getBean("dateEasy", Date.class);
+        System.out.println(dateEasy);
     }
 }
