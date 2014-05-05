d106e3a1a5b5c8465df2cf54818f77659f6b185c
Yablokov Aleksey <a.yablokov@crystals.ru>
2014-05-05 19:00:42 +0400

    I18n Привел пример в рабочее состояние

---
+++ Java/JSE/I18n/EasyI18nApplication/src/ru/yaal/example/java/se/i18n/EasyI18nApplication.java
@@ -0,0 +1,20 @@
+package ru.yaal.example.java.se.i18n;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class EasyI18nApplication {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        Locale english = Locale.ENGLISH;
+        Locale russian = new Locale("ru", "RU");
+        printMessage(russian);
+        printMessage(english);
+    }
+
+    private static void printMessage(Locale locale) throws UnsupportedEncodingException {
+        ResourceBundle myResources = ResourceBundle.getBundle("ru.yaal.example.java.se.i18n.Messages", locale);
+        String message = new String(myResources.getString("hello").getBytes("ISO-8859-1"), "UTF-8");
+        System.out.printf("%s: %s\n", locale.toLanguageTag(), message);
+    }
+}
