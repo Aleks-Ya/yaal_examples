9869168e234cae2ea1733237d6d8bb4afdaeb6ee
Yablokov Aleksey <a.yablokov@crystals.ru>
2014-05-08 15:05:11 +0400

    Внедрение бина через @Autowired и @Resource

--- Java/Spring/AnnotationConfig/ServiceAnnotation/src/scan/City.java
+++ Java/Spring/AnnotationConfig/ServiceAnnotation/src/scan/City.java
@@ -1,7 +1,10 @@
 package scan;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service("city")
 public class City {
@@ -12,8 +15,14 @@ public class City {
     @Value("1000000")
     private int population;
 
+    @Autowired
+    private Mayor mayor;
+
+    @Resource
+    private Mayor mayor2;
+
     @Override
     public String toString() {
-        return String.format("scan.City[name='%s' population='%d']", name, population);
+        return String.format("scan.City[name='%s' population='%d' mayor='%s' mayor2='%s']", name, population, mayor, mayor2);
     }
 }
