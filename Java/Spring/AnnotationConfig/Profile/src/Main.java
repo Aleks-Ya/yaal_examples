23740d0770d5b4d3e9e8797dd53abd2d2495edac
Aleks Yablokov <ya_al@bk.ru>
2014-05-09 17:57:51 +0400

    Profile

--- Java/Spring/AnnotationConfig/Profile/src/Main.java
+++ Java/Spring/AnnotationConfig/Profile/src/Main.java
@@ -1,6 +1,12 @@
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import provider.IMessageProvider;
 
+/**
+ * Способы выбрать активные профили:
+ * 1. context.getEnvironment().addActiveProfile("rough");
+ * 2. аргумент JVM: -Dspring.profiles.active=rough
+ * 3. переменные окружения: spring.profiles.active=rough
+ */
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
