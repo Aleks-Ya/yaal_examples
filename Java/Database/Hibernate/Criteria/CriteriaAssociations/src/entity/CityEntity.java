c69f97abc1ddb7c34706109dc949a64a0e3ef4fc
Yablokov Aleksey <a.yablokov@crystals.ru>
2014-05-23 13:49:57 +0400

    CriteriaAssociation

---
+++ Java/Database/Hibernate/Criteria/CriteriaAssociations/src/entity/CityEntity.java
@@ -0,0 +1,34 @@
+package entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class CityEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private Long population;
+
+    @ManyToOne
+    private RegionEntity region;
+
+    public CityEntity() {
+    }
+
+    public CityEntity(String name, Long population, RegionEntity region) {
+        this.name = name;
+        this.population = population;
+        this.region = region;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Город[id=%d, name=%s, population=%dтыс region=%s]", id, name, population/1000, region);
+    }
+}
