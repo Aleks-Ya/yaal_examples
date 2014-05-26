bcd00cd2894fe4db2e7259ef9931c620bbac6a69
Yablokov Aleksey <a.yablokov@crystals.ru>
2014-05-26 10:41:47 +0400

    CriteriaQuerySelect

--- Java/Database/JPA/EntityManager/CriteriaQuery/CriteriaQuerySelect/src/Main.java
+++ Java/Database/JPA/EntityManager/CriteriaQuery/CriteriaQuerySelect/src/Main.java
@@ -7,13 +7,49 @@ import org.hibernate.service.ServiceRegistry;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import javax.persistence.spi.PersistenceUnitTransactionType;
 import java.util.List;
 
 public class Main {
+
+    private static EntityManager em;
+    private static EntityManagerFactory emFactory;
+
     public static void main(String[] args) {
+        initEntityManager();
+        saveEntities();
+        {
+            System.out.println("\nSelect from one table (result list):");
+            CriteriaBuilder builder = emFactory.getCriteriaBuilder();//1ый способ получения CriteriaBuilder
+            CriteriaQuery<CityEntity> cq = builder.createQuery(CityEntity.class);
+            Root<CityEntity> cityRoot = cq.from(CityEntity.class);
+            cq.select(cityRoot);
+            TypedQuery<CityEntity> tq = em.createQuery(cq);
+            List<CityEntity> result = tq.getResultList();
+            for (CityEntity city : result) {
+                System.out.println(city);
+            }
+        }
+        {
+            System.out.println("\nSelect from one table (single result):");
+            CriteriaBuilder builder = em.getCriteriaBuilder();//2ой способ получения CriteriaBuilder
+            CriteriaQuery<CityEntity> cq = builder.createQuery(CityEntity.class);
+            Root<CityEntity> cityRoot = cq.from(CityEntity.class);
+            cq.select(cityRoot);
+            TypedQuery<CityEntity> tq = em.createQuery(cq);
+            tq.setMaxResults(1);
+            CityEntity result = tq.getSingleResult();
+            System.out.println(result);
+        }
+        em.close();
+        System.exit(0);//Почему приложение не завершается?
+    }
+
+    private static void saveEntities() {
         RegionEntity region = new RegionEntity("Вологодская область");
         final long vologdaPopulation = 300000L;
         final String vologdaName = "Вологда";
@@ -28,7 +64,7 @@ public class Main {
         CityEntity city4 = new CityEntity("Москва", moscowPopulation, region2);
 
         {
-            EntityManager em = getEntityManager();
+            em.getTransaction().begin();
             em.persist(region);
             em.persist(region2);
             em.persist(city1);
@@ -36,32 +72,21 @@ public class Main {
             em.persist(city3);
             em.persist(city4);
             em.flush();
-            em.close();
-        }
-        {
-            EntityManager em = getEntityManager();
-            CriteriaBuilder builder = em.getCriteriaBuilder();
-            CriteriaQuery<CityEntity> query = builder.createQuery(CityEntity.class);
+            em.getTransaction().commit();
         }
     }
 
-    private static void printList(List result) {
-        for (Object obj : result) {
-            System.out.println(obj);
-        }
-    }
-
-    private static EntityManager getEntityManager() {
+    private static void initEntityManager() {
         Configuration configuration = getConfiguration();
 
         StandardServiceRegistryBuilder srBuilder = new StandardServiceRegistryBuilder();
         srBuilder.applySettings(configuration.getProperties());
         ServiceRegistry serviceRegistry = srBuilder.build();
 
-        EntityManagerFactory emFactory = new EntityManagerFactoryImpl(
+        emFactory = new EntityManagerFactoryImpl(
                 PersistenceUnitTransactionType.RESOURCE_LOCAL, true, null, configuration, serviceRegistry, null);
 
-        return emFactory.createEntityManager();
+        em = emFactory.createEntityManager();
     }
 
     private static Configuration getConfiguration() {
