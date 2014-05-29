7aecef29b1b60a290f46766e9679c3f504bdaaee
Yablokov Aleksey <a.yablokov@crystals.ru>
2014-05-29 18:58:49 +0400

    HelloEjbWorld

--- Java/JEE/EJB/HelloEjbWorld/web/src/myservlets/HelloWorldServlet.java
+++ Java/JEE/EJB/HelloEjbWorld/web/src/myservlets/HelloWorldServlet.java
@@ -3,23 +3,29 @@ package myservlets;
 import mybeans.HelloWorldBean;
 
 import javax.ejb.EJB;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import java.io.IOException;
 
-@javax.servlet.annotation.WebServlet(name = "HelloWorldServlet", urlPatterns = "/helloworld")
-public class HelloWorldServlet extends javax.servlet.http.HttpServlet {
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/helloworld")
+public class HelloWorldServlet extends HttpServlet {
+
     @EJB
     private HelloWorldBean helloWorldBean;
+
     protected void doPost(javax.servlet.http.HttpServletRequest request,
                           javax.servlet.http.HttpServletResponse response)
             throws javax.servlet.ServletException, IOException {
 
     }
+
     @Override
     protected void doGet(javax.servlet.http.HttpServletRequest request,
                          javax.servlet.http.HttpServletResponse response)
             throws javax.servlet.ServletException, IOException {
-        String hello=helloWorldBean.sayHello();
-        request.setAttribute("hello",hello);
-        request.getRequestDispatcher("hello.jsp").forward(request,response);
+
+        String hello = helloWorldBean.sayHello();
+        request.setAttribute("hello", hello);
+        request.getRequestDispatcher("hello.jsp").forward(request, response);
     }
 }
--- Java/JEE/EJB/HelloEjbWorld/web/web/index.jsp
+++ Java/JEE/EJB/HelloEjbWorld/web/web/index.jsp
@@ -1,11 +1,11 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
+<head>
     <title>Hello Ebj World</title>
-  </head>
-  <body>
-  <form method="get" action="helloworld">
-      <input type="submit" value="Say Hello"/>
-  </form>
-  </body>
+</head>
+<body>
+<form method="get" action="helloworld">
+    <input type="submit" value="Say Hello"/>
+</form>
+</body>
 </html>
