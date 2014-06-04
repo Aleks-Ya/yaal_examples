2479581e6bfd3811164b3fc94334cc915efd85af
Yablokov Aleksey <a.yablokov@crystals.ru>
2014-06-04 22:23:03 +0400

    JodaTimeFirstUse

--- Java/JodaTime/JodaTimeExamples/src/ParsingString.java
+++ Java/JodaTime/JodaTimeExamples/src/ParsingString.java
@@ -1,12 +1,33 @@
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
 
 public class ParsingString {
     public static void main(String[] args) {
+        useMethodParse();
+        useConstructor();
+        useFormatter();
+    }
+
+    private static void useMethodParse() {
         DateTime yyyyMmDd = DateTime.parse("2013-08-15"); // 2013-08-15T00:00:00.000+04:00
         DateTime yyyyMmDdHhMm = DateTime.parse("2013-08-15T10:32"); // 2013-08-15T10:32:00.000+04:00
         DateTime yyyyMmDdHhMmSs = DateTime.parse("2013-08-15T10:32:16"); // 2013-08-15T10:32:16.000+04:00
+
+        print(yyyyMmDd, yyyyMmDdHhMm, yyyyMmDdHhMmSs);
+    }
+
+    private static void useConstructor() {
         DateTime yyyyMmDdHhMmSs2 = new DateTime("2013-08-15T10:32:16"); // 2013-08-15T10:32:16.000+04:00
-        print(yyyyMmDd, yyyyMmDdHhMm, yyyyMmDdHhMmSs, yyyyMmDdHhMmSs2);
+        print(yyyyMmDdHhMmSs2);
+    }
+
+    private static void useFormatter() {
+        DateTimeFormatter fmt = DateTimeFormat.forPattern("HH:mm");
+        DateTime HhMm = DateTime.parse("10:32", fmt); // 10:32:16.000+04:00
+
+        print(HhMm);
     }
 
     private static void print(DateTime... times) {
@@ -14,5 +35,4 @@ public class ParsingString {
             System.out.println(time);
         }
     }
-
 }
