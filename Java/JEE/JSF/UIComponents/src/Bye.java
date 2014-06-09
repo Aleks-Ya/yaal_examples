42d1b3809376fb7849df13a5f04c9c2c9edf31f4
Aleks Yablokov <ya_al@bk.ru>
2014-06-09 06:13:53 +0400

    UIComponents

---
+++ Java/JEE/JSF/UIComponents/src/Bye.java
@@ -0,0 +1,8 @@
+import javax.enterprise.inject.Model;
+
+@Model
+public class Bye {
+    public String getBye() {
+        return "Good Bye";
+    }
+}
---
+++ Java/JEE/JSF/UIComponents/src/Hello.java
@@ -0,0 +1,20 @@
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class Hello {
+
+    private String name;
+
+    public Hello() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String user_name) {
+        this.name = user_name;
+    }
+}
