5b80b28517e33be4b9994988828c1b1bde8b4e12
Aleksey Yablokov <ya_al@bk.ru>
2014-06-18 15:11:24 +0400

    HelloWorldServlet

---
+++ Java/JEE/Servlet/HelloWorldServlet/src/HelloWorldHandler.java
@@ -0,0 +1,20 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloWorldHandler extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.getWriter().println("<h1>Hello Servlet World!</h1>");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+}
