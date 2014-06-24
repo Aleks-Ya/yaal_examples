456aadd57f1d7f691e330c22edb39595d22d3801
Aleksey Yablokov <ya_al@bk.ru>
2014-06-24 10:28:45 +0400

    CloneFetch

--- Java/Git/JGit/CloneFetch/src/Main.java
+++ Java/Git/JGit/CloneFetch/src/Main.java
@@ -4,7 +4,9 @@ import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.transport.CredentialsProvider;
 import org.eclipse.jgit.transport.FetchResult;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,7 +14,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException, GitAPIException {
         FileRepositoryBuilder builder = new FileRepositoryBuilder();
-        Repository repository = builder.setGitDir(new File("/home/aleks/examples"))
+        Repository repository = builder.setGitDir(new File("/home/alexx/examples/.git"))
                 .readEnvironment()
                 .findGitDir()
                 .build();
@@ -21,7 +23,10 @@ public class Main {
         System.out.println(head);
 
         Git git = new Git(repository);
+
+        CredentialsProvider credentials = new UsernamePasswordCredentialsProvider("", "");
         FetchCommand fetchCommand = git.fetch();
+        fetchCommand.setCredentialsProvider(credentials);
         FetchResult fetchResult = fetchCommand.call();
         System.out.println(fetchResult);
     }
