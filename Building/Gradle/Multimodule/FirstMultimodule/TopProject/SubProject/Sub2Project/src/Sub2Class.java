bde2edfb43c16ea565b35fa28d3a7496ddede540
Aleksey Yablokov <ya_al@bk.ru>
2014-06-26 17:58:32 +0400

    Multimudule

--- Building/Gradle/Multimodule/FirstMultimodule/TopProject/SubProject/Sub2Project/src/Sub2Class.java
+++ Building/Gradle/Multimodule/FirstMultimodule/TopProject/SubProject/Sub2Project/src/Sub2Class.java
@@ -1,7 +1,7 @@
 import org.apache.log4j.Logger;
 
-public class MyClass {
+public class Sub2Class {
     public static void main(String[] args) {
-        Logger LOG = Logger.getLogger(MyClass.class);
+        Logger LOG = Logger.getLogger(Sub2Class.class);
     }
 }
--- Building/Gradle/Multimodule/FirstMultimodule/TopProject/SubProject/src/SubClass.java
+++ Building/Gradle/Multimodule/FirstMultimodule/TopProject/SubProject/src/SubClass.java
@@ -1,1 +1,7 @@
-public class SubClass{}
+import org.apache.log4j.Logger;
+
+public class SubClass{
+    public static void main(String[] args) {
+        Logger LOG = Logger.getLogger(SubClass.class);
+    }
+}
