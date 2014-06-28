f65a53d8658e6d3c1e584aa1f2d1db0c5d7b6752
Aleksey Yablokov <ya_al@bk.ru>
2014-06-28 17:30:54 +0400

    ShowStandardOutput

---
+++ Building/Gradle/UnitTest/ShowStandardOutput/test/MyTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class MyTest {
+    @Test
+    public void test1() {
+            System.out.println("==== MESSAGE TO System.out FROM UNIT TEST ====");
+            System.err.println("==== MESSAGE TO System.err FROM UNIT TEST ====");
+    }
+}
