750fb1e7c21e0696740f453f88391162ab61c7a1
Aleksey Yablokov <ya_al@bk.ru>
2014-06-29 02:00:39 +0400

    JarExecutable

---
+++ Building/Gradle/JarExecutable/src/Main.java
@@ -0,0 +1,7 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        new AnnotationConfigApplicationContext();
+    }
+}
