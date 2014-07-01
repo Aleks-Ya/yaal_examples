9ef5cc2eafc7740a66e5f6fb14eb8135e82f1611
Aleksey Yablokov <ya_al@bk.ru>
2014-07-01 15:27:44 +0400

    H2Hibernate

--- Java^/Database^/H2^/H2Hibernate/src/JavaConfig.java
+++ Java^/Database^/H2^/H2Hibernate/src/JavaConfig.java
@@ -18,32 +18,35 @@ public class JavaConfig {
         SessionFactory sessionFactory = null;
         Session session = null;
         try {
+
             Configuration configuration = getConfiguration();
 
             ServiceRegistryBuilder serviceRegistryBuilder = new ServiceRegistryBuilder().applySettings(configuration.getProperties());
             ServiceRegistry serviceRegistry = serviceRegistryBuilder.buildServiceRegistry();
             sessionFactory = configuration.buildSessionFactory(serviceRegistry);
             session = sessionFactory.openSession();
+
             Cashier mary = new Cashier("Mary", "Noise", 25);
             Shift maryShift = new Shift(mary, new DateTime("2013-10-11").toDate(), new DateTime("2013-10-12").toDate(),
                     new DateTime("2013-10-13").toDate());
             Cashier ann = new Cashier("Ann", "Big", 18);
             Shift annShift = new Shift(ann, new DateTime("2012-10-11").toDate(), new DateTime("2012-10-12").toDate(),
                     new DateTime("2012-10-13").toDate());
+
             Transaction t = session.beginTransaction();
             session.save(mary);
             session.save(ann);
             session.save(maryShift);
             session.save(annShift);
             t.commit();
-//            Transaction t2 = session.beginTransaction();
+
             Date now = new DateTime("2013-10-09").toDate();
             Query query = session.createQuery("FROM Shift WHERE createDate <= :now");
             query.setDate("now", now);
             List results = query.list();
             System.out.printf("Found: %d\n", results.size());
             System.out.println(Arrays.deepToString(results.toArray()));
-//            t2.commit();
+
         } finally {
             if (session != null) {
                 session.close();
@@ -58,6 +61,8 @@ public class JavaConfig {
         Configuration configuration = new Configuration();
         configuration.setProperty("hibernate.connection.driver_class", "org.h2.Driver");
         configuration.setProperty("hibernate.connection.url", "jdbc:h2:~/test");
+        configuration.setProperty("hibernate.connection.username", "sa");
+        configuration.setProperty("hibernate.connection.password", "");
         configuration.setProperty("hibernate.connection.pool_size", "1");
         configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
         configuration.setProperty("hibernate.cache.provider_class", "org.hibernate.cache.internal.NoCachingRegionFactory");
