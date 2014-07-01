5cf1756229aabd50c9d95229cf59923ee94f73ce
Aleksey Yablokov <ya_al@bk.ru>
2014-07-01 17:48:12 +0400

    ConfigurationLog4J

--- Java^/Logging^/Log4J_v2^/ConfigurationLog4J/src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public class HelloWorld {
-    private static Logger logger = LogManager.getLogger("HelloWorld");
-    public static void main(String[] args) {
-        logger.fatal("Hello, World!");
-    }
-}
