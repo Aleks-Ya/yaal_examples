be2e6cf2dac02b0d01b9e629452edec0ef12ad70
Aleksey Yablokov <ya_al@bk.ru>
2014-07-04 12:00:52 +0400

    InputStreamToString

---
+++ Java^/JSE^/io^/InputStreamToString/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        final InputStream io = new ByteArrayInputStream("Привет!\nПока!".getBytes());
+        String str = inputStreamToString(io, Charset.forName("UTF-8"));
+        System.out.printf(">%s<", str);
+    }
+
+    private static String inputStreamToString(InputStream io, Charset charset) throws IOException {
+        InputStreamReader isr = new InputStreamReader(io, charset);
+        StringBuilder result = new StringBuilder();
+        char[] c = new char[1];
+        while (isr.read(c) != -1) {
+            result.append(c);
+        }
+        return result.toString();
+    }
+}
