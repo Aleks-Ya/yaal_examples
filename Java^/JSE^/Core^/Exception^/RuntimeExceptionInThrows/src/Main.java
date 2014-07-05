b05a3cc56da0dc1524beb2cff5fcef437842136f
Aleksey Yablokov <ya_al@bk.ru>
2014-07-05 20:04:01 +0400

    RuntimeExceptionInThrows

---
+++ Java^/JSE^/Core^/Exception^/RuntimeExceptionInThrows/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+      System.out.println(getInt());
+    }
+    
+    static int getInt() throws RuntimeException {
+      return 1;
+    }
+}
