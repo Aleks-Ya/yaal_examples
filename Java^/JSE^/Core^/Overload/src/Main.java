8b0e43ec190a4b38ad03559a532489afb51909df
Aleksey Yablokov <ya_al@bk.ru>
2014-07-10 01:32:57 +0400

    Overload

---
+++ Java^/JSE^/Core^/Overload/src/Main.java
@@ -0,0 +1,19 @@
+import static java.lang.System.out;
+
+public class Main {
+    public static void main(String[] args) {
+        print('a');
+    }
+    
+    private static void print(byte n) {
+      out.println("byte");
+    }
+    
+    private static void print(int n) {
+      out.println("int");
+    }
+    
+    private static void print(long n) {
+      out.println("long");
+    }
+}
