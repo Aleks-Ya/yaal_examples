c33ec5297aa64a9ede9841752231b10d856c469f
Aleksey Yablokov <ya_al@bk.ru>
2014-07-18 17:21:30 +0400

    CollectionUtils

--- Java^/Apache^/Commons^/ApacheCommonsUtils/src/ArrayUtilsUse.java
+++
@@ -1,9 +0,0 @@
-import org.apache.commons.lang3.ArrayUtils;
-
-public class ArrayUtilsUse {
-    public static void main(String[] args) {
-        int[] arr = {100, 200};
-        boolean isEmpty = ArrayUtils.isEmpty(arr);
-        System.out.println(isEmpty);
-    }
-}
--- Java^/Apache^/Commons^/CollectionUtils/src/Main.java
+++ Java^/Apache^/Commons^/CollectionUtils/src/Main.java
@@ -1,10 +1,8 @@
-
 import org.apache.commons.collections4.CollectionUtils;
-
 import java.util.ArrayList;
 
-public class CollectionUtilsUse {
+public class Main {
     public static void main(String[] args) {
-        CollectionUtils.isNotEmpty(new ArrayList(0));
+        System.out.println(CollectionUtils.isNotEmpty(new ArrayList(0)));
     }
 }
