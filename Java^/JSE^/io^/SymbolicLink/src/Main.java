9ca85610e6161acc6fe6d1b2bf6e0d90a48476a2
Aleksey Yablokov <ya_al@bk.ru>
2014-07-28 02:06:01 +0400

    SymbolicLink

---
+++ Java^/JSE^/io^/SymbolicLink/src/Main.java
@@ -0,0 +1,16 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.io.IOException;
+import static java.lang.System.out;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Path target = Files.createTempFile("target_", ".sh");
+        Path link = Paths.get(target.getParent().toString(), "link_to_target");
+        Files.deleteIfExists(link);
+        Files.createSymbolicLink(link, target);
+        out.printf("link   = %s%n", link);
+        out.printf("target = %s%n", target);
+    }
+}
