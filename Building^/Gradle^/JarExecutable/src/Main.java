82251e0a8d63c365b8b58675b8fc9035c7c912ca
Aleksey Yablokov <ya_al@bk.ru>
2014-07-27 14:41:57 +0400

    JarProperties

--- Building^/Gradle^/JarExecutable/src/Main.java
+++
@@ -1,7 +0,0 @@
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-public class Main {
-    public static void main(String[] args) {
-        new AnnotationConfigApplicationContext();
-    }
-}
