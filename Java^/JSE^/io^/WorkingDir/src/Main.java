5ba32b1951f739b0778191f3ea83c0bac15a772a
Aleksey Yablokov <ya_al@bk.ru>
2014-07-28 13:57:06 +0400

    WorkingDir

---
+++ Java^/JSE^/io^/WorkingDir/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        //Linux:
+        System.out.printf("System.getenv(\"PWD\") -> %s%n", System.getenv("PWD"));
+        
+        //TODO Работает ли в Windows??
+    }
+}
