1611f31bae9639bad2a4bbf90c3f565698b2e702
Aleksey Yablokov <ya_al@bk.ru>
2014-07-29 16:35:44 +0400

    WorkingDir

--- Java^/JSE^/io^/WorkingDir/src/Main.java
+++ Java^/JSE^/io^/WorkingDir/src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
+	
+	//TODO Работает ли в Windows??
     public static void main(String[] args) {
         //Linux:
-        System.out.printf("System.getenv(\"PWD\") -> %s%n", System.getenv("PWD"));
+        System.out.printf("System.getenv(\"PWD\")           -> %s%n", System.getenv("PWD"));
+        //При запуске из Idea PWD заполняется неправильно
         
-        //TODO Работает ли в Windows??
+		System.out.printf("System.getProperty(\"user.dir\") -> %s%n", System.getProperty("user.dir"));
     }
 }
