3e0275e9bca342d5c34c91e889de4e70bc1b68c5
Aleksey Yablokov <ya_al@bk.ru>
2014-07-31 10:06:29 +0400

    Relational

---
+++ Java^/JSE^/Core^/Operator^/Relational/src/Main.java
@@ -0,0 +1,8 @@
+import static java.lang.System.out;
+
+public class Main {
+    public static void main(String[] args) {
+		boolean myBool = false;
+		//out.println(myBool > false); //don't compile
+    }
+}
