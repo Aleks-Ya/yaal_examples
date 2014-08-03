40cd2799e5ec5942643142558e38b43de538a3cb
Aleksey Yablokov <ya_al@bk.ru>
2014-08-03 12:57:25 +0400

    Print

---
+++ Java^/JSE^/Core^/Print/src/Main.java
@@ -0,0 +1,13 @@
+import static java.lang.System.out;
+
+public class Main {
+    public static void main(String[] args) {
+        double d = 0;
+        out.print("double: ");
+        out.println(d);
+        
+        float f = 1;
+        out.print("float: ");
+        out.println(f);
+    }
+}
