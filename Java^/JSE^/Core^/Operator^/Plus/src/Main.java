24798506ed32ed51e96c18e9a870d7a1fa390542
Aleksey Yablokov <ya_al@bk.ru>
2014-08-12 18:36:04 +0400

    Plus

---
+++ Java^/JSE^/Core^/Operator^/Plus/src/Main.java
@@ -0,0 +1,14 @@
+import static java.lang.System.out;
+
+public class Main {
+    public static void main(String[] args) {
+		stringCharConcat();
+    }
+    
+    /**
+     * Конкатенция String и char.
+     */
+    private static void stringCharConcat() {
+		out.println("string " + 'c');
+	}
+}
