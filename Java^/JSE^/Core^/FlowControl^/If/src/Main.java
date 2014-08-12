ddbe8e8c273aa9c4d23b6cc80bf0e79f40a05eca
Aleksey Yablokov <ya_al@bk.ru>
2014-08-12 23:14:38 +0400

    If

--- Java^/JSE^/Core^/FlowControl^/If/src/Main.java
+++ Java^/JSE^/Core^/FlowControl^/If/src/Main.java
@@ -2,16 +2,26 @@ import static java.lang.System.out;
 
 public class Main {
     public static void main(String[] args) {
-		withoutBraces();
+        withoutBraces();
+        multiWithoutBraces();
     }
  
-	/**
-	 * if-else без скобок.
-	 */
+    /**
+     * if-else без скобок.
+     */
     private static void withoutBraces() {
-		if (1 < 2) 
-			for (int i = 0; i < 3; i++) System.out.println(i);
-		else
-			for (int i = 9; i > 6; i--) System.out.println(i);
-	}
+        if (1 < 2) 
+          for (int i = 0; i < 3; i++) out.println(i);
+        else
+          for (int i = 9; i > 6; i--) out.println(i);
+    }
+    
+    /**
+     * else относится ко 2му if.
+     */
+    private static void multiWithoutBraces() {
+        if (false) out.println("if 1");
+        if (true) out.println("if 2");
+        else out.println("else");
+    }
 }
