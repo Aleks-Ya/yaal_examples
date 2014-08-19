cab836094112a96da1ec04b39eba46b43caa1457
Aleksey Yablokov <ya_al@bk.ru>
2014-08-19 09:45:02 +0400

    DeclareButDoNotThrow

---
+++ Java^/JSE^/Core^/Exception^/DeclareButDoNotThrow/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+      System.out.println(getInt());
+    }
+    
+    static int getInt() throws Exception {
+      return 1;
+    }
+}
