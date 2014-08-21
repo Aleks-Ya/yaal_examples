fac0a8c293aeb180d1afc20c0d835c4b3667fb1c
Aleksey Yablokov <ya_al@bk.ru>
2014-08-21 15:30:16 +0400

    ForExercise

---
+++ Java^/JSE^/Core^/FlowControl^/For^/ForExercise/src/BankInterest.java
@@ -0,0 +1,24 @@
+import static java.lang.System.out;
+
+/**
+ * Банк начисляет на вклады 5% каждый месяц. 
+ * Напечатать таблицу увеличения суммы вклада по месяцам на год. 
+ * Сумма вклада вводится с клавиатуры
+ */ 
+public class BankInterest {
+    public static void main(String[] args) {
+		final float percent = 0.01f;
+		final double startAmount = 500_000;
+		final int months = 12;
+		
+		double amount = startAmount;
+		for (int m = 0; m < months; m++) {
+			amount *= 1 + percent;
+		}
+		
+		out.printf("Start sum: %.2f%n", startAmount);
+		out.printf("Period: %d months%n", months);
+		out.printf("Month percent: %.0f%%%n", percent * 100);
+		out.printf("Finish sum: %.2f%n", amount);
+    }
+}
