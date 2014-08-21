f64e32335ecc49efe8e0dbf3470189eee1d84671
Aleksey Yablokov <ya_al@bk.ru>
2014-08-21 15:48:37 +0400

    ForExcercise

---
+++ Java^/JSE^/Core^/FlowControl^/For^/ForExercise/src/Fish.java
@@ -0,0 +1,29 @@
+import static java.lang.System.out;
+
+/**
+ * Ежегодный прирост рыбы в пруду составляет 15%. 
+ * Запасы рыбы оценены в A тонн. 
+ * Ежегодный план отлова  B тонн. 
+ * Наименьший запас рыбы, ниже которого запас уже не восстанавливается, 
+ * составляет С тонн. 
+ * Составить алгоритм и программу, подсчитывающую, 
+ * сколько лет можно выдержать заданный план.
+ */ 
+public class Fish {
+    public static void main(String[] args) {
+		final float  yearIncreasePercent = 0.15f;
+		final double startValue = 5_000;
+		final double yearCatchValue = 1_000;
+		final double deadValue = 500;
+		
+		int year = 0;
+		double yearFinishValue = startValue;
+		do {
+			year++;
+			double yearIncreaseValue = yearFinishValue * yearIncreasePercent;
+			out.printf("Year: %d, Year Increase: %.2f, Finish Value: %.0f%n", year, yearIncreaseValue, yearFinishValue);
+			yearFinishValue = yearFinishValue + yearIncreaseValue - yearCatchValue;
+		} while(yearFinishValue >= deadValue);
+		out.printf("Year: %d%n", year);
+    }
+}
