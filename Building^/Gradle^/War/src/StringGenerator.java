b125ccdfc3e3810b0a74f386ba859522bef71534
Aleksey Yablokov <ya_al@bk.ru>
2014-08-25 18:35:54 +0400

    War

--- Building^/Gradle^/War/src/StringGenerator.java
+++ Building^/Gradle^/War/src/StringGenerator.java
@@ -1,5 +1,8 @@
+import org.apache.commons.lang3.StringUtils;
+
 public class StringGenerator {
     public static String getString() {
+		System.out.println(StringUtils.isBlank(null));
         return "Just do it!";
     }
 }
