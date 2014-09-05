1b89a30a6197065e061f1729649ee2aac94a5c90
Aleksey Yablokov <ya_al@bk.ru>
2014-09-05 13:02:57 +0400

    XmlToJava

--- Java^/JSE^/JAXB^/XmlToJava/src/Lamp.java
+++ Java^/JSE^/JAXB^/XmlToJava/src/Lamp.java
@@ -1,11 +1,18 @@
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlValue;
 
 public class Lamp {
     @XmlValue
     private String value;
 
+    /**
+     * Перечислимое.
+     */
+    @XmlAttribute
+    private LampType type;
+
     @Override
     public String toString() {
-        return String.format("Lamp[value='%s']", value);
+        return String.format("Lamp[value='%s' type=%s]", value, type);
     }
 }
---
+++ Java^/JSE^/JAXB^/XmlToJava/src/LampType.java
@@ -0,0 +1,11 @@
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+
+@XmlEnum
+enum LampType {
+    @XmlEnumValue("bright")
+    BRIGHT,
+
+    @XmlEnumValue("dark")
+    DARK;
+}
