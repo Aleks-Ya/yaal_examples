f26dfc5fcb94c1ca028eb3a4d0a950f461e39ed6
Aleksey Yablokov <ya_al@bk.ru>
2014-09-10 16:43:39 +0400

    Cast

--- Java^/JSE^/Core^/Cast/src/Main.java
+++ Java^/JSE^/Core^/Cast/src/Main.java
@@ -4,6 +4,9 @@ public class Main {
 	
     public static void main(String[] args) {
 		charIntByte();
+		integerNums();
+		integerNums2();
+		integerNums3();
     }
     
     static void charIntByte() {
@@ -11,5 +14,84 @@ public class Main {
 		int i = c;
 		byte b = (byte) i;
 		out.printf("char=%s int=%d byte=%d", c, i, b);
+    }
+
+ 	/**
+     * Присвоение примитивных типов без приведения.
+     */
+    static void integerNums() {
+    	final byte b = 1;
+    	final short s = 10_000;
+    	final int i = 1_000_000;
+    	final long l = 5_000_000_000L;
+
+    	long l2 = b;
+    	l2 = s;
+    	l2 = i;
+
+    	int i2 = b;
+    	i2 = s;
+
+    	short s2 = b;
+    }
+    
+    /**
+     * Приведение примитивных типов без переполнения.
+     */
+    static void integerNums2() {
+    	final byte b = 1;
+    	final short s = 2;
+    	final int i = 3;
+    	final long l = 4L;
+
+    	long l2 = b;
+    	l2 = s;
+    	l2 = i;
+    	l2 = l;
+
+    	int i2 = b;
+    	i2 = s;
+    	i2 = i;
+    	i2 = (int) l;
+
+    	short s2 = b;
+    	s2 = s;
+    	s2 = (short) i;
+    	s2 = (short) l;
+
+    	byte b2 = b;
+    	b2 = (byte) s;
+    	b2 = (byte) i;
+    	b2 = (byte) l;
+    }
+
+        /**
+     * Приведение примитивных типов с переполнением.
+     */
+    static void integerNums3() {
+    	final byte b = 1;
+    	final short s = 10_000;
+    	final int i = 1_000_000;
+    	final long l = 5_000_000_000L;
+
+    	long l2 = b;
+    	l2 = s;
+    	l2 = i;
+    	l2 = l;
+
+    	int i2 = b;
+    	i2 = s;
+    	i2 = i;
+    	i2 = (int) l;
+
+    	short s2 = b;
+    	s2 = s;
+    	s2 = (short) i;
+    	s2 = (short) l;
+
+    	byte b2 = b;
+    	b2 = (byte) s;
+    	b2 = (byte) i;
+    	b2 = (byte) l;
     }
 }
