09b8bd1331cdb6c11d8d5ab82a9c0e402f0b028e
Aleksey Yablokov <ya_al@bk.ru>
2014-09-15 10:46:54 +0400

    SubstringSearch

--- Java^/JSE^/RegExp^/SelectSubstring/src/SubstringSearch.java
+++ Java^/JSE^/RegExp^/SelectSubstring/src/SubstringSearch.java
@@ -6,7 +6,7 @@ import static java.lang.System.out;
 /**
  * Поиск нескольких вхождений регулярного выражения в строку.
  */
-public class MultiSubstring {
+public class SubstringSearch {
     public static void main(String[] args) {
         final String source = "concert Einaudi ludovico einaudi today ";
         Pattern p = Pattern.compile("[Ee]inaudi");
