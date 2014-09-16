a9b7f1664e32e4b3886835b9cee773aff74dd07b
Aleksey Yablokov <ya_al@bk.ru>
2014-09-16 13:23:49 +0400

    Map

---
+++ Groovy^/Map/src/Maps.groovy
@@ -0,0 +1,16 @@
+class Maps {
+    static void main(args) {
+        def map = [name:"Gromit", likes:"cheese", id:1234]
+        assert map.get("name") == "Gromit"
+        assert map.get("id") == 1234
+        assert map["name"] == "Gromit"
+        assert map['id'] == 1234
+        assert map instanceof java.util.Map
+
+        def emptyMap = [:]
+        assert emptyMap.size() == 0
+        emptyMap.put("foo", 5)
+        assert emptyMap.size() == 1
+        assert emptyMap.get("foo") == 5
+    }
+}
