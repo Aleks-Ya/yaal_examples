2e53261713a3ac5530a080d937b068e078d313ec
Aleksey Yablokov <ya_al@bk.ru>
2014-09-23 06:45:49 +0400

    KeyWord

---
+++ Java^/JSE^/Core^/KeyWord/test/StringKeyWord.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static java.lang.System.out;
+
+public class StringKeyWord {
+
+	/**
+	 * String String - корректное выражение.
+	 */
+	@Test    
+    public void numbers() {
+		String String = "oppa!"; 
+		out.println(String);
+	}
+}
