dbc9dccbac6e53fbb7be30e44eff1d83b5369d49
Aleksey Yablokov <ya_al@bk.ru>
2014-09-24 12:59:09 +0400

    Replace

--- Java^/JSE^/Core^/String^/StringBuilder/src/Main.java
+++
@@ -1,9 +0,0 @@
-import static java.lang.System.out;
-
-public class Main {
-    public static void main(String[] args) {
-		StringBuilder sb = new StringBuilder("0123456");
-		sb.replace(1,2,"abcde");
-		out.println(sb);
-    }
-}
---
+++ Java^/JSE^/Core^/String^/StringBuilder/test/Replace.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Replace {
+    
+    @Test
+    public void replace() {
+		StringBuilder sb = new StringBuilder("0123456");
+		sb.replace(1,2,"abcde");
+		assertEquals("0abcde23456", sb.toString());
+    }
+}
