c4886aaa3d4259e742441ec957468034503ab6fe
Aleksey Yablokov <ya_al@bk.ru>
2014-09-27 22:45:06 +0400

    Break

---
+++ Java^/JSE^/Core^/FlowControl^/Break/test/WithLabel.java
@@ -0,0 +1,40 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * break с меткой позволяет перейти на любой оператор.
+ */
+public class WithLabel {
+
+    /**
+     * Прерываем блок кода, ограниченный {}.
+     */
+    @Test
+    public void codeBlock() {
+        int a;
+        block:
+        {
+            a = 1;
+            break block;
+        }
+        assertEquals(1, a);
+    }
+
+    /**
+     * Прерываем блок кода, ограниченный {}.
+     */
+    @Test
+    public void twoCodeBlocks() {
+        int a;
+        outer:
+        {
+            block:
+            {
+                a = 1;
+                break outer;
+            }
+        }
+        assertEquals(1, a);
+    }
+}
