6c16c04b80c7670a3ecef666fc5454a11af2d1c6
Aleksey Yablokov <ya_al@bk.ru>
2014-09-27 18:48:39 +0400

    Array

---
+++ Java^/JSE^/Core^/Array/test/assignment/SubClass.java
@@ -0,0 +1,36 @@
+package assignment;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Присвоение переменной массива суперкласса
+ * массива дочернего класса.
+ */
+public class SubClass {
+
+    /**
+     * Примитивные типы.
+     */
+    @Test
+    public void primitive() throws Exception {
+        long[] a;
+        int[] b = {1};
+
+        //Compile errors:
+        //a = b;
+        //a = (long[]) b;
+        //Assert.assertArrayEquals(a, b);
+    }
+
+    /**
+     * Ссылочные типы.
+     */
+    @Test
+    public void reference() throws Exception {
+        Exception[] a;
+        RuntimeException[] b = {new RuntimeException()};
+        a = b;
+        Assert.assertArrayEquals(a, b);
+    }
+}
