cc52082b0ded1b40550ad2377547fff1e6aa03f4
Aleksey Yablokov <ya_al@bk.ru>
2014-09-30 11:49:45 +0400

    InitializationSequence

--- Java^/JSE^/Core^/Inheritance^/InitializationSequence/src/Main.java
+++ Java^/JSE^/Core^/Inheritance^/InitializationSequence/src/Main.java
@@ -1,29 +1,68 @@
+import static java.lang.System.out;
+
 class Parent {
-	static int i = initField();  
-	{      
-		System.out.println(" Parent object initializer");
+	private static int i1 = staticField();
+
+    static {
+		out.println("1 Parent static initializer");
 	}
-	static { 
-		System.out.println(" Parent static initializer"); 
-	}
-	static int initField() {
-		System.out.println(" Parent static field "); 
+
+	private static int i2 = staticField();
+
+	private static int staticField() {
+		out.println("1 Parent static field ");
 		return 0;
 	}
+
+    private static int field() {
+		out.println("3 Parent field ");
+		return 0;
+	}
+
+    Parent() {
+        out.println("4 Parent constructor ");
+    }
+
+    private int o1 = field();
+
+	{
+		out.println("3 Parent object initializer");
+	}
+    private int o2 = field();
 }
 
 class Child extends Parent {
+    private static int i1 = staticField();
 	static {
-		System.out.println(" Child static initializer");
+		out.println("2 Child static initializer");
 	}
+    private static int i2 = staticField();
+
+    Child() {
+        out.println("6 Child constructor ");
+    }
+
+    private int o1 = field();
 	{
-		System.out.println(" Child object initializer");
+		out.println("5 Child object initializer");
 	}
+    private int o2 = field();
+
+
+    private static int staticField() {
+        out.println("2 Child static field ");
+        return 0;
+    }
+
+    private static int field() {
+        out.println("5 Child field ");
+        return 0;
+    }
 }
 
 class ChildOfChild extends Child{
 	static { 
-		System.out.println("ChildOfChild static initializer"); 
+		out.println("ChildOfChild static initializer");
 	}
 }
 
