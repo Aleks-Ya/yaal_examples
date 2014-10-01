edbcf3079d9f21eade7b1281e05ba933c349dd0f
Aleksey Yablokov <ya_al@bk.ru>
2014-10-01 10:57:58 +0400

    Variable

---
+++ Java^/JSE^/Core^/Variable/test/Variables.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static java.lang.System.out;
+
+/**
+ * Имена переменных Java.
+ */
+public class Variables {
+    @Test
+    public void correctNames() {
+        int _ = 1;
+        int $ = 2;
+        out.println(_ + $);
+    }
+}
