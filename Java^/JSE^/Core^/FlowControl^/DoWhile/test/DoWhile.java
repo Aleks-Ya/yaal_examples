cb0b9e3d4e281aa9e0624dfaeb26ba5b8f524bfc
Aleksey Yablokov <ya_al@bk.ru>
2014-10-05 00:11:32 +0400

    DoWhile

---
+++ Java^/JSE^/Core^/FlowControl^/DoWhile/test/DoWhile.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DoWhile {
+
+    /**
+     * Блок do без {} возможен для одного оператора.
+     */
+    @Test
+    public void oneStatement() {
+        int i = 0;
+        do i++; while (i < 5);
+        //do i++; i++; while (i < 5); //compile error: "while expected"
+        assertEquals(5, i);
+    }
+}
