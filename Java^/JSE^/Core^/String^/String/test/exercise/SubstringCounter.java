dcaf7bbfc03830e284d00b634d7181c348f234d6
Aleksey Yablokov <ya_al@bk.ru>
2014-10-05 22:43:13 +0400

    SubstringCounter

---
+++ Java^/JSE^/Core^/String^/String/test/exercise/SubstringCounter.java
@@ -0,0 +1,33 @@
+package exercise;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Подсчитать количество подстроки в строку (разными способами).
+ */
+public class SubstringCounter {
+	
+	@Test
+	public void count1() {
+		assertEquals(3, counter1("doremefadosolliasido", "do"));
+		assertEquals(0, counter1("Semper Fidelis", "not"));
+		assertEquals(0, counter1("Semper Fidelis", ""));
+		assertEquals(0, counter1(null, "no"));
+		assertEquals(0, counter1("", "a"));
+		assertEquals(0, counter1("Semper Fidelis", null));
+	}
+	
+	private int counter1(String src, String substring) {
+		if (src == null || substring == null || substring.isEmpty()) {
+			return 0;
+		}
+		int counter = 0;
+		int index = 0;
+		while((index = src.indexOf(substring, index)) != -1) {
+			counter++;
+			index++;
+		}
+		return counter;
+	}
+}
