2c2336b11c8529741c67d2e65b16c58859918821
Aleksey Yablokov <ya_al@bk.ru>
2014-10-06 09:16:37 +0400

    Init

---
+++ Java^/JSE^/Core^/Operator^/Assignment/test/Init.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Init {
+
+	@Test
+    public void test() {
+        byte b = 127;
+        short s = 1128;
+		double d = 10;
+        double d2 = 2_000_000_000_000d;
+        float f = 1;
+        float f2 = 2_000_000_000_000f;
+        long l = 3;
+        System.out.println(d + f + f2 + l);
+    }
+}
