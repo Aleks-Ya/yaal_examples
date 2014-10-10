1ce7f7d299359e60f89ed102e2a0625cf46e5236
Aleksey Yablokov <ya_al@bk.ru>
2014-10-10 11:41:32 +0400

    remove MavenLocal

--- Building^/Gradle^/Multimodule^/Dependencies^/MavenLocal/src/Main.java
+++
@@ -1,8 +0,0 @@
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-
-public class Main {
-
-    public static void main(String[] args){
-        StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder();
-    }
-}
