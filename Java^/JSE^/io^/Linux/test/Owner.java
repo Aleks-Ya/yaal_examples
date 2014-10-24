9e23e9b3d592619d71a6557cc0e246cbf379aa03
Aleksey Yablokov <ya_al@bk.ru>
2014-10-24 13:40:07 +0400

    Owner

---
+++ Java^/JSE^/io^/Linux/test/Owner.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.UserPrincipal;
+import java.nio.file.attribute.UserPrincipalLookupService;
+
+import static java.lang.System.out;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Изменение владельца файла.
+ */
+public class Owner {
+
+    @Test
+    public void main() throws IOException {
+        Path target = Files.createTempFile("target_", ".sh");
+        target.toFile().deleteOnExit();
+
+        String user = System.getenv("USERNAME");
+        UserPrincipal owner = Files.getOwner(target);
+        assertEquals(user, owner.getName());
+        out.printf("target = %s%n", target);
+        out.printf("%s%n", owner);
+
+
+        UserPrincipalLookupService lookupService = FileSystems.getDefault().getUserPrincipalLookupService();
+        String rootUser = "root";
+        UserPrincipal newOwner = lookupService.lookupPrincipalByName(rootUser);
+
+        //java.nio.file.FileSystemException: /tmp/target_989023771236240807.sh: Operation not permitted
+        //Files.setOwner(target, newOwner);
+        //assertEquals(rootUser, Files.getOwner(target).getName());
+    }
+}
