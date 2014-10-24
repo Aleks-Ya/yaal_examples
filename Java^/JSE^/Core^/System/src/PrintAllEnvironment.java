f6d3237bb021b26312fbd0972f56a8a56efe5608
Aleksey Yablokov <ya_al@bk.ru>
2014-10-24 13:54:26 +0400

    System

---
+++ Java^/JSE^/Core^/System/src/PrintAllEnvironment.java
@@ -0,0 +1,15 @@
+import java.util.Map;
+
+/**
+ * Выводит в консоль все переменные окружения.
+ */
+public class PrintAllEnvironment {
+
+    public static void main(String[] args) {
+        System.out.println("ALL FROM System.getenv() \n");
+        Map<String, String> envs = System.getenv();
+        for (Map.Entry<String, String> entry : envs.entrySet()) {
+            System.out.printf("%s=%s%n", entry.getKey(), entry.getValue());
+        }
+    }
+}
