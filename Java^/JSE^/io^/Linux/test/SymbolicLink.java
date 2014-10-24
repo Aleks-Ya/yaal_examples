9bbbcc005f903e6bfe5c4c8d02b2cf6eb3877236
Aleksey Yablokov <ya_al@bk.ru>
2014-10-24 13:18:04 +0400

    Linux

--- Java^/JSE^/io^/Linux/test/SymbolicLink.java
+++ Java^/JSE^/io^/Linux/test/SymbolicLink.java
@@ -1,14 +1,19 @@
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.Files;
-import java.io.IOException;
+
 import static java.lang.System.out;
 
 /**
  * Создание символической ссылки Linux с правами на исполнение.
  */
 public class SymbolicLink {
-    public static void main(String[] args) throws IOException {
+
+    @Test
+    public void main() throws IOException {
         Path target = Files.createTempFile("target_", ".sh");
         Path link = Paths.get(target.getParent().toString(), "link_to_target");
         Files.deleteIfExists(link);
