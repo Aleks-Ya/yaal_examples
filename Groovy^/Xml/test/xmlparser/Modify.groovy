63bb8fdf75e91be40e47d3379b0be9463e0df6fe
Aleksey Yablokov <ya_al@bk.ru>
2014-10-30 20:10:58 +0300

    Modify

---
+++ Groovy^/Xml/test/xmlparser/Modify.groovy
@@ -0,0 +1,37 @@
+package xmlparser
+
+import org.junit.Test
+
+import static org.junit.Assert.assertEquals
+
+/**
+ * Изменение узлов XML-документа.
+ */
+class Modify {
+    def plan = new XmlParser().parse(ReadXmlFile.class.getResourceAsStream('plan.xml'))
+    def xmlPrinter = new XmlNodePrinter()
+
+    @Test
+    public void changeValue() {
+        def found = plan.week[0].task[1]
+        println "Old: ${found.value()}"
+        def s = 'New value'
+        found.value = s
+        println "New: ${found.value()}"
+        assertEquals(s, found.value())
+    }
+
+    @Test
+    public void changeAttribute() {
+        def found = plan.week[0].task[1]
+        println "Old: ${found.@done}"
+        assertEquals('3', found.@done)
+        def s = '777'
+        found.@done = s
+        println "New: ${found.@done}"
+        assertEquals(s, found.@done)
+
+        xmlPrinter.print(found)
+        xmlPrinter.print(plan)
+    }
+}
