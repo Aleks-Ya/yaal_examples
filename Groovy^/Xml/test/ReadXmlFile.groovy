8c2b882fa26ad4192c7d03829e606da9f42531a3
Aleksey Yablokov <ya_al@bk.ru>
2014-10-30 16:50:43 +0300

    ReadXmlFile

---
+++ Groovy^/Xml/test/ReadXmlFile.groovy
@@ -0,0 +1,25 @@
+import groovy.xml.DOMBuilder
+import groovy.xml.dom.DOMCategory
+import org.junit.Test
+
+import static org.junit.Assert.assertEquals
+
+/**
+ * Чтение и парсинг XML из файла.
+ */
+class ReadXmlFile {
+
+    @Test
+    void parse() {
+        def reader = new InputStreamReader(ReadXmlFile.class.getResourceAsStream('plan.xml'))
+        def doc = DOMBuilder.parse(reader)
+        def plan = doc.documentElement
+        use(DOMCategory) {
+            assertEquals('plan', plan.nodeName)
+            assertEquals('week', plan[1].nodeName)
+            assertEquals(2, plan.week.size())
+            assertEquals('week', plan.week[0].nodeName)
+            assertEquals('8', plan[1].'@capacity')
+        }
+    }
+}
