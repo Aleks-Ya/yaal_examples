62e64676fbd9a30f4b6d2cf33622e822ec959b6b
Aleksey Yablokov <ya_al@bk.ru>
2014-10-30 17:07:14 +0300

    ConstructXml

---
+++ Groovy^/Xml/test/ConstructXml.groovy
@@ -0,0 +1,23 @@
+import groovy.xml.MarkupBuilder
+import org.junit.Test
+
+/**
+ * Конструирование нового XML-документа.
+ */
+class ConstructXml {
+
+    @Test
+    void construct() {
+        def writer = new StringWriter()
+        def xml = new MarkupBuilder(writer)
+        xml.bibliography {
+            author('William Shakespeare')
+            play {
+                year('1595')
+                title('A Midsummer-Night')
+            }
+        }
+
+        println writer
+    }
+}
