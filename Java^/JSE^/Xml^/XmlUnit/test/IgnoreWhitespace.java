aab8d16ebf1012b051f043202ea1a39df1bf79b4
Aleksey Yablokov <ya_al@bk.ru>
2014-10-31 14:24:19 +0300

    IgnoreWhitespace

---
+++ Java^/JSE^/Xml^/XmlUnit/test/IgnoreWhitespace.java
@@ -0,0 +1,36 @@
+import org.custommonkey.xmlunit.DetailedDiff;
+import org.custommonkey.xmlunit.Diff;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Игнорирование пробелов и переносов строк при сравнении XML.
+ */
+public class IgnoreWhitespace {
+    String one = "<plan><week capacity='8' /></plan>";
+
+    String two = "<plan>\n" +
+            "<week capacity='8' />\n" +
+            "</plan>";
+
+    @Test
+    public void ignore() throws IOException, SAXException {
+        XMLUnit.setIgnoreWhitespace(true);
+        DetailedDiff diff = new DetailedDiff(new Diff(one, two));
+        assertTrue(diff.toString(), diff.similar());
+
+    }
+
+    @Test
+    public void notIgnore() throws IOException, SAXException {
+        XMLUnit.setIgnoreWhitespace(false);
+        DetailedDiff diff = new DetailedDiff(new Diff(one, two));
+        assertFalse(diff.toString(), diff.similar());
+    }
+}
