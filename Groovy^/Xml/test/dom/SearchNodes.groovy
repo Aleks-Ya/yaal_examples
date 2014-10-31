1c320d91d1846655e07bda6a71ee6a5fb211746f
Aleksey Yablokov <ya_al@bk.ru>
2014-10-31 11:09:43 +0300

    SearchNodes

--- Groovy^/Xml/test/dom/SearchNodes.groovy
+++ Groovy^/Xml/test/dom/SearchNodes.groovy
@@ -10,24 +10,11 @@ import org.junit.Test
 class SearchNodes {
     @Test
     public void byNodeName() {
-        def reader = new InputStreamReader(ReadXmlFile.class.getResourceAsStream('plan.xml'))
+        def reader = new InputStreamReader(SearchNodes.class.getResourceAsStream('plan.xml'))
         def doc = DOMBuilder.parse(reader)
         def plan = doc.documentElement
-
-        //Не работает нихуя!
-//        println plan
-//        println plan.find{
-//            println it.nodeName
-//            'plan'.equals(it.nodeName)}
-//        println plan.childNodes
-
         use(DOMCategory) {
-           println plan.findAll{}
-//            {
-//            'task' == it.nodeName
-//                1 < 2
-//            }.each {
-//            }
+            println plan.childNodes.findAll { it.name() == 'week' }
         }
     }
 }
