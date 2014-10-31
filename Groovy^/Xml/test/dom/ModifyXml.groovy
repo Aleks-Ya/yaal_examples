d41631677eeaf9a8adfeb4f2b94c40d865fb7fa9
Aleksey Yablokov <ya_al@bk.ru>
2014-10-31 15:54:27 +0300

    ModifyXml

--- Groovy^/Xml/test/dom/ModifyXml.groovy
+++ Groovy^/Xml/test/dom/ModifyXml.groovy
@@ -2,7 +2,12 @@ package dom
 
 import groovy.xml.DOMBuilder
 import groovy.xml.dom.DOMCategory
+import org.custommonkey.xmlunit.DetailedDiff
+import org.custommonkey.xmlunit.Diff
+import org.custommonkey.xmlunit.XMLUnit
 import org.junit.Test
+
+import static org.junit.Assert.assertTrue
 
 /**
  * Изменение XML документа в памяти.
@@ -11,12 +16,33 @@ class ModifyXml {
 
     @Test
     void appendNode() {
-        def reader = new InputStreamReader(ModifyXml.class.getResourceAsStream('plan.xml'))
-        def doc = DOMBuilder.parse(reader)
-        def plan = doc.documentElement
+        def actualReader = new StringReader(
+                """
+            <plan>
+                <week capacity="8" />
+            </plan>
+            """
+        )
+
+        def actualDoc = DOMBuilder.parse(actualReader)
+        def plan = actualDoc.documentElement
         use(DOMCategory) {
             plan.appendNode('itemName', "Item value.")
         }
+
+        def expectedDoc = XMLUnit.buildTestDocument(
+                """
+            <plan>
+                <week capacity="8" />
+                <itemName>Item value.</itemName>
+            </plan>
+            """
+        )
+
+        XMLUnit.setIgnoreWhitespace(true)
+
+        DetailedDiff diff = new DetailedDiff(new Diff(expectedDoc, actualDoc))
+        assertTrue(diff.toString(), diff.similar())
         println plan
     }
 }
