1a8fecd59bca7acb056eff1f0ecde5d1a5a49cf2
Aleksey Yablokov <ya_al@bk.ru>
2014-11-10 17:26:08 +0300

    Size

--- Java^/JSE^/gui^/swing^/Container^/JComponent^/JTextComponent^/JTextField/src/Size.java
+++ Java^/JSE^/gui^/swing^/Container^/JComponent^/JTextComponent^/JTextField/src/Size.java
@@ -1,6 +1,7 @@
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.HeadlessException;
@@ -23,11 +24,14 @@ class SizeFrame extends JFrame {
     SizeFrame() throws HeadlessException {
         init();
 
-        JTextField textShort = new JTextField("Hello", 5);
-        JTextField textLong = new JTextField("Hello", 10);
+        JTextField textShort = new JTextField("Ten symbols ", 10);
+        JTextField textLong = new JTextField("Fifteen symbols", 15);
+        JTextField textHeight = new JTextField("Height text");
+        textHeight.setPreferredSize(new Dimension(200, 50));
 
         add(textShort);
         add(textLong);
+        add(textHeight);
     }
 
     private void init() {
