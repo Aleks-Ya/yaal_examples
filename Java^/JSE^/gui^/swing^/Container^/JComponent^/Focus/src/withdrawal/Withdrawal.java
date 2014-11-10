e8ae1fd6e170912ff9d24534db6e2144ff82efcf
Aleksey Yablokov <ya_al@bk.ru>
2014-11-10 11:42:18 +0300

    Withdrawal

--- Java^/JSE^/gui^/swing^/Container^/JComponent^/Focus/src/withdrawal/Withdrawal.java
+++ Java^/JSE^/gui^/swing^/Container^/JComponent^/Focus/src/withdrawal/Withdrawal.java
@@ -1,8 +1,17 @@
 package withdrawal;
 
-import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
 import javax.swing.border.Border;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.GridLayout;
 
 public class Withdrawal {
     public static void main(String[] args) {
@@ -17,21 +26,30 @@ public class Withdrawal {
 
 class WithdrawalFrame extends JFrame {
     public WithdrawalFrame() {
-        JPanel textFieldsPanel = new JPanel();
-        textFieldsPanel.setLayout(new GridLayout(3, 3, 30, 30));
-        JTextField tf1 = new JTextField("1");
-        textFieldsPanel.add(tf1);
-        textFieldsPanel.add(new JTextField("2"));
-        textFieldsPanel.add(new JTextField("3"));
-        textFieldsPanel.add(new JTextField("4"));
-        textFieldsPanel.add(new JTextField("5"));
-        textFieldsPanel.add(new JPanel());
-        textFieldsPanel.add(new JTextField("7"));
-        textFieldsPanel.add(new JTextField("8"));
-        textFieldsPanel.add(new JTextField("9"));
+        JPanel textFieldsPanel = new JPanel(new GridLayout(3, 3, 30, 30));
+
+        final JComponent c00 = new JTextField("1");
+        final JComponent c01 = new JTextField("2");
+        final JComponent c02 = new JTextField("3");
+        final JComponent c10 = new JTextField("4");
+        final JComponent c11 = new JTextField("5");
+        final JComponent c12 = new JPanel();
+        final JComponent c20 = new JTextField("7");
+        final JComponent c21 = new JTextField("8");
+        final JComponent c22 = new JTextField("9");
+
+        textFieldsPanel.add(c00);
+        textFieldsPanel.add(c01);
+        textFieldsPanel.add(c02);
+        textFieldsPanel.add(c10);
+        textFieldsPanel.add(c11);
+        textFieldsPanel.add(c12);
+        textFieldsPanel.add(c20);
+        textFieldsPanel.add(c21);
+        textFieldsPanel.add(c22);
 
         Border border = BorderFactory.createLineBorder(Color.RED, 5);
-        tf1.setBorder(border);
+        c00.setBorder(border);
 
         JPanel buttonsPanel = new JPanel();
         buttonsPanel.add(new JButton("OK"));
