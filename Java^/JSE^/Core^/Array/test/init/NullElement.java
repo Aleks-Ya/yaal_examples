9b0c47f06d6af53073852957a0b37b419a87793d
Aleksey Yablokov <ya_al@bk.ru>
2014-11-10 12:33:40 +0300

    NullElement

--- Java^/JSE^/Core^/Array/test/init/NullElement.java
+++ Java^/JSE^/Core^/Array/test/init/NullElement.java
@@ -2,17 +2,33 @@ package init;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Использование null при инициализации массива.
  */
 public class NullElement {
+
+    /**
+     * Одномерный массив.
+     */
     @Test
-    public void main() {
-        /* Массив ссылок */
+    public void oneDimension() {
         String[] arr = {null, null};
-        
-        /* Массив примитивов */
-        //compile error: incompatible types
-        //int[] arr2 = {null, null};
+        assertEquals(2, arr.length);
+    }
+
+    /**
+     * Многомерный массив.
+     */
+    @Test
+    public void multiDimensions() {
+        String[][] arr = {
+                null,
+                null,
+                {null, null}
+        };
+        assertEquals(3, arr.length);
+        assertEquals(2, arr[2].length);
     }
 }
