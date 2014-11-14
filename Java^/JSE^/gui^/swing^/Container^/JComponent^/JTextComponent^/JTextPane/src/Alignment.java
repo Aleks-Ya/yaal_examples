aff74cc4f8e59b6f75206db38203c71b388baf79
Aleksey Yablokov <ya_al@bk.ru>
2014-11-14 09:54:18 +0300

    JTextPane

---
+++ Java^/JSE^/gui^/swing^/Container^/JComponent^/JTextComponent^/JTextPane/src/Alignment.java
@@ -0,0 +1,33 @@
+import javax.swing.JFrame;
+import javax.swing.JTextPane;
+import javax.swing.WindowConstants;
+import javax.swing.text.Document;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+import java.awt.Component;
+
+/**
+ * Выравнивание текста.
+ */
+public class Alignment {
+    public static void main(String[] args) {
+        JTextPane editor = new JTextPane();
+        editor.setEditable(false);
+        editor.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        editor.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+        editor.setText("Происхождение Александра Сергеевича Пушкина идёт от разветвлённого нетитулованного дворянского " +
+                "рода Пушкиных, восходившего по генеалогической легенде к «мужу честну» Ратше, современнику " +
+                "Александра Невского.");
+        StyledDocument doc = editor.getStyledDocument();
+        SimpleAttributeSet attrs = new SimpleAttributeSet();
+        StyleConstants.setAlignment(attrs, StyleConstants.ALIGN_RIGHT);
+        doc.setParagraphAttributes(0, doc.getLength(), attrs, false);
+
+        JFrame frame = new JFrame();
+        frame.setSize(600, 400);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.add(editor);
+    }
+}
