1ac295833139f52216b67d8490383511f1da5cc8
Aleksey Yablokov <ya_al@bk.ru>
2014-11-19 14:06:39 +0300

    BundleHierarchy

--- Java^/JSE^/I18n^/ResourceBundle/test/hierarchy/BundleHierarchy.java
+++ Java^/JSE^/I18n^/ResourceBundle/test/hierarchy/BundleHierarchy.java
@@ -3,6 +3,7 @@ package hierarchy;
 import org.junit.Test;
 
 import java.util.Locale;
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 import static org.junit.Assert.assertEquals;
@@ -11,12 +12,25 @@ import static org.junit.Assert.assertEquals;
  * Поиск локализованной строки по иерархии ресурсов.
  */
 public class BundleHierarchy {
+
+    /**
+     * Ищем строку в родительских пакетах.
+     */
     @Test
-    public void test() {
+    public void fromChild() {
         ResourceBundle parent = ResourceBundle.getBundle("hierarchy.Names", new Locale("ru", "RU", "vologda"));
         assertEquals("ru RU vologda", parent.getString("ruRUvologdaKey"));
         assertEquals("ru RU", parent.getString("ruRUKey"));
         assertEquals("ru", parent.getString("ruKey"));
         assertEquals("nothing", parent.getString("Key"));
+    }
+
+    /**
+     * Попытка искать строку, находяюущюся в потомке.
+     */
+    @Test(expected = MissingResourceException.class)
+    public void notFound() {
+        ResourceBundle parent = ResourceBundle.getBundle("hierarchy.Names", new Locale("ru", "RU"));
+        assertEquals("ru RU", parent.getString("ruRUvologdaKey"));
     }
 }
