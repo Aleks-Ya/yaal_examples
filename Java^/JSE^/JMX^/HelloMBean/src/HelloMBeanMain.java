5eb85ef4c3f8e1dc80b9025ac149c828617cc912
Aleksey Yablokov <ya_al@bk.ru>
2015-03-03 15:07:47 +0300

    add HelloMBean project

---
+++ Java^/JSE^/JMX^/HelloMBean/src/HelloMBeanMain.java
@@ -0,0 +1,17 @@
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
+
+public class HelloMBeanMain {
+    public static void main(String[] args)
+            throws Exception {
+
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        ObjectName name = new ObjectName("com.example:type=Hello");
+        Hello mbean = new Hello();
+        mbs.registerMBean(mbean, name);
+
+        System.out.println("Waiting forever...");
+        Thread.sleep(Long.MAX_VALUE);
+    }
+}
