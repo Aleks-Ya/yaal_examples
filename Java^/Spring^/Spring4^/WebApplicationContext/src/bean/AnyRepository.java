1acf1a20ab371fd69c8e4c0cd415c22fb3d2714b
Aleksey Yablokov <ya_al@bk.ru>
2015-03-15 16:49:49 +0300

    chore: WebApplicationContext finished

--- Java^/Spring^/Spring4^/WebApplicationContext/src/bean/AnyRepository.java
+++ Java^/Spring^/Spring4^/WebApplicationContext/src/bean/AnyRepository.java
@@ -2,12 +2,20 @@ package bean;
 
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.PostConstruct;
+
 /**
  * Проверяем инициализацию бинов, отмеченный @Repository.
  */
 @Repository
 public class AnyRepository {
-    public AnyRepository() {
+
+    @PostConstruct
+    public void postConstruct() {
         System.out.println("============= REPOSITORY INSTANTIATED =============");
+    }
+
+    public String getRepositoryMessage() {
+        return "Welcome to the repo!";
     }
 }
