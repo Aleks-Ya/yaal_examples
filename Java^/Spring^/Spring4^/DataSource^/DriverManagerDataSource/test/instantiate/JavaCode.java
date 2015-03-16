99d7319ff9f2ad1302d87c9d46c11d50ad5bad21
Aleksey Yablokov <ya_al@bk.ru>
2015-03-16 07:14:49 +0300

    chore: new module DriverManagerDataSource

---
+++ Java^/Spring^/Spring4^/DataSource^/DriverManagerDataSource/test/instantiate/JavaCode.java
@@ -0,0 +1,31 @@
+package instantiate;
+
+import org.junit.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Инициализация DriverManagerDataSource в Java-коде.
+ */
+public class JavaCode {
+
+    @Test
+    public void test() throws SQLException {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:");
+        dataSource.setUsername("");
+        dataSource.setPassword("");
+
+        try (Connection conn = dataSource.getConnection();
+             Statement st = conn.createStatement()) {
+            st.executeUpdate("CREATE TABLE t1 (k INT PRIMARY KEY)");
+            assertEquals(1, st.executeUpdate("INSERT INTO t1 VALUES (3)"));
+        }
+    }
+}
---
+++ Java^/Spring^/Spring4^/DataSource^/DriverManagerDataSource/test/instantiate/Xml.java
@@ -0,0 +1,34 @@
+package instantiate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Инициализация DriverManagerDataSource в XML.
+ */
+@ContextConfiguration("classpath:context.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class Xml {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void test() throws SQLException {
+        try (Connection conn = dataSource.getConnection();
+             Statement st = conn.createStatement()) {
+            st.executeUpdate("CREATE TABLE t1 (k INT PRIMARY KEY)");
+            assertEquals(1, st.executeUpdate("INSERT INTO t1 VALUES (3)"));
+        }
+    }
+}
