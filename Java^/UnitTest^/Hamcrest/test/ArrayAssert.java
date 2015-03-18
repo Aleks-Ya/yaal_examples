0b2362bc97d49b6f9b82ec93cb66ad49b6bd6544
Aleksey Yablokov <ya_al@bk.ru>
2015-03-18 07:43:42 +0300

    chore: ArrayAssert

--- Java^/UnitTest^/Hamcrest/test/ArrayAssert.java
+++ Java^/UnitTest^/Hamcrest/test/ArrayAssert.java
@@ -1,3 +1,4 @@
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.arrayContaining;
@@ -12,12 +13,37 @@ import static org.junit.Assert.assertThat;
  */
 public class ArrayAssert {
     @Test
-    public void testName() {
+    public void objectArray() {
         final Integer[] arr = {45, 34, 89};
         assertThat(arr, hasItemInArray(34));
         assertThat(arr, arrayContaining(45, 34, 89));//в такой же последовательности
         assertThat(arr, arrayContainingInAnyOrder(89, 34, 45));
         assertThat(arr, arrayWithSize(3));
         assertThat(new Integer[]{}, emptyArray());
+    }
+
+    @Test
+    @Ignore("Падает с ошибкой из-за того, что Mockito тянет старый Harmcrest 1.1")
+    public void primitiveArray() {
+        final int[] arr = {45, 34, 89};
+        final Integer[] arrObj = toObjectArray(arr);
+        assertThat(arrObj, hasItemInArray(34));
+        assertThat(arrObj, arrayContaining(45, 34, 89));//в такой же последовательности
+        assertThat(arrObj, arrayContainingInAnyOrder(89, 34, 45));
+        assertThat(arrObj, arrayWithSize(3));
+        assertThat(new Integer[]{}, emptyArray());
+    }
+
+    /**
+     * Конвертация массива примитивов int[] в массив объектов Integer[]
+     * необходима, п.ч. интерфейс матчер использует Generics
+     * (не может принимать примитивы).
+     */
+    private Integer[] toObjectArray(int[] array) {
+        Integer[] result = new Integer[array.length];
+        for (int i = 0; i < array.length - 1; i++) {
+            result[i] = array[i];
+        }
+        return result;
     }
 }
