79d066579d0b418941dd7f9b35eb756530135396
Aleksey Yablokov <ya_al@bk.ru>
2015-03-18 07:45:24 +0300

    chore: BatchUpdate

---
+++ Java^/Spring^/Spring4^/JDBC^/JdbcTemplate/test/jdbc_template/BatchUpdate.java
@@ -0,0 +1,70 @@
+package jdbc_template;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.hamcrest.Matchers.arrayContaining;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Внесение ПАКЕТНЫХ изменений в БД с помощью JdbcTemplate.
+ */
+@ContextConfiguration("classpath:context.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class BatchUpdate {
+
+    @Autowired
+    private JdbcTemplate template;
+
+    @Test
+    public void insert() {
+        assertArray(template.batchUpdate(
+                "INSERT INTO names values(1, 'Vera')",
+                "INSERT INTO names values(2, 'Bagira')"
+        ), new int[]{1,1});
+    }
+
+    @Test
+    public void update() {
+        assertArray(template.batchUpdate(
+                "UPDATE names SET title='Jerry' WHERE title='Vera'",
+                "UPDATE names SET title='Boris' WHERE id=600"
+        ), new int[] {1,0});
+    }
+
+    @Test
+    public void delete() {
+        assertArray(template.batchUpdate(
+                "DELETE FROM names WHERE title='Mary'",
+                "DELETE FROM names WHERE title='John'"
+        ), new int[]{1,1});
+    }
+
+    /**
+     * Hamcrest невозможно использовать с массивами примитивов
+     * (см. example ArrayAssert).
+     */
+    private void assertArray(int[] actual, int[] expected) {
+        if (actual == null) {
+            throw new AssertionError("Actual array is null");
+        }
+        if (expected == null) {
+            throw new AssertionError("Expected array is null");
+        }
+        if (actual.length != expected.length) {
+            throw new AssertionError("Length mismatch: expected " + expected.length
+                    + " actual " + actual.length);
+        }
+        for (int i = 0; i < actual.length; i++) {
+            if (actual[i] != expected[i]) {
+                throw new AssertionError("Value mismatch: expected " + expected[i]
+                        + " actual " + actual[i]);
+            }
+        }
+    }
+}
