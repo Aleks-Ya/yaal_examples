8546f84e27ac7b59f8c292bade3a862d47d308d6
Aleksey Yablokov <ya_al@bk.ru>
2015-03-19 21:58:28 +0300

    chore: Hamcrest whole map assert

--- Java^/UnitTest^/Hamcrest/test/MapAssert.java
+++ Java^/UnitTest^/Hamcrest/test/MapAssert.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.hasValue;
@@ -14,10 +15,17 @@ import static org.junit.Assert.assertThat;
 public class MapAssert {
     @Test
     public void testName() throws Exception {
-        final Map<Integer, String> map = new HashMap<>();
-        map.put(100, "Dollars");
-        assertThat(map, hasEntry(100, "Dollars"));
-        assertThat(map, hasKey(100));
-        assertThat(map, hasValue("Dollars"));
+        final Map<Integer, String> actual = new HashMap<>();
+        actual.put(100, "Dollars");
+        actual.put(200, "Rouble");
+
+        final Map<Integer, String> expected = new HashMap<>();
+        expected.put(200, "Rouble");
+        expected.put(100, "Dollars");
+
+        assertThat(actual, hasEntry(100, "Dollars"));
+        assertThat(actual, hasKey(100));
+        assertThat(actual, hasValue("Dollars"));
+        assertThat(actual.entrySet(), equalTo(expected.entrySet()));
     }
 }
