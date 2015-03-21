98cc8bb689de9b231e7a429fd0a5314d08239c5c
Aleksey Yablokov <ya_al@bk.ru>
2015-03-21 11:10:48 +0300

    JodaTime

---
+++ Java^/Libs^/JodaTime/test/Initialize.java
@@ -0,0 +1,13 @@
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+/**
+ * Создаем объект с заданными датой и временем.
+ */
+public class Initialize {
+
+    @Test
+    public void decrement() {
+        new DateTime(2015, 3, 25, 15, 45, 20);
+    }
+}
