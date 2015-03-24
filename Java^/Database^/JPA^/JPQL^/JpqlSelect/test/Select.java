e286c1d9237722fdb858ec09eadb14120a643639
Aleksey Yablokov <ya_al@bk.ru>
2015-03-24 17:53:03 +0300

    JpqlSelect runs on spring-test

--- Java^/Database^/JPA^/JPQL^/JpqlSelect/test/Select.java
+++ Java^/Database^/JPA^/JPQL^/JpqlSelect/test/Select.java
@@ -1,26 +1,26 @@
 import entity.CityEntity;
 import entity.RegionEntity;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.jpa.internal.EntityManagerFactoryImpl;
-import org.hibernate.service.ServiceRegistry;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-import javax.persistence.spi.PersistenceUnitTransactionType;
+import javax.transaction.Transactional;
 import java.util.List;
 
-public class JpqlSelect {
+@ContextConfiguration("classpath:context.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+public class Select {
 
-    private static EntityManager em;
+    @PersistenceContext
+    private EntityManager em;
 
-    public static void main(String[] args) {
-        initEntityManager();
+    @Test
+    public void main() {
         saveEntities();
         {
             System.out.println("\nSelect (1 table, one field):");
@@ -56,7 +56,6 @@ public class JpqlSelect {
             pringList(result);
         }
         em.close();
-        System.exit(0);//Почему приложение не завершается?
     }
 
     private static void pringList(List result) {
@@ -65,7 +64,7 @@ public class JpqlSelect {
         }
     }
 
-    private static void saveEntities() {
+    private void saveEntities() {
         RegionEntity region = new RegionEntity("Вологодская область");
         final long vologdaPopulation = 300000L;
         final String vologdaName = "Вологда";
@@ -79,44 +78,12 @@ public class JpqlSelect {
         long moscowPopulation = 12000000L;
         CityEntity city4 = new CityEntity("Москва", moscowPopulation, region2);
 
-        {
-            em.getTransaction().begin();
-            em.persist(region);
-            em.persist(region2);
-            em.persist(city1);
-            em.persist(city2);
-            em.persist(city3);
-            em.persist(city4);
-            em.flush();
-            em.getTransaction().commit();
-        }
-    }
-
-    private static void initEntityManager() {
-        Configuration configuration = getConfiguration();
-
-        StandardServiceRegistryBuilder srBuilder = new StandardServiceRegistryBuilder();
-        srBuilder.applySettings(configuration.getProperties());
-        ServiceRegistry serviceRegistry = srBuilder.build();
-
-        EntityManagerFactory emFactory = new EntityManagerFactoryImpl(
-                PersistenceUnitTransactionType.RESOURCE_LOCAL, true, null, configuration, serviceRegistry, null);
-
-        em = emFactory.createEntityManager();
-    }
-
-    private static Configuration getConfiguration() {
-        Configuration configuration = new Configuration();
-        configuration.setProperty("hibernate.connection.driver_class", "org.h2.Driver");
-        configuration.setProperty("hibernate.connection.url", "jdbc:h2:~/test");
-        configuration.setProperty("hibernate.connection.pool_size", "1");
-        configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-        configuration.setProperty("hibernate.cache.provider_class", "org.hibernate.cache.internal.NoCachingRegionFactory");
-        configuration.setProperty("hibernate.show_sql", "true");
-        configuration.setProperty("hibernate.hbm2ddl.auto", "create");
-        configuration.setProperty("hibernate.connection.autocommit", "false");
-        configuration.addAnnotatedClass(RegionEntity.class);
-        configuration.addAnnotatedClass(CityEntity.class);
-        return configuration;
+        em.persist(region);
+        em.persist(region2);
+        em.persist(city1);
+        em.persist(city2);
+        em.persist(city3);
+        em.persist(city4);
+        em.flush();
     }
 }
