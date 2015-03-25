14e26ddee230fc3b4485762695c92bac3c520b12
Aleksey Yablokov <ya_al@bk.ru>
2015-03-25 19:53:56 +0300

    refactor: DatabasePopulator: xml config move to xml package

--- Java^/Spring^/Spring4^/JDBC^/DataSource^/DatabasePopulator/test/xml/InitializeDataSource.java
+++ Java^/Spring^/Spring4^/JDBC^/DataSource^/DatabasePopulator/test/xml/InitializeDataSource.java
@@ -1,3 +1,5 @@
+package xml;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +18,7 @@ import static org.junit.Assert.assertTrue;
 /**
  * Одна встроенная БД.
  */
-@ContextConfiguration("classpath:context.xml")
+@ContextConfiguration("classpath:xml/context.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
 public class InitializeDataSource {
 
