87ebbe946f34175d72fccd507130343916deffba
Aleksey Yablokov <ya_al@bk.ru>
2015-03-26 12:27:42 +0300

    chore: new project Derby

---
+++ Java^/Database^/Derby/test/DerbyJdbcTest.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.Assert.assertEquals;
+
+public class DerbyJdbcTest {
+
+    @Test
+    public void main() throws SQLException, ClassNotFoundException {
+        //connect
+        Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+        Connection conn = DriverManager.getConnection(
+                "jdbc:derby:memory:db_name;create=true", "", "");
+
+        //insert
+        Statement update = conn.createStatement();
+        update.executeUpdate("CREATE TABLE numbers (numb INTEGER)");
+        update.executeUpdate("INSERT INTO numbers VALUES (3)");
+
+        //select
+        Statement select = conn.createStatement();
+        ResultSet resultSet = select.executeQuery("SELECT * FROM numbers");
+        if (resultSet.next()) {
+            assertEquals(3, resultSet.getInt(1));
+        }
+
+        //disconnect
+        conn.close();
+    }
+}
