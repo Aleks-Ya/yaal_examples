89f39d9b4abdc0f0262fb00bb40e8d6fe703f866
Aleksey Yablokov <ya_al@bk.ru>
2015-04-12 22:44:33 +0300

    Hamcrest

---
+++ Java+/UnitTest+/Hamcrest/test/StringAssert.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
+import static org.hamcrest.Matchers.equalToIgnoringWhiteSpace;
+import static org.hamcrest.Matchers.hasToString;
+import static org.hamcrest.Matchers.isEmptyString;
+import static org.hamcrest.Matchers.startsWith;
+import static org.hamcrest.Matchers.stringContainsInOrder;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Проверка массивов.
+ */
+public class StringAssert {
+    @Test
+    public void strings() {
+        final String str = "Людовико Эйнауди - Tu Sei";
+        assertThat(str, containsString("Эйнауди"));
+        assertThat(str, startsWith("Люд"));
+        assertThat(str, endsWith("Tu Sei"));
+        assertThat(23.4, hasToString("23.4"));
+        //не работает из-за того, что mockito тянет старый harmcrest 1.1
+//        assertThat(null, isEmptyOrNullString());
+        assertThat("", isEmptyString());
+        assertThat("Jamaica Kingston Portion", stringContainsInOrder(asList("Jam", "King", "Port")));
+        assertThat("Gangalee", equalToIgnoringCase("GaNgAlEe"));
+        assertThat("Gangalee Step", equalToIgnoringWhiteSpace("GaNgAlEe\nStep\t"));
+    }
+
+}
