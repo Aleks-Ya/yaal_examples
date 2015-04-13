07553c38f463e92447bfc0be43ea991ab1492502
Aleksey Yablokov <ya_al@bk.ru>
2015-04-14 00:41:09 +0300

    chore Hamcrest StringAssert

--- Java+/UnitTest+/Hamcrest/test/StringAssert.java
+++ Java+/UnitTest+/Hamcrest/test/StringAssert.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static java.util.Arrays.asList;
+import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.endsWith;
 import static org.hamcrest.Matchers.equalToIgnoringCase;
@@ -24,6 +25,7 @@ public class StringAssert {
         assertThat(23.4, hasToString("23.4"));
         //не работает из-за того, что mockito тянет старый harmcrest 1.1
 //        assertThat(null, isEmptyOrNullString());
+//        assertThat("Gangalee Step", anyOf(containsString("Ganga"), containsString("No")));
         assertThat("", isEmptyString());
         assertThat("Jamaica Kingston Portion", stringContainsInOrder(asList("Jam", "King", "Port")));
         assertThat("Gangalee", equalToIgnoringCase("GaNgAlEe"));
