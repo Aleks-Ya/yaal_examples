fe6db01454f1e52f2e1380720e82da3f6b136ca0
Yablokov Aleksey <ya_al@bk.ru>
2015-05-05 12:27:22 +0300

    Java8 forEach

---
+++ Java+/JSE+/Java8/test/stream/ForEach.java
@@ -0,0 +1,57 @@
+package stream;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasItems;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Использование конструкции forEach().
+ */
+public class ForEach {
+
+    @Test
+    public void methodReference() {
+        List<Integer> source = Arrays.asList(-1, 0, 1);
+        source.forEach(System.out::println);
+    }
+
+    @Test
+    public void var() {
+        List<Storage> source = Arrays.asList(Storage.MINUS_ONE, Storage.ZERO, Storage.ONE);
+        source.forEach(n -> n.num++);
+        assertThat(source, hasItems(Storage.ZERO, Storage.ONE, Storage.TWO));
+    }
+
+    static class Storage {
+        public static final Storage MINUS_ONE = new Storage(-1);
+        public static final Storage ZERO = new Storage(0);
+        public static final Storage ONE = new Storage(0);
+        public static final Storage TWO = new Storage(0);
+
+        public Storage(int num) {
+            this.num = num;
+        }
+
+        public int num;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Storage storage = (Storage) o;
+
+            return num == storage.num;
+
+        }
+
+        @Override
+        public int hashCode() {
+            return num;
+        }
+    }
+}
