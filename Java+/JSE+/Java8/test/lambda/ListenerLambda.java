2a975666afc8a345ad01071abbf79de5c74af32a
Yablokov Aleksey <ya_al@bk.ru>
2015-05-05 18:41:26 +0300

    Java8 lambda

---
+++ Java+/JSE+/Java8/test/lambda/ListenerLambda.java
@@ -0,0 +1,24 @@
+package lambda;
+
+import org.junit.Test;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Использование лямбды для реализации интерфейса ActionListener.
+ */
+public class ListenerLambda {
+
+    @Test
+    public void listener() {
+        JButton testButton = new JButton("Test Button");
+        testButton.addActionListener(e -> System.out.println("Click Detected by Lambda Listner"));
+
+        JFrame frame = new JFrame("Listener Test");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(testButton, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
