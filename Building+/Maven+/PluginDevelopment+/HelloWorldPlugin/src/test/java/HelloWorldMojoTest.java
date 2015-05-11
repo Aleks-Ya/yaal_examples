c44babb646a18c8c0015bb5d1b2ee3e08e977ef3
Aleksey Yablokov <ya_al@bk.ru>
2015-05-11 16:00:54 +0300

    HelloWorldMavenPlugin test works

--- Building+/Maven+/PluginDevelopment+/HelloWorldPlugin/src/test/java/HelloWorldMojoTest.java
+++ Building+/Maven+/PluginDevelopment+/HelloWorldPlugin/src/test/java/HelloWorldMojoTest.java
@@ -1,22 +1,50 @@
-import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.apache.maven.plugin.testing.MojoRule;
+import org.apache.maven.plugin.testing.WithoutMojo;
+import org.junit.Rule;
+import org.junit.Test;
 
 import java.io.File;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 /**
+ * Перед запуском теста необходимо поместить тестируемый плагин в локальный репозитарий:
+ * mvn -DskipTests clean install
+ *
  * @author yablokov a.
  */
-public class HelloWorldMojoTest extends AbstractMojoTestCase {
-    protected void setUp() throws Exception {
-        // required for mojo lookups to work
-        super.setUp();
-    }
+public class HelloWorldMojoTest {
+    @Rule
+    public MojoRule rule = new MojoRule() {
+        @Override
+        protected void before() throws Throwable {
+        }
 
+        @Override
+        protected void after() {
+        }
+    };
+
+    @Test
     public void testMojoGoal() throws Exception {
-        File testPom = new File(getBasedir(),
-                "src/test/resources/basic-test-plugin-config.xml");
-        assertTrue(testPom.exists());
-        HelloWorldMavenPlugin mojo = (HelloWorldMavenPlugin) lookupMojo("hello", testPom);
+        String pomXmlPath = HelloWorldMojoTest.class.getResource(
+                "basic-test-plugin-config.xml").getFile();
+        pomXmlPath = pomXmlPath.replaceAll("%2b", "+");
+        File testPom = new File(pomXmlPath);
+        assertTrue("Not exists: " + testPom.getAbsolutePath(), testPom.exists());
+        HelloWorldMavenPlugin mojo = (HelloWorldMavenPlugin) rule.lookupMojo("hello", testPom);
 
         assertNotNull(mojo);
+
+        mojo.execute();
+    }
+
+    /**
+     * Тестовый метод без MojoRule.
+     */
+    @WithoutMojo
+    @Test
+    public void testSomethingWhichDoesNotNeedTheMojoAndProbablyShouldBeExtractedIntoANewClassOfItsOwn() {
     }
 }
