107df8e839cf77f301a24995e9b79f493ebe53ed
Yablokov Aleksey <ya_al@bk.ru>
2015-06-01 12:59:11 +0300

    ClassByElement

---
+++ Java+/JSE+/Core+/src/reflection/annotation_processor/class_by_element/MyAnnotation.java
@@ -0,0 +1,8 @@
+package reflection.annotation_processor.class_by_element;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+public @interface MyAnnotation {
+}
