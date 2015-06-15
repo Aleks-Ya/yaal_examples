c934b79ef644e64411fb1ad4f1071d50e15dac57
Yablokov Aleksey <ya_al@bk.ru>
2015-06-15 12:20:08 +0300

    AOP in Spring config

---
+++ Java+/Spring+/Spring4+/AOP/src/spring_context/MessageDecorator.java
@@ -0,0 +1,14 @@
+package spring_context;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+public class MessageDecorator implements MethodInterceptor {
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        System.out.print("Hello ");
+        Object retVal = invocation.proceed();
+        System.out.println("!");
+        return retVal;
+    }
+}
