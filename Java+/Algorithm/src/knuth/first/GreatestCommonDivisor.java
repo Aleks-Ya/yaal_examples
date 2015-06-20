cbbf2c2b17350b6834245bcf9d321a302fe6bd88
Yablokov Aleksey <ya_al@bk.ru>
2015-06-20 21:50:31 +0300

    add Algorithm module

---
+++ Java+/Algorithm/src/knuth/first/GreatestCommonDivisor.java
@@ -0,0 +1,23 @@
+package knuth.first;
+
+/**
+ * Алгоритм Эвклида (стр. 28).
+ *
+ * @author yablokov a.
+ */
+public class GreatestCommonDivisor {
+    public static int calculate(int m, int n) {
+        assert (m > 0);
+        assert (n > 0);
+
+        int reminder;
+        int r;
+        do {
+            r = m % n;
+            reminder = m = n;
+            n = r;
+        } while (r != 0);
+
+        return reminder;
+    }
+}
---
+++ Java+/Algorithm/test/knuth/first/GreatestCommonDivisorTest.java
@@ -0,0 +1,22 @@
+package knuth.first;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author yablokov a.
+ */
+public class GreatestCommonDivisorTest {
+
+    @Test
+    public void testCalculate() {
+        assertEquals(50, GreatestCommonDivisor.calculate(100, 50));
+        assertEquals(50, GreatestCommonDivisor.calculate(50, 100));
+        assertEquals(2, GreatestCommonDivisor.calculate(10, 4));
+        assertEquals(2, GreatestCommonDivisor.calculate(4, 10));
+        assertEquals(1, GreatestCommonDivisor.calculate(10, 3));
+        assertEquals(1, GreatestCommonDivisor.calculate(3, 10));
+        assertEquals(17, GreatestCommonDivisor.calculate(119, 544));
+    }
+}
