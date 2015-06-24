a875518111cb20082f5f2b5af5761d45100aa80d
Yablokov Aleksey <ya_al@bk.ru>
2015-06-24 17:18:54 +0300

    Generics MultiTypeContainer

---
+++ Java+/JSE+/Core/test/generics/MultiTypeContainer.java
@@ -0,0 +1,42 @@
+package generics;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * Неоднородный контейнер.
+ * Пример из книги Дж. Блоха "Java. Эффективное програмирование" (статья 29).
+ */
+public class MultiTypeContainer {
+
+    @Test
+    public void testName() {
+        Integer integer = 1;
+        String string = "string";
+
+        Favorites c = new Favorites();
+        c.putFavorite(Integer.class, integer);
+        c.putFavorite(String.class, string);
+
+        assertEquals(integer, c.getFavorite(Integer.class));
+        assertEquals(string, c.getFavorite(String.class));
+    }
+
+    static class Favorites {
+        private Map<Class<?>, Object> favorites = new HashMap<>();
+
+        public <T> void putFavorite(Class<T> type, T instance) {
+            assert (type != null);
+            favorites.put(type, instance);
+        }
+
+        public <T> T getFavorite(Class<T> type) {
+            return type.cast(favorites.get(type));
+        }
+    }
+}
