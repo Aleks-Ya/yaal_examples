20e85d5246d4e1deb81d6cac5e1efc60154b715b
Yablokov Aleksey <ya_al@bk.ru>
2015-06-24 16:55:51 +0300

    SuppressWarning

---
+++ Java+/JSE+/Core/test/suppress_warnings/SuppressMethod.java
@@ -0,0 +1,38 @@
+package suppress_warnings;
+
+/**
+ * На поле, конструкторе, методе, параметре метода и локальной переменной.
+ */
+public class SuppressMethod {
+
+    /**
+     * Поле.
+     */
+    @SuppressWarnings("unused")
+    int num;
+
+    /**
+     * Конструктор.
+     */
+    @SuppressWarnings("unused")
+    SuppressMethod() {
+    }
+
+    /**
+     * Метод.
+     */
+    @SuppressWarnings("unused")
+    void doNothing() {
+        parameter(null);
+    }
+
+    /**
+     * Параметр метода.
+     * Локальная переменная.
+     */
+    void parameter(@SuppressWarnings("unused") String unused) {
+        @SuppressWarnings("unused")
+        long a = 3;
+    }
+
+}
