c6f339546ec220ee3e23e11bb8b2e49e94a143d2
Yablokov Aleksey <ya_al@bk.ru>
2015-07-10 12:20:04 +0300

    All classes in package

--- Java+/Libs+/Reflections/test/subtype/BySubTypeScannerTest.java
+++ Java+/Libs+/Reflections/test/subtype/BySubTypeScannerTest.java
@@ -22,4 +22,18 @@ public class BySubTypeScannerTest {
         System.out.println();
     }
 
+    /**
+     * Не работает. См. Guava (com.google.common.reflect.ClassPath)
+     */
+    @Test
+    public void allClassesInPackage() {
+        System.out.println("allClassesInPackage:");
+        Reflections reflections = new Reflections(BySubTypeScannerTest.class.getPackage(), new SubTypesScanner(false));
+        Set<Class<?>> classes = reflections.getSubTypesOf(Object.class);
+        for (Class<?> clazz : classes) {
+            System.out.println(clazz.getName());
+        }
+        System.out.println();
+    }
+
 }
