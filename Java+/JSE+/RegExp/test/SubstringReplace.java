746fab940e2add2b0c43f037472550eaa204e5a7
Yablokov Aleksey <ya_al@bk.ru>
2015-08-02 15:29:08 +0300

    SubstringReplace

--- Java+/JSE+/RegExp/test/SubstringReplace.java
+++ Java+/JSE+/RegExp/test/SubstringReplace.java
@@ -3,7 +3,9 @@ import org.junit.Test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 /**
  * Замена вхождения регулярного выражения в строку подстрокой.
@@ -25,5 +27,12 @@ public class SubstringReplace {
     public void realization2() {
         assertEquals("concert Tankian ludovico einaudi today ", SOURCE.replaceFirst(REGEX, REPLACER));
         assertEquals("concert Tankian ludovico Tankian today ", SOURCE.replaceAll(REGEX, REPLACER));
+    }
+
+    @Test
+    public void doubleDollar() {
+        assertThat("$$".replaceAll("\\$", "A"), equalTo("AA"));
+        assertThat("$$".replaceAll("\\${2}", "A"), equalTo("A"));
+        assertThat("$$".replaceAll("\\${2}", "\\$"), equalTo("$"));
     }
 }
