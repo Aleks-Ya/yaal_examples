33fae3fe4b61cec3969be324bc77d35c83b7a0bd
Aleksey Yablokov <ya_al@bk.ru>
2015-08-11 08:19:52 +0300

    FindLighterCoin

--- Java+/Algorithm/test/intuit/combinatorics/FindLighterCoin.java
+++ Java+/Algorithm/test/intuit/combinatorics/FindLighterCoin.java
@@ -4,6 +4,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.util.Arrays;
+
 /**
  * Алгоритм поиска легкой монеты.
  */
@@ -12,7 +14,45 @@ public class FindLighterCoin {
         if (coins.length < 2) {
             throw new IllegalArgumentException("Количество элементов должно быть больше 1");
         }
-        return 0;
+        boolean isEven = coins.length % 2 == 0;
+        Integer iExcessCoin;
+        int[] left;
+        int[] right;
+        int iLastCoin;
+        if (isEven) {
+            iExcessCoin = null;
+            iLastCoin = coins.length / 2;
+        } else {
+            iExcessCoin = coins.length - 1;
+            iLastCoin = (coins.length - 1) / 2;
+
+        }
+        left = Arrays.copyOfRange(coins, 0, iLastCoin / 2 - 1);
+        right = Arrays.copyOfRange(coins, iLastCoin / 2, coins.length - 1);
+        while (left.length > 1) {
+            assert left.length == right.length;
+            int leftWeight = 0;
+            int rightWeight = 0;
+            for (int i = 0; i < left.length; i++) {
+                leftWeight += left[i];
+                rightWeight += right[i];
+            }
+            if (leftWeight < rightWeight) {
+                left = Arrays.copyOfRange(left, 0, left.length / 2 - 1);
+                right = Arrays.copyOfRange(left, left.length / 2, left.length - 1);
+            } else if (leftWeight > rightWeight) {
+                left = Arrays.copyOfRange(right, 0, right.length / 2 - 1);
+                right = Arrays.copyOfRange(right, right.length / 2, right.length - 1);
+            } else {
+                if (iExcessCoin != null) {
+                    return iExcessCoin;
+                } else {
+                    throw new IllegalArgumentException("Одна монета должна быть легче. " +
+                            "Остальные монеты дожны быть одинакового веса.");
+                }
+            }
+        }
+        return 0;//todo закончить
     }
 
     @Rule
