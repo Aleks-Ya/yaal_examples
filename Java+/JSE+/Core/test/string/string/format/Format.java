5c530c35ad2aff79210a43def0bd9d8a8afa2127
Yablokov Aleksey <ya_al@bk.ru>
2015-08-14 12:08:01 +0300

    Format

--- Java+/JSE+/Core/test/string/string/format/Format.java
+++ Java+/JSE+/Core/test/string/string/format/Format.java
@@ -2,33 +2,53 @@ package string.string.format;
 
 import org.junit.Test;
 
-import java.io.PrintStream;
+import static java.lang.String.format;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 /**
  * Примеры работы с методом String#format.
  */
 public class Format {
     @Test
-    public void main() {
-        PrintStream out = System.out;
+    public void string() {
+        assertThat(format("\nСтрока: %s%n", "Hello!"), equalTo("\nСтрока: Hello!\n"));
 
-        out.printf("Null: %s,%d,%f%n", null, null, null);
-        out.printf("Порядок аргументов: %2$s %1$s %n", "2nd", "1st");
+        assertThat(format("Перенос строки 1: %n"), equalTo("Перенос строки 1: \n"));
+        assertThat(format("Перенос строки 2: \n"), equalTo("Перенос строки 2: \n"));
 
+        // Знакоместа для строк:
+        assertThat(format("%30s%n", "The first string."), equalTo("             The first string.\n"));
+        assertThat(format("%30s%n", "The 2nd string!"), equalTo("               The 2nd string!\n"));
 
-        out.printf("\nСтрока: %s%n", "Hello!");
-        out.printf("Перенос строки 1: %n");
-        out.printf("Перенос строки 2: \n");
+    }
 
-        out.println("\nЗнакоместа для строк:");
-        out.printf("%50s%n", "The first string.");
-        out.printf("%50s%n", "The 2nd string!");
+    @Test
+    public void argOrder() {
+        assertThat(format("Порядок аргументов: %2$s %1$s %n", "2nd", "1st"), equalTo("Порядок аргументов: 1st 2nd \n"));
+    }
 
-        out.printf("\nЦелое число: %d%n", 300);
-        out.printf("Знакоместа в начале числа: %20d %15f %n", 300, 9.8);
-        out.printf("Ведущие нули: %05d %010f %n", 300, 9.8);
-        out.printf("Вещественное число: %f%n", 3.14);
-        out.printf("Десятичные разряды: %.2f%n", 3.14);
-        out.printf("Разделители групп разрядов: %,d%n", 1000000);
+    @Test
+    public void nulls() {
+        //noinspection MalformedFormatString
+        assertThat(format("Null: %s,%d,%f%n", null, null, null), equalTo("Null: null,null,null\n"));
+    }
+
+    @Test
+    public void integer() {
+        assertThat(format("\nЦелое число: %d%n", 300), equalTo("\nЦелое число: 300\n"));
+        assertThat(format("Ведущие нули: %05d %n", 300), equalTo("Ведущие нули: 00300 \n"));
+        assertThat(format("Знакоместа в начале числа: %20d %n", 300), equalTo("Знакоместа в начале числа:                  300 \n"));
+    }
+
+    @Test
+    public void floating() {
+        assertThat(format("Знакоместа в начале числа: %15f %n", 9.8), equalTo("Знакоместа в начале числа:        9.800000 \n"));
+        assertThat(format("Ведущие нули: %010f %n", 9.8), equalTo("Ведущие нули: 009.800000 \n"));
+        assertThat(format("Ведущий ноль и десятичные разряды : %01.2f %n", .14), equalTo("Ведущий ноль и десятичные разряды : 0.14 \n"));
+        assertThat(format("Вещественное число: %f%n", 3.14), equalTo("Вещественное число: 3.140000\n"));
+        assertThat(format("Десятичные разряды: %.2f%n", 3.14), equalTo("Десятичные разряды: 3.14\n"));
+        assertThat(format("Разделители групп разрядов: %,d%n", 1000000), equalTo("Разделители групп разрядов: 1,000,000\n"));
+
     }
 }
