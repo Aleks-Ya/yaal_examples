6a22005041fca80b80ba5f91fe777dd1fddb0cfd
Yablokov Aleksey <ya_al@bk.ru>
2015-09-08 16:29:59 +0300

    ExecutableJar

---
+++ Building+/Maven+/JarPlugin+/ExecutableJar/src/myjar/Main.java
@@ -0,0 +1,7 @@
+package myjar;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("myjar.Main: Hello!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!111");
+    }
+}
