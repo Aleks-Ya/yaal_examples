9f7426c0ad31077664c60f2e624a491f2abbb516
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2015-10-07 19:48:28 +0300

    LoadClassFromJarTest

---
+++ Java+/JSE+/Core/test/class_loader/LoadClassFromJarTest.java
@@ -0,0 +1,28 @@
+package class_loader;
+
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Динамическая загрузка класса из jar.
+ */
+public class LoadClassFromJarTest {
+    @Test
+    public void loadClass()
+            throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+
+        URL plugin = getClass().getResource("StringPlugin.jar");
+        URLClassLoader loader = new URLClassLoader(new URL[]{plugin}, getClass().getClassLoader());
+        Class<?> clazz = Class.forName("class_loader.StringInversion", true, loader);
+        Method method = clazz.getDeclaredMethod("invertString", String.class);
+        String actual = (String) method.invoke(null, "abc");
+        assertThat(actual, equalTo("cba"));
+    }
+}
