7b453ae9096e75461aac7d682155bce96a088a7d
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2015-10-08 16:05:30 +0300

    Convert

--- Java+/JSE+/Java8/test/time_api/Convert.java
+++ Java+/JSE+/Java8/test/time_api/Convert.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.time.temporal.ChronoField;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
@@ -23,21 +24,22 @@ public class Convert {
     public void localDateToLocalDateTime() {
         LocalDate date = LocalDate.parse("2015-03-25");
         LocalTime time = LocalTime.MIDNIGHT;
-
         LocalDateTime dateTime = LocalDateTime.of(date, time);
-
         assertThat(dateTime.toString(), equalTo("2015-03-25T00:00"));
     }
 
     @Test
-    public void localDateToInstant() {
-        LocalDate date = LocalDate.parse("2015-03-25");
-        LocalTime time = LocalTime.MIDNIGHT;
+    public void localDateTimeToLocalDate() {
+        LocalDateTime dateTime = LocalDateTime.parse("2015-03-25T10:15:30");
+        LocalDate date = LocalDate.ofEpochDay(dateTime.getLong(ChronoField.EPOCH_DAY));
+        assertThat(date.toString(), equalTo("2015-03-25"));
+    }
 
-        LocalDateTime dateTime = LocalDateTime.of(date, time);
+    @Test
+    public void localDateTimeToInstant() {
+        LocalDateTime dateTime = LocalDateTime.parse("2015-03-25T10:15:30");
         Instant instant = dateTime.toInstant(ZoneOffset.UTC);
-
-        assertThat(instant.toString(), equalTo("2015-03-25T00:00:00Z"));
+        assertThat(instant.toString(), equalTo("2015-03-25T10:15:30Z"));
     }
 
     @Test
@@ -45,5 +47,13 @@ public class Convert {
         Instant instant = Instant.parse("2007-03-25T10:15:30.00Z");
         LocalDateTime dateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
         assertThat(dateTime.toString(), equalTo("2007-03-25T14:15:30"));
+    }
+
+    @Test
+    public void instantToLocalDate() {
+        Instant instant = Instant.parse("2007-03-25T10:15:30.00Z");
+        LocalDateTime dateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+        LocalDate date = LocalDate.ofEpochDay(dateTime.getLong(ChronoField.EPOCH_DAY));
+        assertThat(date.toString(), equalTo("2007-03-25"));
     }
 }
