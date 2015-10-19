625ae0a606602f799adfa4db924d0bb08fff05f7
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2015-10-19 13:20:55 +0300

    LookAround cleanup

--- Java+/JSE+/RegExp/test/LookAround.java
+++ Java+/JSE+/RegExp/test/LookAround.java
@@ -9,52 +9,44 @@ import static org.junit.Assert.assertTrue;
  * Примеры позиционной проверки.
  */
 public class LookAround {
+    private static final String NO = "obsolete obsolete";
+    private static final String BEGIN = "hede obsolete";
+    private static final String MIDDLE = "obsolete hede obsolete";
+    private static final String END = "obsolete hede";
 
-    /**
-     * Строка не должна заканчиваться словом "fail".
-     */
     @Test
     public void notEndWith() {
         Pattern p = Pattern.compile("^.*(?<!hede)$");
-        assertTrue(p.matcher("hede obsolete").matches());
-        assertTrue(p.matcher("obsolete hede obsolete").matches());
-        assertFalse(p.matcher("obsolete hede").matches());
+        assertTrue(p.matcher(NO).matches());
+        assertTrue(p.matcher(BEGIN).matches());
+        assertTrue(p.matcher(MIDDLE).matches());
+        assertFalse(p.matcher(END).matches());
     }
 
-    /**
-     * Проверяем, что URL заканчивается строкой "/success" или ."/success/"
-     * (позитивная опережающая проверка).
-     */
     @Test
     public void endWith() {
-        Pattern p = Pattern.compile(".*(?=/success/?$).*");
-        assertFalse(p.matcher("/app/check").matches());
-        assertFalse(p.matcher("/app/success/check").matches());
-        assertTrue(p.matcher("/app/check/success").matches());
-        assertTrue(p.matcher("/app/check/success/").matches());
+        Pattern p = Pattern.compile(".*(?<=hede$)");
+        assertFalse(p.matcher(NO).matches());
+        assertFalse(p.matcher(BEGIN).matches());
+        assertFalse(p.matcher(MIDDLE).matches());
+        assertTrue(p.matcher(END).matches());
     }
 
-    /**
-     * Строка не должна содержать слово "fail".
-     */
     @Test
     public void notContain() {
-        Pattern p = Pattern.compile("^((?!fail).)*$");
-        assertTrue(p.matcher("/app/check/success").matches());
-        assertFalse(p.matcher("/fail/check").matches());
-        assertFalse(p.matcher("fail/app/success/check").matches());
-        assertFalse(p.matcher("/app/success/fail").matches());
+        Pattern p = Pattern.compile("^((?!hede).)*$");
+        assertTrue(p.matcher(NO).matches());
+        assertFalse(p.matcher(BEGIN).matches());
+        assertFalse(p.matcher(MIDDLE).matches());
+        assertFalse(p.matcher(END).matches());
     }
 
-    /**
-     * Строка не должна начинаться со слова "fail".
-     */
     @Test
     public void notStartWith() {
-        Pattern p = Pattern.compile("^(?!fail).*$");
-        assertTrue(p.matcher("/app/check/success").matches());
-        assertTrue(p.matcher("/fail/check").matches());
-        assertTrue(p.matcher("/app/success/fail").matches());
-        assertFalse(p.matcher("fail/app/success/check").matches());
+        Pattern p = Pattern.compile("^(?!hede).*$");
+        assertTrue(p.matcher(NO).matches());
+        assertFalse(p.matcher(BEGIN).matches());
+        assertTrue(p.matcher(MIDDLE).matches());
+        assertTrue(p.matcher(END).matches());
     }
 }
