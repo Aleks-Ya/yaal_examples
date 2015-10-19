92a14f47c14272014a98f5c3dbbebb1764457f6f
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2015-10-19 12:24:48 +0300

    Look around

---
+++ Java+/JSE+/RegExp/test/LookAround.java
@@ -0,0 +1,63 @@
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.regex.Pattern;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Примеры позиционной проверки.
+ */
+public class LookAround {
+
+    /**
+     * Строка не должна начинаться со слова "fail".
+     */
+    @Test
+    @Ignore("Не работает, сука")
+    public void notEndWith() {
+        Pattern p = Pattern.compile("^.*(?!fail)$");
+        assertTrue(p.matcher("/app/check/success").matches());
+        assertTrue(p.matcher("/fail/check").matches());
+        assertTrue(p.matcher("fail/app/success/check").matches());
+        assertFalse(p.matcher("/app/success/fail").matches());
+    }
+
+    /**
+     * Проверяем, что URL заканчивается строкой "/success" или ."/success/"
+     * (позитивная опережающая проверка).
+     */
+    @Test
+    public void endWith() {
+        Pattern p = Pattern.compile(".*(?=/success/?$).*");
+        assertFalse(p.matcher("/app/check").matches());
+        assertFalse(p.matcher("/app/success/check").matches());
+        assertTrue(p.matcher("/app/check/success").matches());
+        assertTrue(p.matcher("/app/check/success/").matches());
+    }
+
+    /**
+     * Строка не должна содержать слово "fail".
+     */
+    @Test
+    public void notContain() {
+        Pattern p = Pattern.compile("^((?!fail).)*$");
+        assertTrue(p.matcher("/app/check/success").matches());
+        assertFalse(p.matcher("/fail/check").matches());
+        assertFalse(p.matcher("fail/app/success/check").matches());
+        assertFalse(p.matcher("/app/success/fail").matches());
+    }
+
+    /**
+     * Строка не должна начинаться со слова "fail".
+     */
+    @Test
+    public void notStartWith() {
+        Pattern p = Pattern.compile("^(?!fail).*$");
+        assertTrue(p.matcher("/app/check/success").matches());
+        assertTrue(p.matcher("/fail/check").matches());
+        assertTrue(p.matcher("/app/success/fail").matches());
+        assertFalse(p.matcher("fail/app/success/check").matches());
+    }
+}
