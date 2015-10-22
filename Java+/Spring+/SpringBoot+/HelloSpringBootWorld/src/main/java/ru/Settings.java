abc67430de99a96d12da0f24b6f0990f36834f0e
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2015-10-22 19:02:43 +0300

    Spring Boot @NotEmpty

--- Java+/Spring+/SpringBoot+/HelloSpringBootWorld/src/main/java/ru/Settings.java
+++ Java+/Spring+/SpringBoot+/HelloSpringBootWorld/src/main/java/ru/Settings.java
@@ -1,11 +1,13 @@
 package ru;
 
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "configuration")
 public class Settings {
+    @NotEmpty
     private String message;
 
     public String getMessage() {
