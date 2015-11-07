4784982f86edf1c466f6d1ebe6923826ffdb8d54
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2015-11-07 16:32:33 +0300

    add RxJava

---
+++ Java+/Libs+/Reactive^/RxJava/test/Operators.java
@@ -0,0 +1,63 @@
+import org.junit.Test;
+import rx.Observable;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Использование операторов RxJava.
+ */
+public class Operators {
+    @Test
+    public void map() {
+        Observable.just("Hello, world!")
+                .map(String::hashCode)
+                .map(i -> Integer.toString(i))
+                .subscribe(System.out::println);
+    }
+
+    @Test
+    public void filter() {
+        Observable.just("A", "", null, "B")
+                .filter(s -> s != null)
+                .filter(s -> s.length() > 0)
+                .subscribe(System.out::println);
+    }
+
+    /**
+     * Преобразование потока коллекций в поток элементов этих коллекций.
+     */
+    @Test
+    public void flatMap() {
+        List<String> list1 = Arrays.asList("A", "B");
+        List<String> list2 = Arrays.asList("C", "D");
+        Observable.just(list1, list2)
+                .flatMap(Observable::from)
+                .subscribe(System.out::println);
+    }
+
+    /**
+     * Взять 2 элемента из Источника.
+     */
+    @Test
+    public void take() {
+        Observable.just("A", "B", "C")
+                .take(2)
+                .subscribe(System.out::println);
+    }
+
+    /**
+     * Выполнить действие над каждым элементом Источника.
+     */
+    @Test
+    public void doOnNext() {
+        StringBuilder sb = new StringBuilder();
+        Observable.just("A", "B")
+                .doOnNext(sb::append)
+                .subscribe(System.out::println);
+        assertThat(sb.toString(), equalTo("AB"));
+    }
+}
---
+++ Java+/Libs+/Reactive^/RxJava/test/RxJavaHelloWorld.java
@@ -0,0 +1,57 @@
+import org.junit.Test;
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action1;
+
+/**
+ * @author Yablokov Aleksey
+ */
+public class RxJavaHelloWorld {
+    @Test
+    public void superCompact() {
+        Observable.just("Hello, world!").subscribe(System.out::println);
+    }
+
+    @Test
+    public void compact() {
+        Observable<String> myObservable = Observable.just("Hello, world!");
+        @SuppressWarnings({"Convert2Lambda", "Anonymous2MethodRef"})
+        Action1<String> onNextAction = new Action1<String>() {
+            @Override
+            public void call(String s) {
+                System.out.println(s);
+            }
+        };
+        myObservable.subscribe(onNextAction);
+    }
+
+    @Test
+    public void boilerplate() {
+        Observable<String> myObservable = Observable.create(
+                new Observable.OnSubscribe<String>() {
+                    @Override
+                    public void call(Subscriber<? super String> sub) {
+                        sub.onNext("Hello, world!");
+                        sub.onCompleted();
+                    }
+                }
+        );
+
+        Subscriber<String> mySubscriber = new Subscriber<String>() {
+            @Override
+            public void onNext(String s) {
+                System.out.println(s);
+            }
+
+            @Override
+            public void onCompleted() {
+            }
+
+            @Override
+            public void onError(Throwable e) {
+            }
+        };
+
+        myObservable.subscribe(mySubscriber);
+    }
+}
