7a41470895fc9bfc85de07c40c524dc016e990b2
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2015-11-13 15:07:02 +0300

    StreamToPromise

---
+++ Java+/Libs+/Reactive+/Reactor/test/promise/StreamToPromise.java
@@ -0,0 +1,27 @@
+package promise;
+
+import org.junit.Test;
+import reactor.rx.Promise;
+import reactor.rx.Stream;
+import reactor.rx.Streams;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Преобразование Stream в Promise.
+ */
+public class StreamToPromise {
+    @Test
+    public void takeFirstElementOfStream() {
+        Stream<String> stream = Streams.just("a", "b");
+
+        Promise<String> promise1 = stream.next();
+        String value1 = promise1.get();
+        assertThat(value1, equalTo("a"));
+
+        Promise<String> promise2 = stream.next();
+        String value2 = promise2.get();
+        assertThat(value2, equalTo("a"));//все равно "a"
+    }
+}
