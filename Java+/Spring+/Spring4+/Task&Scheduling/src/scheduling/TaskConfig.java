eeb5907dd97129b4accde1593528bff9c9c3563f
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2015-11-25 10:26:38 +0300

    Task

---
+++ Java+/Spring+/Spring4+/Task&Scheduling/src/scheduling/TaskConfig.java
@@ -0,0 +1,28 @@
+package scheduling;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
+import org.springframework.scheduling.config.IntervalTask;
+import org.springframework.scheduling.config.Task;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@EnableScheduling
+public class TaskConfig {
+
+    @Bean
+    public Task task() {
+        return new Task(() -> System.out.println("TASK"));
+    }
+
+
+    @Bean
+    public Task task2() {
+        IntervalTask task = new IntervalTask(() -> System.out.println("TASK"), 1000);
+        return task;
+    }
+
+}
