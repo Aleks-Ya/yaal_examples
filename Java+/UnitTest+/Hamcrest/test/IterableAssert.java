45c5143fb8368285d64a090006c9574e5a8ee605
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2016-01-21 12:37:15 +0300

    every item

--- Java+/UnitTest+/Hamcrest/test/IterableAssert.java
+++ Java+/UnitTest+/Hamcrest/test/IterableAssert.java
@@ -8,10 +8,12 @@ import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.emptyIterable;
 import static org.hamcrest.Matchers.emptyIterableOf;
+import static org.hamcrest.Matchers.emptyString;
 import static org.hamcrest.Matchers.endsWith;
 import static org.hamcrest.Matchers.everyItem;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -20,13 +22,14 @@ import static org.junit.Assert.assertThat;
 public class IterableAssert {
 
     @Test
-    public void hasSizeTest() {
+    public void test() {
         final Iterable<String> iter = asList("Piter_", "SPB_", "Leningrad_", "Saint-Petersburg_");
         assertThat(iter, hasItem("SPB_"));
         assertThat(iter, hasItems("SPB_", "Leningrad_"));
         assertThat(iter, everyItem(endsWith("_")));
         assertThat(iter, contains("Piter_", "SPB_", "Leningrad_", "Saint-Petersburg_"));//все элементы
         assertThat(iter, containsInAnyOrder("Saint-Petersburg_", "Piter_", "SPB_", "Leningrad_"));//все элементы
+        assertThat(iter, everyItem(not(emptyString())));
         assertThat(new ArrayList<>(), emptyIterable());
         assertThat(new ArrayList<Random>(), emptyIterableOf(Random.class));
     }
