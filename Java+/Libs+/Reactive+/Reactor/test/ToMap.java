0159bd3160ce601f0affd7a3e1ab0d80d7008939
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2016-01-22 18:00:42 +0300

    ToMap

---
+++ Java+/Libs+/Reactive+/Reactor/test/ToMap.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+import reactor.rx.Stream;
+import reactor.rx.StreamUtils;
+import reactor.rx.Streams;
+
+import java.util.Map;
+
+/**
+ * Преобразование Stream в Map.
+ */
+public class ToMap {
+    @Test
+    public void test() {
+        Stream<String> stream = Streams.just("a", "b");
+        //непонятно
+    }
+
+
+}
