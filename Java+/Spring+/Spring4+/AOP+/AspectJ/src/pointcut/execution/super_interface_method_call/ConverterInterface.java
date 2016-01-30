3e8e8f39229735321ddefabe83099b1bdcffaae4
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2016-01-31 00:37:19 +0300

    call super interface method call

---
+++ Java+/Spring+/Spring4+/AOP+/AspectJ/src/pointcut/execution/super_interface_method_call/ConverterInterface.java
@@ -0,0 +1,4 @@
+package pointcut.execution.super_interface_method_call;
+
+interface ConverterInterface extends ConverterSuperInterface {
+}
---
+++ Java+/Spring+/Spring4+/AOP+/AspectJ/src/pointcut/execution/super_interface_method_call/ConverterSuperInterface.java
@@ -0,0 +1,5 @@
+package pointcut.execution.super_interface_method_call;
+
+interface ConverterSuperInterface {
+    String reverse(String s);
+}
