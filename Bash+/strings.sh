2a4c2710805c6964e13989f774ece4c81535cf70
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2016-03-16 14:33:15 +0300

    starts with

--- Bash+/strings.sh
+++ Bash+/strings.sh
@@ -3,3 +3,11 @@ str="feature/test-8"
 echo "Replace 8 with 9: ${str/8/9}"
 
 echo "Replace '/' with '-': ${str////-}"
+
+name="release/gvc-3"
+if [[ "${name}" == release/* ]]
+then
+    echo "'${name}' starts with 'release/'"
+else
+    echo "'${name}' doesn't starts with 'release/'"
+fi
