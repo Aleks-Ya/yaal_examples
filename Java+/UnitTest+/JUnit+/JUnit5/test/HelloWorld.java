35d61c9c79e15bb723f0c844e3e76834cd91dc95
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2016-03-18 23:20:38 +0300

    JUnit5

---
+++ Java+/UnitTest+/JUnit+/JUnit5/test/HelloWorld.java
@@ -0,0 +1,15 @@
+import org.junit.gen5.api.Test;
+
+import static org.junit.gen5.api.Assertions.assertTrue;
+
+/**
+ * Run with ConsoleRunner
+ */
+public class HelloWorld {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
