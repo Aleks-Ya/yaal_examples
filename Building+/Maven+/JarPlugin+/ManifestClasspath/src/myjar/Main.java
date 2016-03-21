04f7da467fbea69276e4cb524fecec468051267d
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2016-03-21 18:10:15 +0300

    create ManifestClasspath

---
+++ Building+/Maven+/JarPlugin+/ManifestClasspath/src/myjar/Main.java
@@ -0,0 +1,11 @@
+package myjar;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        logger.info("myjar.Main: Hello!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!111");
+    }
+}
--- Java+/Spring+/SpringBoot+/HelloSpringBootWorld/src/main/java/ru/YamlSettings.java
+++ Java+/Spring+/SpringBoot+/HelloSpringBootWorld/src/main/java/ru/YamlSettings.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties
+@ConfigurationProperties(prefix = "values")
 class YamlSettings {
     private Boolean enabled;
 
