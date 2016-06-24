293ebf89de5036df458be430ff77c666b934e99c
Yablokov Aleksey <ya_al@bk.ru>
2016-06-24 06:14:44 +0300

    VaadinComponents: add a combo box

--- Java+/JEE+/Vaadin+/VaadinComponents/src/main/java/com/example/MyVaadinUI.java
+++ Java+/JEE+/Vaadin+/VaadinComponents/src/main/java/com/example/MyVaadinUI.java
@@ -2,13 +2,26 @@ package com.example;
 
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 
 @SpringUI
 public class MyVaadinUI extends UI {
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        setContent(new Label("Hello! I'm the root UI!"));
+        VerticalLayout content = new VerticalLayout();
+
+        Label label = new Label("Hello! I'm the root UI!");
+
+        ComboBox comboBox = new ComboBox();
+        comboBox.addItem("Hello");
+        comboBox.addItem("Bye");
+
+        content.addComponent(label);
+        content.addComponent(comboBox);
+
+        setContent(content);
     }
 }
