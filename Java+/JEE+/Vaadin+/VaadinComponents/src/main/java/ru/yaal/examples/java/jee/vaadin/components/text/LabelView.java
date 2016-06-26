036a4790c48f8218d2ec182af4843592303d596d
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2016-06-26 11:43:53 +0300

    VaadinComponents: add LabelView

---
+++ Java+/JEE+/Vaadin+/VaadinComponents/src/main/java/ru/yaal/examples/java/jee/vaadin/components/text/LabelView.java
@@ -0,0 +1,17 @@
+package ru.yaal.examples.java.jee.vaadin.components.text;
+
+import com.vaadin.ui.Label;
+import ru.yaal.examples.java.jee.vaadin.AbstractVerticalView;
+
+@SuppressWarnings("unused")
+public class LabelView extends AbstractVerticalView {
+    public LabelView() {
+        Label shortLabel = new Label("A short label");
+
+        Label multiLineLabel = new Label("A multiline label");
+        multiLineLabel.setValue("Лукоморье — самая свободная, правдивая и открытая для всех вики-энциклопедия интернет-личностей, современной культуры, фольклора, субкультур и интернета, а также всего остального. Это альтернатива, свежий взгляд если хотите, традиционного устоявшегося и классического стандартного представления об интернет-энциклопедии. Заходите почаще на Lukomore и читайте в своё удовольствие, узнавайте что-то новое, здесь Вам действительно всегда очень рады. Если есть желание, то можете поделиться своими знаниями и информацией с другими читателями и участниками проекта на страницах сайта — дописывайте и переписывайте существующие статьи, смело создавайте новые, делитесь своим мнением на страницах обсуждений статей и в комментариях. Лукоморье так же как и Википедия располагается в международной не коммерческой доменной зоне ORG. ");
+
+        addComponent(shortLabel);
+        addComponent(multiLineLabel);
+    }
+}
