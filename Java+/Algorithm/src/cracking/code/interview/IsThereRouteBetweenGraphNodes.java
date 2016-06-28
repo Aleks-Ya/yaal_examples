635e6441982ef0984d2daba12a1a18cc4b30fba5
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2016-06-28 09:36:35 +0300

    IsThereRouteBetweenGraphNodes

---
+++ Java+/Algorithm/src/cracking/code/interview/IsThereRouteBetweenGraphNodes.java
@@ -0,0 +1,36 @@
+package cracking.code.interview;
+
+/**
+ * Given a directed graph, design an algorithm to find out whether there is a route
+ * between two nodes.
+ */
+class IsThereRouteBetweenGraphNodes {
+
+    static boolean hasPath(Node from, Node to) {
+        if (from == to) {
+            throw new IllegalArgumentException();
+        }
+        if (from.visited) {
+            return false;
+        }
+        from.visited = true;
+        if (from.paths == null) {
+            return false;
+        }
+        for (Node path : from.paths) {
+            boolean has = path == to || hasPath(path, to);
+            if (has) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static class Node {
+        Node() {
+        }
+
+        Node[] paths;
+        boolean visited;
+    }
+}
---
+++ Java+/Algorithm/test/cracking/code/interview/IsThereRouteBetweenGraphNodesTest.java
@@ -0,0 +1,46 @@
+package cracking.code.interview;
+
+import cracking.code.interview.IsThereRouteBetweenGraphNodes.Node;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class IsThereRouteBetweenGraphNodesTest {
+    private final Node D = new Node();
+    private final Node B = new Node();
+    private final Node C = new Node();
+    private final Node A = new Node();
+
+    public IsThereRouteBetweenGraphNodesTest() {
+        D.paths = new Node[]{C};
+        B.paths = new Node[]{D};
+        C.paths = new Node[]{D};
+        A.paths = new Node[]{B, C};
+    }
+
+    @Test
+    public void noPath() {
+        assertFalse(IsThereRouteBetweenGraphNodes.hasPath(D, A));
+    }
+
+    @Test
+    public void noPath2() {
+        assertFalse(IsThereRouteBetweenGraphNodes.hasPath(D, B));
+    }
+
+    @Test
+    public void hasPath() {
+        assertTrue(IsThereRouteBetweenGraphNodes.hasPath(A, D));
+    }
+
+    @Test
+    public void hasPath2() {
+        assertTrue(IsThereRouteBetweenGraphNodes.hasPath(B, D));
+    }
+
+    @Test
+    public void hasPath3() {
+        assertTrue(IsThereRouteBetweenGraphNodes.hasPath(B, C));
+    }
+}
