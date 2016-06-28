022d3e363d6564cc045add442f33ee14ead7d210
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2016-06-28 08:31:31 +0300

    BinaryTreeDepth

---
+++ Java+/Algorithm/src/cracking/code/interview/BinaryTreeDepth.java
@@ -0,0 +1,52 @@
+package cracking.code.interview;
+
+/**
+ * Implement a function to check if a binary tree is balanced. For the purposes of this
+ * question, a balanced tree is defined to be a tree such that the heights of the two
+ * subtrees of any node never differ by more than on.
+ */
+class BinaryTreeDepth {
+    private static final int NOT_BALANCED = -1;
+
+    static boolean isBalanced(Node root) {
+        return height(root) != NOT_BALANCED;
+    }
+
+    private static int height(Node node) {
+        int leftHeight = node.left != null ? height(node.left) : 0;
+        if (leftHeight == NOT_BALANCED) {
+            return NOT_BALANCED;
+        }
+        int rightHeight = node.right != null ? height(node.right) : 0;
+        if (rightHeight == NOT_BALANCED) {
+            return NOT_BALANCED;
+        }
+        int diff = Math.abs(leftHeight - rightHeight);
+        if (diff <= 1) {
+            return Math.max(leftHeight, rightHeight) + 1;
+        } else {
+            return NOT_BALANCED;
+        }
+    }
+
+    static class Node {
+        Node() {
+        }
+
+        Node(Node left, Node right) {
+            this.left = left;
+            this.right = right;
+        }
+
+        Node left;
+        Node right;
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    left +
+                    ", " + right +
+                    '}';
+        }
+    }
+}
---
+++ Java+/Algorithm/test/cracking/code/interview/BinaryTreeDepthTest.java
@@ -0,0 +1,40 @@
+package cracking.code.interview;
+
+import cracking.code.interview.BinaryTreeDepth.Node;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class BinaryTreeDepthTest {
+
+    @Test
+    public void emptyTree() {
+        Node root = new Node();
+        assertTrue(BinaryTreeDepth.isBalanced(root));
+    }
+
+    @Test
+    public void unbalancedTree() {
+        Node rightAA = new Node();
+        Node rightA = new Node(null, rightAA);
+        Node right = new Node(null, rightA);
+        Node left = new Node();
+        Node root = new Node(left, right);
+
+        assertFalse(BinaryTreeDepth.isBalanced(root));
+    }
+
+    @Test
+    public void balancedTree() {
+        Node rightAA = new Node();
+        Node rightA1 = new Node(null, rightAA);
+        Node rightA2 = new Node(null, rightAA);
+        Node right = new Node(rightA2, rightA1);
+        Node leftA = new Node();
+        Node left = new Node(leftA, null);
+        Node root = new Node(left, right);
+
+        assertTrue(BinaryTreeDepth.isBalanced(root));
+    }
+}
