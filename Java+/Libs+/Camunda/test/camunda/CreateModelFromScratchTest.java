ef88f87fdaad341bc7334a943a89a5cba932f995
Aleksey Yablokov <ya_al@bk.ru>
2016-07-13 19:08:32 +0300

    add Camunda

---
+++ Java+/Libs+/Camunda/test/camunda/CreateModelFromScratchTest.java
@@ -0,0 +1,65 @@
+package camunda;
+
+import org.camunda.bpm.model.bpmn.Bpmn;
+import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+import org.camunda.bpm.model.bpmn.instance.*;
+import org.camunda.bpm.model.bpmn.instance.Process;
+import org.junit.Test;
+
+import java.io.File;
+
+
+/**
+ * Создание модели в java-коде и сохранение ее в bpmn-файл.
+ */
+public class CreateModelFromScratchTest {
+
+    @Test
+    public void name() throws Exception {
+        BpmnModelInstance modelInstance = Bpmn.createEmptyModel();
+
+        Definitions definitions = modelInstance.newInstance(Definitions.class);
+        definitions.setTargetNamespace("http://camunda.org/examples");
+        modelInstance.setDefinitions(definitions);
+
+        // create the process
+        Process process = createElement(definitions, "process-with-one-task", Process.class, modelInstance);
+
+        // create start event, user task and end event
+        StartEvent startEvent = createElement(process, "start", StartEvent.class, modelInstance);
+        UserTask task1 = createElement(process, "task1", UserTask.class, modelInstance);
+        task1.setName("User Task");
+        EndEvent endEvent = createElement(process, "end", EndEvent.class, modelInstance);
+
+        // create the connections between the elements
+        createSequenceFlow(process, startEvent, task1, modelInstance);
+        createSequenceFlow(process, task1, endEvent, modelInstance);
+
+        // validate and write model to file
+        Bpmn.validateModel(modelInstance);
+        File file = File.createTempFile(getClass().getSimpleName() + "-", ".bpmn");
+        System.out.println(file.getAbsolutePath());
+        Bpmn.writeModelToFile(file, modelInstance);
+    }
+
+    private <T extends BpmnModelElementInstance> T createElement(BpmnModelElementInstance parentElement,
+                                                                 String id,
+                                                                 Class<T> elementClass,
+                                                                 BpmnModelInstance modelInstance) {
+        T element = modelInstance.newInstance(elementClass);
+        element.setAttributeValue("id", id, true);
+        parentElement.addChildElement(element);
+        return element;
+    }
+
+    private SequenceFlow createSequenceFlow(Process process, FlowNode from, FlowNode to, BpmnModelInstance modelInstance) {
+        String identifier = from.getId() + "-" + to.getId();
+        SequenceFlow sequenceFlow = createElement(process, identifier, SequenceFlow.class, modelInstance);
+        process.addChildElement(sequenceFlow);
+        sequenceFlow.setSource(from);
+        from.getOutgoing().add(sequenceFlow);
+        sequenceFlow.setTarget(to);
+        to.getIncoming().add(sequenceFlow);
+        return sequenceFlow;
+    }
+}
---
+++ Java+/Libs+/Camunda/test/camunda/FluentBuilderApiTest.java
@@ -0,0 +1,30 @@
+package camunda;
+
+import org.camunda.bpm.model.bpmn.Bpmn;
+import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+import org.junit.Test;
+
+import java.io.File;
+
+
+public class FluentBuilderApiTest {
+
+    @Test
+    public void name() throws Exception {
+        BpmnModelInstance modelInstance = Bpmn.createProcess()
+                .name("Example process")
+                .executable()
+                .startEvent()
+                .userTask()
+                .name("Some work to do")
+                .endEvent()
+                .done();
+
+        // validate and write model to file
+        Bpmn.validateModel(modelInstance);
+
+        File file = File.createTempFile("FluentBuilderApiTest-", ".bpmn");
+        System.out.println(file.getAbsolutePath());
+        Bpmn.writeModelToFile(file, modelInstance);
+    }
+}
