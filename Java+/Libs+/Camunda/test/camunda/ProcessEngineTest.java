b6479d53c64f877a7556aa1879d68ec3f0ab1d5d
Aleksey Yablokov <ya_al@bk.ru>
2016-07-14 13:06:08 +0300

    ProcessEngineTest

---
+++ Java+/Libs+/Camunda/test/camunda/ProcessEngineTest.java
@@ -0,0 +1,42 @@
+package camunda;
+
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngineConfiguration;
+import org.camunda.bpm.engine.RepositoryService;
+import org.camunda.bpm.engine.repository.DeploymentBuilder;
+import org.camunda.bpm.model.bpmn.Bpmn;
+import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class ProcessEngineTest {
+
+    @Test
+    @Ignore("not finished")
+    public void file() {
+        ProcessEngineConfiguration config = ProcessEngineConfiguration.createStandaloneInMemProcessEngineConfiguration();
+        ProcessEngine engine = config.buildProcessEngine();
+
+        RepositoryService repositoryService = engine.getRepositoryService();
+        DeploymentBuilder deployment = repositoryService.createDeployment();
+
+//        BpmnModelInstance modelInstance = Bpmn.createEmptyModel();
+//        Definitions definitions = modelInstance.newInstance(Definitions.class);
+//        definitions.setTargetNamespace("http://camunda.org/examples");
+//        modelInstance.setDefinitions(definitions);
+
+        BpmnModelInstance modelInstance = Bpmn.readModelFromStream(getClass().getResourceAsStream("read_model_from_file.bpmn"));
+        Bpmn.validateModel(modelInstance);
+
+        deployment.addModelInstance("model", modelInstance);
+
+//        repositoryService.activateProcessDefinitionById("process-with-one-task");
+        BpmnModelInstance modelFromDb = repositoryService.getBpmnModelInstance("model");
+//        repositoryService.getProcessDefinition("process-with-one-task");
+//        assertEquals(modelInstance, modelFromDb);
+
+    }
+}
