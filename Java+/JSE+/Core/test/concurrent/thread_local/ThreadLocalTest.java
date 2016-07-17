8ec7db8f7ebe050fde31ed4033c29aacca9e30bd
Aleksey Yablokov <ya_al@bk.ru>
2016-07-17 13:37:38 +0300

    ThreadLocalTest

---
+++ Java+/JSE+/Core/test/concurrent/thread_local/ThreadLocalTest.java
@@ -0,0 +1,37 @@
+package concurrent.thread_local;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadLocalTest {
+    private static Runnable work = new Runnable() {
+        private ThreadLocal<LocalTime> date = ThreadLocal.withInitial(() -> {
+            LocalTime initValue = LocalTime.of(12, 12);
+            System.out.printf("Init %s with %s\n", Thread.currentThread().getName(), initValue);
+            return initValue;
+        });
+
+        @Override
+        public void run() {
+            System.out.printf("Thread name=%s, init value=%s\n", Thread.currentThread().getName(), date.get());
+            date.set(LocalTime.now());
+            System.out.printf("Thread name=%s, value=%s\n", Thread.currentThread().getName(), date.get());
+        }
+    };
+
+    @Test
+    public void test() throws InterruptedException {
+        ExecutorService se = Executors.newFixedThreadPool(2);
+        se.submit(work);
+        TimeUnit.SECONDS.sleep(2);
+        se.submit(work);
+        se.shutdown();
+    }
+
+}
