e31907c1b2ec21953abacf0a114b036aa2bf192c
Aleksey Yablokov <ya_al@bk.ru>
2016-07-23 18:06:07 +0300

    refactor TextAreaView

--- Java+/JEE+/Vaadin+/VaadinComponents/src/main/java/ru/yaal/examples/java/jee/vaadin/components/text/TextAreaView.java
+++ Java+/JEE+/Vaadin+/VaadinComponents/src/main/java/ru/yaal/examples/java/jee/vaadin/components/text/TextAreaView.java
@@ -7,20 +7,29 @@ import ru.yaal.examples.java.jee.vaadin.EmptyEnterView;
 @SuppressWarnings("unused")
 public class TextAreaView extends VerticalLayout implements EmptyEnterView {
     public TextAreaView() {
-        TextArea editable = new TextArea("An editable area");
-        editable.setValue("A row\nAnother row\nYet another row");
+        addComponent(editable());
+        addComponent(readOnly());
+        addComponent(large());
+    }
 
-        TextArea readOnly = new TextArea("A readonly area");
-        readOnly.setValue("A row\nAnother row\nYet another row");
-        readOnly.setReadOnly(true);
-
+    private TextArea large() {
         TextArea large = new TextArea("Large text");
         large.setValue("Лукоморье — самая свободная, правдивая и открытая для всех вики-энциклопедия интернет-личностей, современной культуры, фольклора, субкультур и интернета, а также всего остального. Это альтернатива, свежий взгляд если хотите, традиционного устоявшегося и классического стандартного представления об интернет-энциклопедии. Заходите почаще на Lukomore и читайте в своё удовольствие, узнавайте что-то новое, здесь Вам действительно всегда очень рады. Если есть желание, то можете поделиться своими знаниями и информацией с другими читателями и участниками проекта на страницах сайта — дописывайте и переписывайте существующие статьи, смело создавайте новые, делитесь своим мнением на страницах обсуждений статей и в комментариях. Лукоморье так же как и Википедия располагается в международной не коммерческой доменной зоне ORG. ");
         large.setRows(7);
         large.setColumns(50);
+        return large;
+    }
 
-        addComponent(editable);
-        addComponent(readOnly);
-        addComponent(large);
+    private TextArea readOnly() {
+        TextArea readOnly = new TextArea("A readonly area");
+        readOnly.setValue("A row\nAnother row\nYet another row");
+        readOnly.setReadOnly(true);
+        return readOnly;
+    }
+
+    private TextArea editable() {
+        TextArea editable = new TextArea("An editable area");
+        editable.setValue("A row\nAnother row\nYet another row");
+        return editable;
     }
 }
