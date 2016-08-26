79bf11f6ea504687ecc9cd68cbf57d661b06ce05
Aleksey Yablokov <ya_al@bk.ru>
2016-08-27 00:13:43 +0300

    clean up SpringMvcSpringBoot

--- Java+/Spring+/Spring4+/SpringMVC+/SpringMvcSpringBoot/src/main/java/ru/yaal/spring/mvc/MvcApplication.java
+++ Java+/Spring+/Spring4+/SpringMVC+/SpringMvcSpringBoot/src/main/java/ru/yaal/spring/mvc/MvcApplication.java
@@ -1,14 +1,12 @@
 package ru.yaal.spring.mvc;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * The main class.
  */
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class MvcApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(MvcApplication.class, args);
