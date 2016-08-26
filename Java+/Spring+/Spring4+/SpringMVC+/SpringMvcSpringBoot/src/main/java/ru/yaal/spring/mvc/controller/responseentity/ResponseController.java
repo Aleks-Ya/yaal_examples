828778739747af7e102bd37739ae3ad7ddb6d3d3
Aleksei Iablokov <Aleksei_Iablokov@epam.com>
2016-08-26 15:19:54 +0300

    @ResponseBody

--- Java+/Spring+/Spring4+/SpringMVC+/SpringMvcSpringBoot/src/main/java/ru/yaal/spring/mvc/controller/responseentity/ResponseController.java
+++ Java+/Spring+/Spring4+/SpringMVC+/SpringMvcSpringBoot/src/main/java/ru/yaal/spring/mvc/controller/responseentity/ResponseController.java
@@ -7,17 +7,30 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Return a http-response instead of redirection to JSP.
  */
 @Controller
-public class ResponseEntityController {
-	@RequestMapping(value = "/respose_entity", method = RequestMethod.GET)
+public class ResponseController {
+	
+	/**
+	 * Using ResponseEntity.
+	 */
+	@RequestMapping(value = "/respose_entity")
 	public ResponseEntity<String> responseEntity() {
 		HttpHeaders headers = new HttpHeaders();
 		headers.put("my_header", Arrays.asList("my header value"));
 		return new ResponseEntity<>("Body", headers, HttpStatus.ACCEPTED);
+	}
+
+	/**
+	 * Using ResponseBody.
+	 */
+	@RequestMapping(value = "/respose_body")
+	@ResponseBody
+	public String responseBody() {
+		return "Response body";
 	}
 }
--- Java+/Spring+/Spring4+/SpringMVC+/SpringMvcSpringBoot/src/main/webapp/WEB-INF/jsp/home.jsp
+++ Java+/Spring+/Spring4+/SpringMVC+/SpringMvcSpringBoot/src/main/webapp/WEB-INF/jsp/home.jsp
@@ -7,11 +7,17 @@
 		<li><a href="/hello">Hello</a></li>
 		<li><a href="/form/student">Form</a></li>
 		<li><a href="/redirect">Redirect</a></li>
-		<li><a href="/respose_entity">ResponseEntity</a></li>
+		<li>Response
+			<ul>
+				<li><a href="/respose_entity">ResponseEntity</a></li>
+				<li><a href="/respose_body">@ResponseBody</a></li>
+			</ul>
+		</li>
 		<li>Exception
 			<ul>
 				<li><a href="/throw_io_exception">Throw an IOException</a></li>
-				<li><a href="/throw_enexpected_exception">Throw an unexpected exception</a></li>
+				<li><a href="/throw_enexpected_exception">Throw an
+						unexpected exception</a></li>
 			</ul>
 		</li>
 		<li>Arguments
