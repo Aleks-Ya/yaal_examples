82c4efc3e627087586ae2b870e772c6e60a38fbd
Aleksei Iablokov <Aleksei_Iablokov@epam.com>
2016-08-26 14:47:09 +0300

    Spring MVC path variable

--- Java+/Spring+/Spring4+/SpringMVC+/SpringMvcSpringBoot/src/main/java/ru/yaal/spring/mvc/controller/argument/ArgumentController.java
+++ Java+/Spring+/Spring4+/SpringMVC+/SpringMvcSpringBoot/src/main/java/ru/yaal/spring/mvc/controller/argument/ArgumentController.java
@@ -1,25 +1,38 @@
 package ru.yaal.spring.mvc.controller.argument;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ArgumentController {
-	@RequestMapping(value = "/argument")
-	public ResponseEntity<String> agrument(HttpServletRequest request, HttpSession session,
-			@RequestParam("code") String code) {
+	@RequestMapping(value = "/http")
+	public ResponseEntity<String> http(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
 
 		int length = request.getContentLength();
+		String encoding = response.getCharacterEncoding();
 		int maxInactiveInterval = session.getMaxInactiveInterval();
-		String body = String.format("Content length: %d, Max inactive interval: %d, Parameter code: %s",
-				length, maxInactiveInterval, code);
+		String body = String.format("Content length: %d, Max inactive interval: %d, Encoding: %s", length,
+				maxInactiveInterval, encoding);
 
+		return new ResponseEntity<>(body, HttpStatus.ACCEPTED);
+	}
+
+	@RequestMapping(value = "/request_param")
+	public ResponseEntity<String> agrument(@RequestParam("code") String code) {
+		return new ResponseEntity<>("Request param 'code': " + code, HttpStatus.ACCEPTED);
+	}
+	
+	@RequestMapping(value = "/path_variable/{operation}/{id}")
+	public ResponseEntity<String> pathVariable(@PathVariable("operation") String operationName, @PathVariable int id) {
+		String body = String.format("Operation: %s, Id: %d", operationName, id);
 		return new ResponseEntity<>(body, HttpStatus.ACCEPTED);
 	}
 }
--- Java+/Spring+/Spring4+/SpringMVC+/SpringMvcSpringBoot/src/main/webapp/WEB-INF/jsp/home.jsp
+++ Java+/Spring+/Spring4+/SpringMVC+/SpringMvcSpringBoot/src/main/webapp/WEB-INF/jsp/home.jsp
@@ -7,10 +7,21 @@
 		<li><a href="/hello">Hello</a></li>
 		<li><a href="/form/student">Form</a></li>
 		<li><a href="/redirect">Redirect</a></li>
-		<li><a href="/throw_io_exception">Throw an IOException</a></li>
-		<li><a href="/throw_enexpected_exception">Throw an unexpected exception</a></li>
 		<li><a href="/respose_entity">ResponseEntity</a></li>
-		<li><a href="/argument?code=abc">Argument</a></li>
+		<li>Exception
+			<ul>
+				<li><a href="/throw_io_exception">Throw an IOException</a></li>
+				<li><a href="/throw_enexpected_exception">Throw an unexpected exception</a></li>
+			</ul>
+		</li>
+		<li>Arguments
+			<ul>
+				<li><a href="/http">Http request, response and session</a></li>
+				<li><a href="/request_param?code=abc">@RequestParam</a></li>
+				<li><a href="/path_variable/find/1234">@PathVariable</a></li>
+			</ul>
+		</li>
+
 	</ul>
 </body>
 </html>
