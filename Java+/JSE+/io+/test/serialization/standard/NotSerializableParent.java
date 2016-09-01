873d421e23b0b12d36c69184a212f8a809e56667
Aleksei Iablokov <Aleksei_Iablokov@epam.com>
2016-09-01 13:48:56 +0300

    fix NotSerializableParent

--- Java+/JSE+/io+/test/serialization/standard/NotSerializableParent.java
+++ Java+/JSE+/io+/test/serialization/standard/NotSerializableParent.java
@@ -1,47 +1,49 @@
 package serialization.standard;
 
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
-import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
-import org.junit.Test;
-
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
+import org.junit.Test;
+
 /**
- * Если у сериализуемого класса есть несериализуемый родитель,
- * то во время десериализации у родителя будет вызван конструктор по-умолчанию.
+ * Если у сериализуемого класса есть несериализуемый родитель, то во время
+ * десериализации у родителя будет вызван конструктор по-умолчанию.
  */
 public class NotSerializableParent {
 
-    @Test
-    public void constructorsInvoke() throws Exception {
-        System.out.println("Create object:");
-        Child child = new Child();
+	@Test
+	public void constructorsInvoke() throws Exception {
+		System.out.println("Create object:");
+		Child child = new Child();
 
-        ByteOutputStream bos = new ByteOutputStream();
-        ObjectOutputStream oos = new ObjectOutputStream(bos);
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		ObjectOutputStream oos = new ObjectOutputStream(bos);
 
-        System.out.println("\nSerialization:");
-        oos.writeObject(child);
+		System.out.println("\nSerialization:");
+		oos.writeObject(child);
 
-        ByteInputStream bis = new ByteInputStream(bos.getBytes(), bos.getCount());
-        ObjectInputStream ois = new ObjectInputStream(bis);
+		ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+		ObjectInputStream ois = new ObjectInputStream(bis);
 
-        System.out.println("\nDeserialization:");
-        ois.readObject();
+		System.out.println("\nDeserialization:");
+		ois.readObject();
 
-    }
+	}
 
-    private static class Parent {
-        Parent() {
-            System.out.println("Parent's constructor");
-        }
-    }
+	private static class Parent {
+		Parent() {
+			System.out.println("Parent's constructor");
+		}
+	}
 
-    private static class Child extends Parent implements Serializable {
-        Child() {
-            System.out.println("Child's constructor");
-        }
-    }
+	private static class Child extends Parent implements Serializable {
+		private static final long serialVersionUID = 1L;
+
+		Child() {
+			System.out.println("Child's constructor");
+		}
+	}
 }
