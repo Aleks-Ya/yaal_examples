edd37de7bd07478411c7c702064c607ef90991b7
Aleksey Yablokov <ya_al@bk.ru>
2016-09-13 05:45:33 +0300

    join MvcConfiguration and WebMvcConfigurer

--- Java+/Spring+/Spring4+/SpringMVC+/SpringMvcSpringBoot/src/main/java/ru/yaal/spring/mvc/config/WebAppContext.java
+++ Java+/Spring+/Spring4+/SpringMVC+/SpringMvcSpringBoot/src/main/java/ru/yaal/spring/mvc/config/WebAppContext.java
@@ -1,21 +1,33 @@
 package ru.yaal.spring.mvc.config;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import ru.yaal.spring.mvc.controller.argument.RequestBodyController.UserConverter;
+
 @Configuration
-@EnableConfigurationProperties
-@Import({PropertiesSettings.class, YamlSettings.class})
-class MvcConfiguration {
+public class WebAppContext extends WebMvcConfigurerAdapter {
+
+	@Autowired
+	private UserConverter converter;
+
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		assert converter != null;
+		converters.add(converter);
+	}
+	
 	@Bean
-	public InternalResourceViewResolver internalResourceViewResolver() {
+	public InternalResourceViewResolver viewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setPrefix("/WEB-INF/jsp/");
 		resolver.setSuffix(".jsp");
@@ -23,7 +35,7 @@ class MvcConfiguration {
 	}
 	
 	@Bean
-	public SimpleMappingExceptionResolver simpleMappingExceptionResolver() {
+	public SimpleMappingExceptionResolver exceptionResolver() {
 		Properties props = new Properties();
 		props.put(IOException.class.getName(), "exception/io_error");
 
