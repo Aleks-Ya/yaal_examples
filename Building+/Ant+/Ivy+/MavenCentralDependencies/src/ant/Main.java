14acad25463f810139d0a4e02519c6a0559b6b92
Aleksei Iablokov <Aleksei_Iablokov@epam.com>
2016-09-15 20:30:09 +0300

    resolve dependencies from Maven Central with Ivy

--- Building+/Ant+/Ivy+/MavenCentralDependencies/src/ant/Main.java
+++ Building+/Ant+/Ivy+/MavenCentralDependencies/src/ant/Main.java
@@ -1,7 +1,11 @@
 package ant;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Main {
 	public static void main(String[] args) {
+		boolean isEquals = StringUtils.equalsIgnoreCase("a", "A");
 		System.out.println("Hello, Ant!");
+		System.out.println(isEquals);
 	}
 }
