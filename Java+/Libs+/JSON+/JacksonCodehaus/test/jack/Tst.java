40ac6b6fa196903e9058eb1217d74639445d73c9
Aleksei Iablokov <Aleksei_Iablokov@epam.com>
2016-09-23 17:21:21 +0300

    Jackson

---
+++ Java+/Libs+/JSON+/JacksonCodehaus/test/jack/Tst.java
@@ -0,0 +1,12 @@
+package jack;
+
+import org.junit.Test;
+import org.codehaus.jackson.map.ObjectMapper;
+
+public class Tst {
+
+	@Test
+	public void test() {
+		ObjectMapper mapper = new ObjectMapper();
+	}
+}
