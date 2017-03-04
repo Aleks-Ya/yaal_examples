cf5949fa2153aac270b3774bc57b3516d443dfca
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-04 17:07:25 +0300

    Hadoop HDFS

--- Java+/BigData+/Hadoop+/HDFS/src/main/java/ru/yaal/bigdata/epam/course/lesson2/Easy.java
+++
@@ -1,7 +0,0 @@
-package ru.yaal.bigdata.epam.course.lesson2;
-
-public class Easy {
-	public static void main(String[] args) {
-		System.out.println("hello!");
-	}
-}
--- Java+/BigData+/Hadoop+/HDFS/src/main/resources/ipinyou.sh
+++
@@ -1,1 +0,0 @@
-java -cp iPinyou-1-jar-with-dependencies.jar -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=y ru.yaal.bigdata.epam.course.lesson2.IPinYouMain
