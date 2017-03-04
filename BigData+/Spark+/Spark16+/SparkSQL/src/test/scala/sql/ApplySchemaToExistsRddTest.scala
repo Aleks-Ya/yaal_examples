153c39fff2775de832af18e317de2e98534b3344
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-04 19:22:35 +0300

    add assertions to ApplySchemaToExistsRddTest

--- BigData+/Spark+/Spark16+/SparkSQL/src/test/scala/sql/ApplySchemaToExistsRddTest.scala
+++ BigData+/Spark+/Spark16+/SparkSQL/src/test/scala/sql/ApplySchemaToExistsRddTest.scala
@@ -1,15 +1,15 @@
-package mysql
+package sql
 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.scalatest.FlatSpec
-import org.scalatest.Matchers.convertToStringShouldWrapper
+import org.scalatest.Matchers._
 import org.scalatest.BeforeAndAfterAll
 import org.apache.spark.sql._
 import org.apache.spark.sql.types._
 import org.apache.spark.rdd._
 
-class SqlContextTest extends FlatSpec with BeforeAndAfterAll {
+class ApplySchemaToExistsRddTest extends FlatSpec with BeforeAndAfterAll {
 
   var sc: SparkContext = null
   var sql: SQLContext = null
@@ -20,14 +20,22 @@ class SqlContextTest extends FlatSpec with BeforeAndAfterAll {
     sql = new SQLContext(sc)
   }
 
-  "Process lines from the access log" should "return Top5" in {
+  "Apply schema to RDD" should "print table" in {
     val people = sc.parallelize(Seq("Jhon,25", "Peter,35"))
     val schemaStr = "name age"
     val schema = StructType(schemaStr.split(" ").map(fieldName => StructField(fieldName, StringType, true)))
     val rowRdd = people.map(_.split(",")).map(p => Row(p(0), p(1).trim))
     val peopleSchemaRdd = sql.applySchema(rowRdd, schema)
+    peopleSchemaRdd.registerTempTable("people")
+    sql.tableNames.toList should contain ("people")
+    
+    println("Tables: " + sql.tableNames.toList)
+
+    val result = sql.sql("SELECT name, age FROM people WHERE age > 30")
+    result.map(r => r.toString()).collect should contain ("[Peter,35]")
+    
     peopleSchemaRdd.printSchema //-> peopleSchemaRdd.schema.treeString
-    peopleSchemaRdd.show()
+    peopleSchemaRdd.show
 
     println("JSON: " + peopleSchemaRdd.schema.prettyJson)
     val tree = peopleSchemaRdd.schema.treeString
