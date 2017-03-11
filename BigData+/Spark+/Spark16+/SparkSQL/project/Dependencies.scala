163f0b4be3e19caf3f891ae95cde07b0110fde0e
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-11 11:39:38 +0300

    SparkSQL: remove implicit transitive dependencies

--- BigData+/Spark+/Spark16+/SparkSQL/project/Dependencies.scala
+++ BigData+/Spark+/Spark16+/SparkSQL/project/Dependencies.scala
@@ -2,10 +2,7 @@ import sbt._
 
 object Dependencies {
   val allDeps = Seq(
-    "org.scala-lang" % "scala-library" % "2.11.8" % "provided",
-    "org.apache.spark" % "spark-core_2.11" % "1.6.2" % "provided",
-    "org.apache.spark" % "spark-sql_2.11" % "1.6.2" % "provided",
-    "org.apache.spark" % "spark-hive_2.11" % "1.6.2" % "provided",
+    "org.apache.spark" % "spark-hive_2.11" % "1.6.2",
     "com.databricks" % "spark-csv_2.11" % "1.5.0",
     "org.scalatest" % "scalatest_2.11" % "3.0.1" % Test)
 }
