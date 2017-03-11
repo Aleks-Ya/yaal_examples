9d5c3c737494cecff9c12981b2d584a0424e0f0a
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-11 16:48:45 +0300

    Scalatest: remove dependency on Scala

--- Scala+/Scalatest/project/Dependencies.scala
+++ Scala+/Scalatest/project/Dependencies.scala
@@ -2,6 +2,6 @@ import sbt._
 
 object Dependencies {
   val allDeps = Seq(
-    "org.scala-lang" % "scala-library" % "2.11.8" % Provided,
-    "org.scalatest" % "scalatest_2.11" % "3.0.1" % Test)
+    "org.scalatest" % "scalatest_2.11" % "3.0.1" % Test
+  )
 }
