5899297608e2d2369e0fad56b245e8e39c960659
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-12 20:16:06 +0300

    create Spark2Hive

---
+++ BigData+/Spark+/Spark2+/Spark2Hive/src/test/scala/sql/hive/TestHiveSingletonTest.scala
@@ -0,0 +1,16 @@
+package sql.hive
+
+import org.apache.spark.sql.hive.test._
+import org.scalatest.{BeforeAndAfterAll, FlatSpec}
+import org.scalatest.Matchers._
+
+class TestHiveSingletonTest extends FlatSpec
+  with BeforeAndAfterAll
+//  with TestHive
+{
+
+  "Taste TestHiveSingleton" should "work" in {
+//    sqlContext.sql("CREATE TABLE IF NOT EXISTS src1 (key INT, value STRING)")
+//    hiveContext.sql("CREATE TABLE IF NOT EXISTS src2 (key INT, value STRING)")
+  }
+}
