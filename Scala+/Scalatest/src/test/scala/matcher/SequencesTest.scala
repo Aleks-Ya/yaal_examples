c02af9e1b9ab1a79dd8b5e628dd885a44845219b
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-12 14:00:21 +0300

    Scalatest: add ArrayTest.scala

--- Scala+/Scalatest/src/test/scala/matcher/SequencesTest.scala
+++ Scala+/Scalatest/src/test/scala/matcher/SequencesTest.scala
@@ -7,7 +7,6 @@ class SequencesTest extends FlatSpec with Matchers {
   "sequences" should "work" in {
     List(1, 2, 2, 3, 3, 3) should contain inOrderOnly(1, 2, 3)
     List(1, 2, 2, 3, 3, 3) should contain(1)
-    //List(1, 2, 2, 3, 3, 3) should contain (1)// don't work
   }
 
 }
