a03306641462ac9e9db143e40bf78a584879400c
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-19 20:00:13 +0300

    ScalaCore: append ReplaceElementTest

--- Scala+/ScalaCore/src/test/scala/collection/ReplaceElementTest.scala
+++ Scala+/ScalaCore/src/test/scala/collection/ReplaceElementTest.scala
@@ -10,4 +10,10 @@ class ReplaceElementTest extends FlatSpec with Matchers {
     patched should contain inOrderOnly(1, 7, 8, 4)
   }
 
+  it should "replace single elements in sequence" in {
+    val s = Seq(1, 2, 3, 4)
+    val updated = s.updated(2, 8)
+    updated should contain inOrderOnly(1, 2, 8, 4)
+  }
+
 }
