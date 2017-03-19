69b7bdc9ec275761381a2992263344596cc2fccc
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-20 00:36:03 +0300

    Spark2MLlib: fix dependency

--- BigData+/Spark+/Spark2+/Spark2MLlib/project/Dependencies.scala
+++ BigData+/Spark+/Spark2+/Spark2MLlib/project/Dependencies.scala
@@ -2,6 +2,6 @@ import sbt._
 
 object Dependencies {
   val allDeps = Seq(
-    "org.apache.spark" % "spark-mllib_2.11" % "2.1.0",
+    "org.apache.spark" % "spark-mllib_2.11" % "2.1.0" exclude("org.scalatest", "scalatest_2.11"),
     "org.scalatest" % "scalatest_2.11" % "3.0.1" % Test)
 }
