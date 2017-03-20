5eb560d28c04570c4fb0778f9fcabed52606616f
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-20 17:34:06 +0300

    ScalaCore: add MultilineStringTest

---
+++ Scala+/ScalaCore/src/test/scala/string/MultilineStringTest.scala
@@ -0,0 +1,13 @@
+package string
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class MultilineStringTest extends FlatSpec with Matchers {
+
+  it should "format string without problems" in {
+    val text =
+      """You can solve this problem
+        |in several different ways.""".stripMargin
+    text shouldEqual "You can solve this problem\r\nin several different ways."
+  }
+}
