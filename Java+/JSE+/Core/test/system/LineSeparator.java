7701f5368ad3f83d5c42cc6a6f29490f66112448
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-20 23:55:55 +0300

    Java+: add LineSeparator

---
+++ Java+/JSE+/Core/test/system/LineSeparator.java
@@ -0,0 +1,17 @@
+package system;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Перевод строки.
+ */
+public class LineSeparator {
+
+    @Test
+    public void test() {
+        String separator = System.getProperty("line.separator");
+        assertTrue("\n".equals(separator) || "\r\n".equals(separator));
+    }
+}
