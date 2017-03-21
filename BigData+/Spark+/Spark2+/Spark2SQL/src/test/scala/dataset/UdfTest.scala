910a6f8f7385d6959542f0fe120cb7a5bec889c9
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-21 14:27:49 +0300

    Spark2SQL: update UdfTest

--- BigData+/Spark+/Spark2+/Spark2SQL/src/test/scala/dataset/UdfTest.scala
+++ BigData+/Spark+/Spark2+/Spark2SQL/src/test/scala/dataset/UdfTest.scala
@@ -15,4 +15,17 @@ class UdfTest extends FlatSpec with BeforeAndAfterAll with Matchers {
     val upperUDF = udf(upper)
     ds.withColumn("upper", upperUDF('value)).show
   }
+
+  it should "put sum of an array to new column" in {
+    val sqlContext = Factory.ss.sqlContext
+    import sqlContext.implicits._
+    val ds = Factory.ss.createDataset(Seq(Array(1, 2, 3), Array(4, 5, 6)))
+    ds.show
+    val sum: Seq[Int] => Int = _.sum
+    sum(Array(1, 2, 3)) shouldEqual 6
+    val sumUdf = udf(sum)
+    val newDs = ds.withColumn("sum", sumUdf('value))
+    newDs.show
+    newDs.select(col("sum").as[Int]).collectAsList should contain inOrderOnly(6, 15)
+  }
 }
