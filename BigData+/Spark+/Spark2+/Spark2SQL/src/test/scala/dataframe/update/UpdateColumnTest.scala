1bffe3cab3f5a15a552eb3a135a770356a04703b
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-22 11:01:12 +0300

    Spark2SQL: update UpdateColumnTest

--- BigData+/Spark+/Spark2+/Spark2SQL/src/test/scala/dataframe/update/UpdateColumnTest.scala
+++ BigData+/Spark+/Spark2+/Spark2SQL/src/test/scala/dataframe/update/UpdateColumnTest.scala
@@ -1,8 +1,8 @@
 package dataframe.update
 
 import factory.Factory
+import org.apache.spark.sql.Row
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types.ByteType
 import org.scalatest.{FlatSpec, Matchers}
 
 class UpdateColumnTest extends FlatSpec with Matchers {
@@ -11,6 +11,21 @@ class UpdateColumnTest extends FlatSpec with Matchers {
     val df = Factory.peopleDf
     df.show
     df.withColumn("age", col("age").multiply(2)).show
+  }
+
+  it should "change" in {
+    val df = Factory.peopleDf
+    df.show
+    implicit val mapEncoder = org.apache.spark.sql.Encoders.kryo[Row]
+    df.map(row => {
+      val index = row.fieldIndex("name")
+      val newValue = "Mr. " + row.getString(index)
+      val rowSeq = row.toSeq.toBuffer
+      rowSeq(index) = newValue
+      val newRow = Row(rowSeq)
+      println("New row: " + newRow)
+      newRow
+    }).foreach(row => println(row))
   }
 
 }
