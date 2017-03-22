fa01f1a1abaeb773c866ef2804062b05e7398cb8
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-22 11:16:32 +0300

    Spark2SQL: add init DataSet with Seq

--- BigData+/Spark+/Spark2+/Spark2SQL/src/test/scala/dataset/InCodeTest.scala
+++ BigData+/Spark+/Spark2+/Spark2SQL/src/test/scala/dataset/InCodeTest.scala
@@ -1,14 +1,21 @@
 package dataset
 
 import factory.Factory
-import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
+import org.scalatest.{FlatSpec, Matchers}
 
-class InCodeTest extends FlatSpec with BeforeAndAfterAll with Matchers {
+class InCodeTest extends FlatSpec with Matchers {
 
-  it should "init dataset" in {
+  it should "init DataSet" in {
     val sqlContext = Factory.ss.sqlContext
     import sqlContext.implicits._
     val ds = Factory.ss.createDataset(Seq("a", "b"))
+    ds.show
+  }
+
+  it should "init DataSet with Seq" in {
+    val sqlContext = Factory.ss.sqlContext
+    import sqlContext.implicits._
+    val ds = Seq("c", "d").toDS()
     ds.show
   }
 
