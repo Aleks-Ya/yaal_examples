8479b78fa74ae21a856e34c0286e7a55c4a9595a
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-24 12:52:22 +0300

    Spark2SQL: add ToDfTest

---
+++ BigData+/Spark+/Spark2+/Spark2SQL/src/test/scala/dataset/ToDfTest.scala
@@ -0,0 +1,32 @@
+package dataset
+
+import factory.Factory
+import org.scalatest.{FlatSpec, Matchers}
+
+class InCodeTest extends FlatSpec with Matchers {
+
+  it should "init DataSet" in {
+    val sqlContext = Factory.ss.sqlContext
+    import sqlContext.implicits._
+    val ds = Factory.ss.createDataset(Seq("a", "b"))
+    ds.show
+    ds.toDF().foreach(row => )
+  }
+
+  it should "init DataSet with Seq" in {
+    val sqlContext = Factory.ss.sqlContext
+    import sqlContext.implicits._
+    val ds = Seq("c", "d").toDS()
+    ds.show
+  }
+
+  it should "init DataSet with POJO" in {
+    implicit val mapEncoder = org.apache.spark.sql.Encoders.kryo[PeopleInCode]
+    val list = List(PeopleInCode("John", 25), PeopleInCode("Petr", 30))
+    val ds = Factory.ss.createDataset(list)
+
+  }
+
+}
+
+case class PeopleInCode(name: String, age: Int) {}
