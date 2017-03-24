22216a9dae0a7700c44d5d8cc511615ccda7a103
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-24 13:16:52 +0300

    create CloudKafka

---
+++ BigData+/Kafka+/KafkaJava+/CloudKafka/src/main/java/cloud/ConsumerMain.java
@@ -0,0 +1,34 @@
+package cloud;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class ConsumerMain {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        Consumer<String, String> consumer = new KafkaConsumer<>(props);
+        List<String> topics = Arrays.asList("test", "foo");
+        consumer.subscribe(topics);
+        //noinspection InfiniteLoopStatement
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+            }
+        }
+    }
+
+}
