45a8113059cb152ef55560de3b6f9dffbc22ef6d
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-26 16:52:15 +0300

    ScalaTest: append MapTest

--- Scala+/Scalatest/src/test/scala/matcher/collections/MapTest.scala
+++ Scala+/Scalatest/src/test/scala/matcher/collections/MapTest.scala
@@ -1,6 +1,6 @@
 package matcher.collections
 
-import org.scalatest.{Entry, FlatSpec, Matchers}
+import org.scalatest.{FlatSpec, Matchers}
 
 class MapTest extends FlatSpec with Matchers {
 
@@ -8,8 +8,14 @@ class MapTest extends FlatSpec with Matchers {
     val map = Map("x" -> 24, "y" -> 25)
     map should contain key "x"
     map should contain value 24
-//    map should contain  (Entry("x", 24))
-//    map should contain only (Entry("x", 24))
+    //    map should contain  (Entry("x", 24))
+    //    map should contain only (Entry("x", 24))
   }
+
+  it should "verify empty maps" in {
+    Map() shouldBe empty
+    Map(1 -> 2) should not be empty
+  }
+
 
 }
