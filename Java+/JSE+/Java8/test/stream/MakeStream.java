d19191d24dc1cf5a94ab2854531bf2d193ed7a1b
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-28 20:23:40 +0300

    add MakeStream#iterable

--- Java+/JSE+/Java8/test/stream/MakeStream.java
+++ Java+/JSE+/Java8/test/stream/MakeStream.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  * Создание объекта Stream.
@@ -17,5 +18,11 @@ public class MakeStream {
         Stream<String> stream = Stream.of("a", "b");
         IntStream range = IntStream.range(1, 5);
         DoubleStream array = Arrays.stream(new double[]{1, 2, 4});
+    }
+
+    @Test
+    public void iterable() {
+        Iterable<String> itr = Arrays.asList("a", "b");
+        Stream<String> stream = StreamSupport.stream(itr.spliterator(), false);
     }
 }
