57b616a7b32ba8e12576db12b8dbfd1855c60bc4
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-04-04 00:22:32 +0300

    Apache Commons: FileUtilsTest

---
+++ Java+/Libs+/ApacheCommons+/IO/test/FileUtilsTest.java
@@ -0,0 +1,41 @@
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class FileUtilsTest {
+
+    private File outFile;
+
+    @Before
+    public void setUp() throws Exception {
+        outFile = Files.createTempFile("fileUtilsUse_", ".tmp").toFile();
+        outFile.deleteOnExit();
+    }
+
+    @Test
+    public void stringToFile() throws IOException {
+        String expected = "Hey, file!";
+        FileUtils.writeStringToFile(outFile, expected);
+        String actual = FileUtils.readFileToString(outFile);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void inputStreamToFile() throws IOException {
+        byte[] expected = {1, 2, 3};
+        InputStream is = new ByteArrayInputStream(expected);
+        FileUtils.copyInputStreamToFile(is, outFile);
+        byte[] actual = FileUtils.readFileToByteArray(outFile);
+        assertThat(actual, equalTo(expected));
+    }
+}
