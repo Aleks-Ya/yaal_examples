bda177d177118d85b51eb9120c49772390976ca7
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-04-04 18:29:08 +0300

    add UnpackGzipTest

---
+++ Java+/JSE+/io+/test/archive/gzip/UnpackGzipTest.java
@@ -0,0 +1,32 @@
+package archive.gzip;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+import java.util.zip.GZIPInputStream;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class UnpackGzipTest {
+
+    @Test
+    public void unpack() throws IOException {
+        InputStream is = getClass().getResource("content.txt.gz").openStream();
+        GZIPInputStream gzip = new GZIPInputStream(is);
+        String content = new BufferedReader(new InputStreamReader(gzip)).lines().collect(Collectors.joining());
+        assertThat(content, equalTo("file_content"));
+    }
+
+    @Test
+    public void packedFileName() throws IOException {
+        String archiveFileName = "content.txt.gz";
+        int lastDot = archiveFileName.lastIndexOf(".");
+        String packedFileName = archiveFileName.substring(0, lastDot);
+        assertThat(packedFileName, equalTo("content.txt"));
+    }
+}
