8259742c852dfa46cf4a3f65c401fd86e72a44f5
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-04-04 14:40:58 +0300

    Hamcrest: append FileAssert

--- Java+/UnitTest+/Hamcrest/test/FileAssert.java
+++ Java+/UnitTest+/Hamcrest/test/FileAssert.java
@@ -2,8 +2,10 @@ import org.hamcrest.io.FileMatchers;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 
+import static org.hamcrest.Matchers.endsWith;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -11,10 +13,16 @@ import static org.junit.Assert.assertThat;
  */
 public class FileAssert {
     @Test
-    public void testName() throws Exception {
+    public void existingFile() throws IOException {
         File f = Files.createTempFile("pref", "suffix").toFile();
         f.deleteOnExit();
-
         assertThat(f, FileMatchers.anExistingFile());
+    }
+
+    @Test
+    public void canonicalPath() throws IOException {
+        File f = Files.createTempFile("pref", "suffix").toFile();
+        f.deleteOnExit();
+        assertThat(f, FileMatchers.aFileWithCanonicalPath(endsWith("suffix")));
     }
 }
