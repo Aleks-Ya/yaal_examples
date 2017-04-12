00c3dfc86490c56cb20d2c19a1d8fac8b7968fc7
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-04-12 13:52:48 +0300

    add IOUtilsTest

---
+++ Java+/Libs+/ApacheCommons+/IO/test/IOUtilsTest.java
@@ -0,0 +1,21 @@
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class IOUtilsTest {
+
+    @Test
+    public void inputStreamToString() throws IOException {
+        String exp = "abc";
+        InputStream is = new ByteArrayInputStream(exp.getBytes());
+        String act = IOUtils.toString(is, Charset.defaultCharset());
+        assertThat(act, equalTo(exp));
+    }
+}
