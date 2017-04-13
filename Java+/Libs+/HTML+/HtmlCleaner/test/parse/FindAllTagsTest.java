800e9a44c28c901ab556eea812ed25f53bf1b712
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-04-14 01:37:22 +0300

    add FindAllTagsTest

---
+++ Java+/Libs+/HTML+/HtmlCleaner/test/parse/FindAllTagsTest.java
@@ -0,0 +1,34 @@
+package parse;
+
+import org.htmlcleaner.HtmlCleaner;
+import org.htmlcleaner.TagNode;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+public class FindAllTagsTest {
+    @Test
+    public void getElementListByName() {
+        String html = "<html>" +
+                "<head>" +
+                "<title>First parse</title>" +
+                "</head>" +
+                "<body>" +
+                "<p>Parsed HTML.</p>" +
+                "<div>Part 1</div>" +
+                "<div><p>nested</p></div>" +
+                "</body>" +
+                "</html>";
+
+        HtmlCleaner cleaner = new HtmlCleaner();
+        TagNode node = cleaner.clean(html);
+        List<? extends TagNode> pTags = node.getElementListByName("p", true);
+        assertThat(pTags, hasSize(2));
+        assertThat(pTags.get(0).getText(), hasToString(equalTo("Parsed HTML.")));
+        assertThat(pTags.get(1).getText(), hasToString(equalTo("nested")));
+
+    }
+}
