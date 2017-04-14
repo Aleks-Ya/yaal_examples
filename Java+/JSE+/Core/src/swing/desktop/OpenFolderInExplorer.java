bd197be062a8085b0dfb6fb5a831e8ea92fcb1dc
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-04-14 23:49:56 +0300

    include Java Desktop to Core

--- Java+/JSE+/Core/src/swing/desktop/OpenFolderInExplorer.java
+++ Java+/JSE+/Core/src/swing/desktop/OpenFolderInExplorer.java
@@ -1,8 +1,13 @@
+package swing.desktop;
+
 import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
 
-public class Main {
+/**
+ * Задача: открыть папку в проводнике (Windows и Linux).
+ */
+public class OpenFolderInExplorer {
     public static void main(String[] args) throws IOException {
         File dir = new File(".");
         Desktop.getDesktop().open(dir);
