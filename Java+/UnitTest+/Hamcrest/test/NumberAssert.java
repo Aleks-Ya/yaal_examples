af10e214399ae0136714352d8d0e8d062becd542
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-04-21 21:10:40 +0300

    add closeTo into NumberAssert

--- Java+/UnitTest+/Hamcrest/test/NumberAssert.java
+++ Java+/UnitTest+/Hamcrest/test/NumberAssert.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertThat;
 
@@ -8,5 +9,10 @@ public class NumberAssert {
     public void doubleNumbers() {
         double d = 1.5;
         assertThat(d, greaterThan(1D));
+    }
+
+    @Test
+    public void close() {
+        assertThat(1020D, closeTo(1000D, 50D));
     }
 }
