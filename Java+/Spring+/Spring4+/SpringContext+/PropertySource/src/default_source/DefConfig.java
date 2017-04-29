341fd1882361a4ef21f753e2ac0665ab39cd36de
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-04-29 16:13:28 +0300

    PropertySource: add default source test

---
+++ Java+/Spring+/Spring4+/SpringContext+/PropertySource/src/default_source/DefConfig.java
@@ -0,0 +1,13 @@
+package default_source;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Можно задать путь к файлу свойств через свойства JVM: -Dconf=file:${user.dir}/any/file.properties
+ * Если свойство "conf" не задано, будет использовано значение по-умолчанию.
+ */
+@Configuration
+@PropertySource("${conf:file:${user.dir}/resources/default_source/def.properties}")
+class DefConfig {
+}
