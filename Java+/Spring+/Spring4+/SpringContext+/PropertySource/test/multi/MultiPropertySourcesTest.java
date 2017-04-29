a524513f07af9266ecfd81b250857ba04c1d95b9
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-04-29 16:07:41 +0300

    PropertySource: add multi test

--- Java+/Spring+/Spring4+/SpringContext+/PropertySource/test/multi/MultiPropertySourcesTest.java
+++ Java+/Spring+/Spring4+/SpringContext+/PropertySource/test/multi/MultiPropertySourcesTest.java
@@ -1,8 +1,9 @@
-package environment;
+package multi;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -11,16 +12,17 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 /**
- * Take properties from {@link Environment}.
+ * Load properties from several {@link PropertySource}.
  */
-@ContextConfiguration(classes = EnvironmentConfig.class)
+@ContextConfiguration(classes = MultiConfig.class)
 @RunWith(SpringJUnit4ClassRunner.class)
-public class EnvironmentTest {
+public class MultiPropertySourcesTest {
     @Autowired
     private Environment env;
 
     @Test
     public void environmentFromClasspath() {
-        assertThat(env.getProperty("planet.name"), equalTo("Mars"));
+        assertThat(env.getProperty("star.name"), equalTo("Sun"));
+        assertThat(env.getProperty("planet.name"), equalTo("Earth"));
     }
 }
--- Java+/Spring+/Spring4+/SpringContext+/PropertySource/test/value/ValueTest.java
+++ Java+/Spring+/Spring4+/SpringContext+/PropertySource/test/value/ValueTest.java
@@ -9,7 +9,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
-@ContextConfiguration(classes = {ValueConfig.class})
+/**
+ * Take properties using {@link Value} annotation.
+ */
+@ContextConfiguration(classes = ValueConfig.class)
 @RunWith(SpringJUnit4ClassRunner.class)
 public class ValueTest {
 
