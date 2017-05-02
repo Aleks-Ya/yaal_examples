f7c23513245419ade6bd01d65b6affae13534d15
Aleksey Yablokov <ya_al@bk.ru>
2017-05-03 00:23:33 +0300

    add ValueByPathTest

---
+++ Java+/Libs+/JSON+/JacksonFasterxml/test/deserialize/ValueByPathTest.java
@@ -0,0 +1,27 @@
+package deserialize;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Extract value from JSON by path.
+ */
+public class ValueByPathTest {
+
+    @Test
+    public void at() throws IOException {
+        String json = "{\"a\": 1, \"b\": \"abc\", \"c\": {\"d\": true}}";
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(json);
+        assertThat(root.at("/a").asInt(), equalTo(1));
+        assertThat(root.at("/b").asText(), equalTo("abc"));
+        assertThat(root.at("/c/d").asBoolean(), equalTo(true));
+    }
+
+}
