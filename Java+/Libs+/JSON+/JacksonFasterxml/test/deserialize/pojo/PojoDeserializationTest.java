79be01c3c1cf7e64f9f6a46658df56f8257c5b0b
Aleksey Yablokov <ya_al@bk.ru>
2017-05-03 09:13:52 +0300

    add PojoDeserializationTest

---
+++ Java+/Libs+/JSON+/JacksonFasterxml/test/deserialize/pojo/PojoDeserializationTest.java
@@ -0,0 +1,43 @@
+package deserialize.pojo;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Deserialize JSON to POJO.
+ */
+public class PojoDeserializationTest {
+
+    @Test
+    public void readValue() throws IOException {
+        String json = "{\"id\": 123, \"name\": \"aleks\"}";
+        ObjectMapper mapper = new ObjectMapper();
+
+        User user = mapper.readValue(json, User.class);
+
+        assertThat(user.getId(), equalTo(123));
+        assertThat(user.getName(), equalTo("aleks"));
+    }
+
+    /**
+     * Skip unknown fields.
+     */
+    @Test
+    public void failOnUnknownProperties() throws IOException {
+        String json = "{\"id\": 123, \"name\": \"aleks\", \"age\": 30}";
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        User user = mapper.readValue(json, User.class);
+
+        assertThat(user.getId(), equalTo(123));
+        assertThat(user.getName(), equalTo("aleks"));
+    }
+
+}
