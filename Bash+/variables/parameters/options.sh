3ce00679de3ed35c78dfd978306dc1c733e9db9f
Aleksey Yablokov <ya_al@bk.ru>
2017-07-12 19:14:22 +0300

    bash parameters

---
+++ Bash+/variables/parameters/options.sh
@@ -0,0 +1,23 @@
+echo "OPTIND starts at $OPTIND"
+while getopts ":pq:" optname
+  do
+    case "$optname" in
+      "p")
+        echo "Option $optname is specified"
+        ;;
+      "q")
+        echo "Option $optname has value $OPTARG"
+        ;;
+      "?")
+        echo "Unknown option $OPTARG"
+        ;;
+      ":")
+        echo "No argument value for option $OPTARG"
+        ;;
+      *)
+      # Should not occur
+        echo "Unknown error while processing options"
+        ;;
+    esac
+    echo "OPTIND is now $OPTIND"
+  done
---
+++ Bash+/variables/parameters/parameters.sh
@@ -0,0 +1,19 @@
+echo "Param 0 (the script filename): $0"
+echo "Param 1: $1"
+echo "Param 2: $2"
+echo "Number of parameters: $#"
+echo "All params (*): $*"
+echo "All params (@): $@"
+
+echo
+echo "Parameters on new line:";
+echo Using '$*';
+ for p in $*;
+ do
+     echo "$p";
+ done;
+ echo Using '$@';
+ for p in $@;
+ do
+     echo "$p";
+ done;
