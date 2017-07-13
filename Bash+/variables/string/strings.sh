00aec9cd8aa444ca844b95f33cad2ab00efe0879
Aleksey Yablokov <ya_al@bk.ru>
2017-07-13 17:57:36 +0300

    update strings.sh

--- Bash+/variables/string/strings.sh
+++ Bash+/variables/string/strings.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-str="feature/test-8"
+str="Feature/Test-8"
+echo "Source string: ${str}"
 echo "Replace 8 with 9: ${str/8/9}"
 echo "Replace '/' with '-': ${str////-}"
 echo "Interval from 8 to the end: ${str:8}"
+echo "To uppercase: ${str^^}"
