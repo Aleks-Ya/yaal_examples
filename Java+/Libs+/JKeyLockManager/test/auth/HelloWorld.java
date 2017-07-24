abed9c42250134e94200ca79a7443f909b09bdff
Aleksey Yablokov <ya_al@bk.ru>
2017-07-24 20:09:41 +0300

    add JKeyLockManager project

---
+++ Java+/Libs+/JKeyLockManager/test/auth/HelloWorld.java
@@ -0,0 +1,17 @@
+package auth;
+
+import de.jkeylockmanager.manager.KeyLockManager;
+import de.jkeylockmanager.manager.KeyLockManagers;
+import org.junit.Test;
+
+public class HelloWorld {
+        private final KeyLockManager lockManager = KeyLockManagers.newLock();
+//        public void updateWeatherData(String cityName, float temperature) {
+//            lockManager.executeLocked(cityName, () -> delegate.updateWeatherData(cityName, temperature));
+//        }
+
+    @Test
+    public void name() throws Exception {
+
+    }
+}
