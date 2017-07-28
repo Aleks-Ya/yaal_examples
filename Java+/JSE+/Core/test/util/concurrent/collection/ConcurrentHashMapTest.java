20b20decb8be3bd38293b43a0115b8655c694059
Aleksey Yablokov <ya_al@bk.ru>
2017-07-28 10:07:14 +0300

    add ConcurrentHashMapTest#removeByIterator

--- Java+/JSE+/Core/test/util/concurrent/collection/ConcurrentHashMapTest.java
+++ Java+/JSE+/Core/test/util/concurrent/collection/ConcurrentHashMapTest.java
@@ -2,11 +2,13 @@ package util.concurrent.collection;
 
 import org.junit.Test;
 
+import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.anEmptyMap;
 
 public class ConcurrentHashMapTest {
 
@@ -20,5 +22,20 @@ public class ConcurrentHashMapTest {
         Integer value = map.computeIfAbsent(key, s -> 1);
         assertThat(value, equalTo(1));
         assertThat(map.get(key), equalTo(1));
+    }
+
+    /**
+     * Delete element of a ConcurrentHashMap during iterating.
+     */
+    @Test
+    public void removeByIterator() {
+        Map<String, Integer> map = new ConcurrentHashMap<>();
+        map.put("abc", 1);
+        Iterator<Map.Entry<String, Integer>> it = map.entrySet().iterator();
+        while(it.hasNext()) {
+            it.next();
+            it.remove();
+        }
+        assertThat(map, anEmptyMap());
     }
 }
