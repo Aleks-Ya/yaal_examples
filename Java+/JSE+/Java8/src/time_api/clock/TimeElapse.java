31a3ffeab9834b55c9de0e4951adc2cf9e35b5b2
Aleksey Yablokov <ya_al@bk.ru>
2017-08-09 12:55:01 +0300

    add ClockInTestsTest

---
+++ Java+/JSE+/Java8/src/time_api/clock/TimeElapse.java
@@ -0,0 +1,11 @@
+package time_api.clock;
+
+import java.time.Instant;
+
+class TimeElapse {
+    private final Instant endTime = Clocks.instance.getClock().instant().plusSeconds(60*60);
+
+    boolean isHourElapsed() {
+        return  Clocks.instance.getClock().instant().isAfter(endTime);
+    }
+}
---
+++ Java+/JSE+/Java8/test/time_api/clock/ClockInTestsTest.java
@@ -0,0 +1,30 @@
+package time_api.clock;
+
+import org.junit.Test;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ClockInTestsTest {
+    @Test
+    public void isHourElapsed() {
+        Clock fixedClock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+        Clocks.instance.setClock(fixedClock);
+        TimeElapse out = new TimeElapse();
+        assertFalse(out.isHourElapsed());
+
+        Clock clock30Minutes = Clock.offset(fixedClock, Duration.ofMinutes(30));
+        Clocks.instance.setClock(clock30Minutes);
+        assertFalse(out.isHourElapsed());
+
+        Clock clock61Minutes = Clock.offset(fixedClock, Duration.ofMinutes(61));
+        Clocks.instance.setClock(clock61Minutes);
+        assertTrue(out.isHourElapsed());
+    }
+
+}
