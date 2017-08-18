06c7d750510f1c690543a884c2cbb3c06fa4c1d5
Aleksey Yablokov <ya_al@bk.ru>
2017-08-18 17:45:50 +0300

    add Guava Intern

---
+++ Java+/Libs+/Guava/test/collections/InternerTest.java
@@ -0,0 +1,46 @@
+package collections;
+
+import com.google.common.collect.Interner;
+import com.google.common.collect.Interners;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class InternerTest {
+
+    @Test
+    public void emptyToNull() {
+        ImmutableObject obj1 = new ImmutableObject(1);
+        ImmutableObject obj2 = new ImmutableObject(1);
+        assertFalse(obj1 == obj2);
+
+        Interner<ImmutableObject> interner = Interners.newStrongInterner();
+
+        assertTrue(obj1 == interner.intern(obj1));
+        assertTrue(obj1 == interner.intern(obj2));
+    }
+
+    private static class ImmutableObject {
+        private final int number;
+
+        ImmutableObject(int number) {
+            this.number = number;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            ImmutableObject that = (ImmutableObject) o;
+
+            return number == that.number;
+        }
+
+        @Override
+        public int hashCode() {
+            return number;
+        }
+    }
+}
