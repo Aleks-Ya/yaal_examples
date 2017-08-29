63cb082fef940849588f87afc958daab2b28bdcd
Aleksey Yablokov <ya_al@bk.ru>
2017-08-29 16:00:56 +0300

    update PODAM version

--- Java+/UnitTest+/PODAM/test/PodamTest.java
+++ Java+/UnitTest+/PODAM/test/PodamTest.java
@@ -2,9 +2,9 @@ import org.junit.Test;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
-public class Main {
+public class PodamTest {
     @Test
-    public void testName() {
+    public void test() {
         PodamFactory factory = new PodamFactoryImpl();
         Country myPojo = factory.manufacturePojo(Country.class);
         System.out.println(myPojo);
