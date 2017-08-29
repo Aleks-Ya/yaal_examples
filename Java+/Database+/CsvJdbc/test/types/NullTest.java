486e80f169b5e6058357a099d775dcfabc99eb85
Aleksey Yablokov <ya_al@bk.ru>
2017-08-29 18:11:10 +0300

    add NullTest in CsvJdbc

---
+++ Java+/Database+/CsvJdbc/test/types/NullTest.java
@@ -0,0 +1,39 @@
+package types;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.math.BigDecimal;
+import java.sql.*;
+import java.text.ParseException;
+import java.util.Properties;
+import java.util.TimeZone;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertNull;
+
+/**
+ * CSV standard doesn't support NULL values.
+ * NULL, null, "NULL" will be treated as strings.
+ */
+public class NullTest {
+
+    @Test
+    public void test() throws ClassNotFoundException, SQLException, ParseException {
+        Class.forName("org.relique.jdbc.csv.CsvDriver");
+
+        Properties props = new Properties();
+
+        String database = new File(NullTest.class.getResource("null.csv").getFile()).getParent();
+        Connection conn = DriverManager.getConnection("jdbc:relique:csv:" + database, props);
+        Statement stmt = conn.createStatement();
+
+        ResultSet results = stmt.executeQuery("SELECT * FROM null");
+        results.next();
+
+        assertThat(results.getString("string"), equalTo("NULL"));
+
+        conn.close();
+    }
+}
