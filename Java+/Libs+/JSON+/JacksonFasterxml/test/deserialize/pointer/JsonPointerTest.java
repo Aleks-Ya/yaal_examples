918372d32de10347b3572cce0e7b6893563afd3b
Aleksey Yablokov <ya_al@bk.ru>
2017-09-01 17:18:22 +0300

    add Jackson JsonPoint

---
+++ Java+/Libs+/JSON+/JacksonFasterxml/test/deserialize/pointer/JsonPointerTest.java
@@ -0,0 +1,28 @@
+package deserialize.pointer;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Convert part of JSON to POJO.
+ */
+public class JsonPointerTest {
+
+    @Test
+    public void pointer() throws IOException {
+        String json = "{ \"person\": {\"id\": 123, \"name\": \"aleks\"}}";
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readTree(json);
+        JsonNode person = rootNode.at("/person");
+        User user = mapper.treeToValue(person, User.class);
+
+        assertThat(user.getId(), equalTo(123));
+        assertThat(user.getName(), equalTo("aleks"));
+    }
+}
