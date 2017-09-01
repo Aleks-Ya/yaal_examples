aefae8176ff88fa7e41200711682798c8f66c293
Aleksey Yablokov <ya_al@bk.ru>
2017-09-01 16:41:46 +0300

    add "custom header" to Spring RestTemplate

--- Java+/Spring+/Spring4+/SpringWeb+/SpringRestTemplate/src/main/java/data/Quote.java
+++ Java+/Spring+/Spring4+/SpringWeb+/SpringRestTemplate/src/main/java/data/Quote.java
@@ -1,4 +1,4 @@
-package spring.web.rest;
+package data;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
---
+++ Java+/Spring+/Spring4+/SpringWeb+/SpringRestTemplate/src/test/java/post/PostForObjectTest.java
@@ -0,0 +1,63 @@
+package post;
+
+import org.junit.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
+import data.Quote;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.client.ExpectedCount.once;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+
+public class PostForObjectTest {
+
+    @Test
+    public void customHeader() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        String headerName = "myheader";
+        String headerValue = "abc";
+        final String url = "localhost/path2";
+        final MockRestServiceServer server = MockRestServiceServer.bindTo(restTemplate).build();
+        server.expect(once(), requestTo(url))
+                .andExpect(method(HttpMethod.POST))
+                .andExpect(header(headerName, equalTo(headerValue)))
+                .andRespond(withSuccess("{}", MediaType.APPLICATION_JSON));
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(headerName, headerValue);
+        HttpEntity<String> entity = new HttpEntity<>("body", headers);
+
+        Quote quote = restTemplate.postForObject(url, entity, Quote.class);
+        System.out.println(quote.toString());
+    }
+
+    @Test
+    public void bodyTest() {
+        final RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+
+        String body = "{key: value}";
+        HttpEntity<String> entity = new HttpEntity<>(body, headers);
+
+        final String URL = "localhost/path";
+        final MockRestServiceServer server = MockRestServiceServer.bindTo(restTemplate).build();
+        server.expect(once(), requestTo(URL)).andExpect(method(HttpMethod.POST))
+                .andExpect(jsonPath("key", equalTo("value")))
+                .andExpect(header("content-type", equalTo(MediaType.APPLICATION_JSON_UTF8_VALUE)))
+                .andRespond(withSuccess("{}", MediaType.APPLICATION_JSON));
+
+        Quote quote = restTemplate.postForObject(URL, entity, Quote.class);
+        System.out.println(quote.toString());
+
+        server.verify();
+    }
+}
