1eaa7f081390ed34364797ac3111ff9890dccf93
Aleksey Yablokov <ya_al@bk.ru>
2017-09-06 14:32:30 +0300

    fix InitWithouWebXml

---
+++ Java+/Spring+/Spring4+/SpringWeb+/WebApplicationContext+/InitWithoutWebXml/src/mvc/Config.java
@@ -0,0 +1,11 @@
+package mvc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@Import(HelloController.class)
+public class Config {
+}
