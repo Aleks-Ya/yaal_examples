25ac2ab26e466627749519aff7cc5eb7c8915cc4
Aleksey Yablokov <ya_al@bk.ru>
2017-09-25 15:12:04 +0300

    add CustomizationTest

---
+++ Java+/UnitTest+/JsonAssert/test/CustomizationTest.java
@@ -0,0 +1,40 @@
+import org.json.JSONException;
+import org.junit.Test;
+import org.skyscreamer.jsonassert.Customization;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.skyscreamer.jsonassert.JSONCompareResult;
+import org.skyscreamer.jsonassert.ValueMatcher;
+import org.skyscreamer.jsonassert.comparator.CustomComparator;
+import org.skyscreamer.jsonassert.comparator.JSONComparator;
+
+import static org.junit.Assert.assertTrue;
+import static org.skyscreamer.jsonassert.JSONCompare.compareJSON;
+
+public class CustomizationTest {
+
+    @Test
+    public void anyValueByPath() throws JSONException {
+        String actual = "{first: actual, second:1}";
+        String expected = "{first: expected, second: 1}";
+
+        ValueMatcher<Object> comparator = (o1, o2) -> true;
+
+        Customization custom = Customization.customization("first", comparator);
+        JSONComparator jsonCmp = new CustomComparator(JSONCompareMode.STRICT, custom);
+        JSONCompareResult result = compareJSON(expected, actual, jsonCmp);
+        assertTrue(result.getMessage(), result.passed());
+    }
+
+    @Test
+    public void customCompareValueByPath() throws JSONException {
+        String actual = "{first: actual, second:1}";
+        String expected = "{first: expected, second: 1}";
+
+        ValueMatcher<Object> comparator = (o1, o2) -> o1.toString().equals("actual") && o2.toString().equals("expected");
+
+        Customization custom = Customization.customization("first", comparator);
+        JSONComparator jsonCmp = new CustomComparator(JSONCompareMode.STRICT, custom);
+        JSONCompareResult result = compareJSON(expected, actual, jsonCmp);
+        assertTrue(result.getMessage(), result.passed());
+    }
+}
