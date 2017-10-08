d3c9dcbd128b5f58ed75d57b7193687b77c519b6
Aleksey Yablokov <ya_al@bk.ru>
2017-10-08 20:45:00 +0300

    refactor ExcludeDependencies

--- Building+/Gradle+/Multimodule+/Dependencies+/ExcludeDependency/TopProject/SubProject/test/SubClass.java
+++
@@ -1,8 +0,0 @@
-import org.junit.Test;
-
-public class SubClass {
-    @Test(expected = ClassNotFoundException.class)
-    public void main() throws ClassNotFoundException {
-        Class.forName("org.joda.time.DateTime");
-    }
-}
