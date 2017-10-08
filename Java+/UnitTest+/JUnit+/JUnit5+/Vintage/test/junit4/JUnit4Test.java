8383893ab6b56d0814130cdc3fff18b461810257
Aleksey Yablokov <ya_al@bk.ru>
2017-10-08 20:54:07 +0300

    JUnit5 dependencies

---
+++ Java+/UnitTest+/JUnit+/JUnit5+/Vintage/test/junit4/JUnit4Test.java
@@ -0,0 +1,13 @@
+package junit4;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class JUnit4Test {
+
+    @Test
+    public void success() {
+        assertTrue(true);
+    }
+}
