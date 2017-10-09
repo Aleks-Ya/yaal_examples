6fec4c3f5f290b07aa087db730ac83f71ac0abc7
Aleksey Yablokov <ya_al@bk.ru>
2017-10-09 17:02:50 +0300

    add Mockito project

---
+++ Java+/UnitTest+/Mockito/test/mockito/ConstructorWithParameters.java
@@ -0,0 +1,29 @@
+package mockito;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
+
+public class ConstructorWithParameters {
+
+    @Test
+    public void string() {
+        Data mock = mock(Data.class);
+        assertNull(mock.getTitle());
+    }
+
+    private static class Data {
+        private String title;
+
+        private Data(String title) {
+            this.title = title;
+        }
+
+        String getTitle() {
+            return title;
+        }
+
+    }
+
+}
