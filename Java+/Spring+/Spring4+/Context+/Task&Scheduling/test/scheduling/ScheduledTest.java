aa3415588f12d3d8b1cbd1bc991224d8a96c5693
Aleksey Yablokov <ya_al@bk.ru>
2017-10-09 15:39:09 +0300

    fix ScheduledTest

--- Java+/Spring+/Spring4+/Context+/Task&Scheduling/test/scheduling/ScheduledTest.java
+++ Java+/Spring+/Spring4+/Context+/Task&Scheduling/test/scheduling/ScheduledTest.java
@@ -3,10 +3,12 @@ package scheduling;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@TestPropertySource(properties = {"my-prop = 1000"})
 @ContextConfiguration(classes = ScheduledConfig.class)
-@RunWith(SpringJUnit4ClassRunner.class)
 public class ScheduledTest {
     @Test
     public void name() throws InterruptedException {
