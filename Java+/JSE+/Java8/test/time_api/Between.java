078f7dada79453a2b207fe0b905c4a27c572a436
Aleksey Yablokov <ya_al@bk.ru>
2017-10-10 22:05:52 +0300

    add Between

---
+++ Java+/JSE+/Java8/test/time_api/Between.java
@@ -0,0 +1,24 @@
+package time_api;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Интервал между датами.
+ */
+public class Between {
+    @Test
+    public void date() {
+        LocalDate date1 = LocalDate.of(2017, 10, 1);
+        long daysToAdd = 3;
+        LocalDate date2 = date1.plusDays(daysToAdd);
+
+        long between = ChronoUnit.DAYS.between(date1, date2);
+        assertThat(between, equalTo(daysToAdd));
+    }
+}
