6f2e54128110643ccc93ab818d4affdb203b082a
Aleksey Yablokov <ya_al@bk.ru>
2017-10-10 23:00:14 +0300

    weight

--- Groovy+/Scripts/weight.groovy
+++ Groovy+/Scripts/weight.groovy
@@ -1,3 +1,6 @@
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.text.NumberFormat
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
@@ -7,23 +10,40 @@ startDate = LocalDate.of(2017, 9, 17)
 finishDate = LocalDate.of(2017, 12, 31)
 daysTotal = ChronoUnit.DAYS.between(startDate, finishDate)
 daysPast = ChronoUnit.DAYS.between(startDate, currentDate)
-daysPastPersent = Math.round(daysPast * 100 * 10 / daysTotal) / 10
-daysPastPersetTxt = daysPastPersent.toString().replace(".", ",")
+daysPastPersent = daysPast / daysTotal
 
-persentPast =
 startWeight = 86.5
 finishWeight = 70.0
 weightTotal = startWeight - finishWeight
-currentWeight = args[0].toDouble()
+currentWeight = args[1].toDouble()
+previousWeight = args[0].toDouble()
 weightChange = startWeight - currentWeight
-wightPersent = Math.round(weightChange * 10 / weightTotal) / 10
+wightPersent = weightChange / weightTotal
+weightChangeDaily = currentWeight - previousWeight
+
+ruRu = new Locale("ru", "RU")
 
 formatter = DateTimeFormatter.ofPattern("dd.MM.yy")
 currentDateText = currentDate.format(formatter)
 
-message = """${currentDateText} / прошло ${daysPast} из ${daysTotal} дней (${daysPastPersent}%) / сброшено ${
-    weightChange
-} из ${weightTotal} кг (${wightPersent}%)
-Вес: ${currentWeight} кг, ${weightChange} кг
+percentFormat = NumberFormat.getPercentInstance(ruRu)
+percentFormat.setMaximumFractionDigits(1)
+percentFormat.setMinimumFractionDigits(1)
+daysPastPercetTxt=percentFormat.format(daysPastPersent)
+wightPersentTxt = percentFormat.format(wightPersent)
+
+decimalFormat = new DecimalFormat("##0.0", DecimalFormatSymbols.getInstance(ruRu))
+weightChangeTxt = decimalFormat.format(weightChange)
+weightTotalTxt = decimalFormat.format(weightTotal)
+previousWeightTxt = decimalFormat.format(previousWeight)
+currentWeightTxt = decimalFormat.format(currentWeight)
+weightChangeDailyTxt = (weightChangeDaily > 0 ? "+" : "") + decimalFormat.format(weightChangeDaily)
+
+message = """
+${currentDateText} 
+прошло ${daysPast} из ${daysTotal} дней (${daysPastPercetTxt}) 
+сброшено ${weightChangeTxt} из ${weightTotalTxt} кг (${wightPersentTxt})
+Вес: ${previousWeightTxt} -> ${currentWeightTxt} кг / ${weightChangeDailyTxt} кг
+Тренировка вчера:
 """
 println(message)
