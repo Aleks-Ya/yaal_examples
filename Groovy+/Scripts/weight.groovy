e7e01d97886dfe9066b2beda8a003315aa7e0a94
Aleksey Yablokov <ya_al@bk.ru>
2017-10-11 21:39:13 +0300

    update weight

--- Groovy+/Scripts/weight.groovy
+++ Groovy+/Scripts/weight.groovy
@@ -3,24 +3,34 @@ import java.text.DecimalFormatSymbols
 import java.text.NumberFormat
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
-import java.time.temporal.ChronoUnit
 
+import static java.time.temporal.ChronoUnit.DAYS
+
+//Parameters
+currentWeight = args[1].toDouble()
+previousWeight = args[0].toDouble()
+
+//Date
 currentDate = LocalDate.now().minusDays(1)
 startDate = LocalDate.of(2017, 9, 17)
 finishDate = LocalDate.of(2017, 12, 31)
-daysTotal = ChronoUnit.DAYS.between(startDate, finishDate)
-daysPast = ChronoUnit.DAYS.between(startDate, currentDate)
+daysTotal = DAYS.between(startDate, finishDate)
+daysPast = DAYS.between(startDate, currentDate)
+daysRemain = DAYS.between(currentDate, finishDate)
 daysPastPersent = daysPast / daysTotal
 
+//Weight
 startWeight = 86.5
 finishWeight = 70.0
 weightTotal = startWeight - finishWeight
-currentWeight = args[1].toDouble()
-previousWeight = args[0].toDouble()
+weightRemain = currentWeight - finishWeight
 weightChange = startWeight - currentWeight
 wightPersent = weightChange / weightTotal
 weightChangeDaily = currentWeight - previousWeight
+weightPerDay = weightRemain / daysRemain
+weightPerWeek = weightPerDay * 7
 
+//Text
 ruRu = new Locale("ru", "RU")
 
 formatter = DateTimeFormatter.ofPattern("dd.MM.yy")
@@ -32,18 +42,24 @@ percentFormat.setMinimumFractionDigits(1)
 daysPastPercetTxt=percentFormat.format(daysPastPersent)
 wightPersentTxt = percentFormat.format(wightPersent)
 
-decimalFormat = new DecimalFormat("##0.0", DecimalFormatSymbols.getInstance(ruRu))
-weightChangeTxt = decimalFormat.format(weightChange)
-weightTotalTxt = decimalFormat.format(weightTotal)
-previousWeightTxt = decimalFormat.format(previousWeight)
-currentWeightTxt = decimalFormat.format(currentWeight)
-weightChangeDailyTxt = (weightChangeDaily > 0 ? "+" : "") + decimalFormat.format(weightChangeDaily)
+decimalFormat1 = new DecimalFormat("##0.0", DecimalFormatSymbols.getInstance(ruRu))
+decimalFormat2 = new DecimalFormat("##0.00", DecimalFormatSymbols.getInstance(ruRu))
+weightChangeTxt = decimalFormat1.format(weightChange)
+weightTotalTxt = decimalFormat1.format(weightTotal)
+previousWeightTxt = decimalFormat1.format(previousWeight)
+currentWeightTxt = decimalFormat1.format(currentWeight)
+weightChangeDailyTxt = (weightChangeDaily > 0 ? "+" : "") + decimalFormat1.format(weightChangeDaily)
+weightPerDayTxt = decimalFormat2.format(weightPerDay)
+weightPerWeekTxt = decimalFormat2.format(weightPerWeek)
 
-message = """
+//Output
+println """
 ${currentDateText} 
 прошло ${daysPast} из ${daysTotal} дней (${daysPastPercetTxt}) 
 сброшено ${weightChangeTxt} из ${weightTotalTxt} кг (${wightPersentTxt})
 Вес: ${previousWeightTxt} -> ${currentWeightTxt} кг / ${weightChangeDailyTxt} кг
+Чтобы успеть, нужно скидывать по: ${weightPerWeekTxt} кг в неделю, по ${weightPerDayTxt} кг в день
 Тренировка вчера:
+Еда до тренировки:
+Еда после тренировки:
 """
-println(message)
