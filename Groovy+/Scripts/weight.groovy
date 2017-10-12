bdbbeacdb378c8445d51f5bfc3afcd8bd8939764
Aleksey Yablokov <ya_al@bk.ru>
2017-10-12 21:37:12 +0300

    write weight to file

--- Groovy+/Scripts/weight.groovy
+++ Groovy+/Scripts/weight.groovy
@@ -1,3 +1,5 @@
+import java.nio.file.Files
+import java.nio.file.StandardOpenOption
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
 import java.text.NumberFormat
@@ -6,18 +8,39 @@ import java.time.format.DateTimeFormatter
 
 import static java.time.temporal.ChronoUnit.DAYS
 
-//Parameters
-currentWeight = args[1].toDouble()
-previousWeight = args[0].toDouble()
-
 //Date
-currentDate = LocalDate.now().minusDays(1)
 startDate = LocalDate.of(2017, 9, 17)
 finishDate = LocalDate.of(2017, 12, 31)
+currentDate = LocalDate.now().minusDays(1)
+dayBeforeDate = currentDate.minusDays(1)
 daysTotal = DAYS.between(startDate, finishDate)
 daysPast = DAYS.between(startDate, currentDate)
 daysRemain = DAYS.between(currentDate, finishDate)
 daysPastPersent = daysPast / daysTotal
+
+//File
+homeDir = System.getProperty('user.home')
+File file = new File(homeDir, "weight.txt")
+
+//Parameters
+if (args.length == 2) {
+    currentWeight = args[1].toDouble()
+    previousWeight = args[0].toDouble()
+} else if (args.length == 1) {
+    currentWeight = args[0].toDouble()
+
+    lines = Files.readAllLines(file.toPath()).reverse()
+    previousWeight = lines.stream()
+            .map {line -> line.split(" ")}
+            .map {array -> new Tuple2<LocalDate, Double>(LocalDate.parse(array[0]), Double.parseDouble(array[1]))}
+            .filter {tuple -> (tuple.first() == dayBeforeDate) }
+            .map {tuple -> tuple.second}
+            .findAny().orElseThrow{new IllegalStateException("Can't find weight for date: $dayBeforeDate in file ${file.absolutePath}.")}
+} else {
+    throw new IllegalArgumentException("Expected one or two arguments. But got: ${args}")
+}
+
+Files.write(file.toPath(), ["$currentDate $currentWeight"], StandardOpenOption.APPEND, StandardOpenOption.CREATE)
 
 //Weight
 startWeight = 86.5
