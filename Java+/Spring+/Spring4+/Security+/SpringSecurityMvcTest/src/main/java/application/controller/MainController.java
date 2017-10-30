9c8e12f8cd842a99cd1fd45638dfb0324460378e
Aleksey Yablokov <ya_al@bk.ru>
2017-10-30 17:48:40 +0300

    cleanup MainController

--- Java+/Spring+/Spring4+/Security+/SpringSecurityMvcTest/src/main/java/application/controller/MainController.java
+++ Java+/Spring+/Spring4+/Security+/SpringSecurityMvcTest/src/main/java/application/controller/MainController.java
@@ -8,49 +8,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ResponseBody
 public class MainController {
 
-//    @RequestMapping("/")
-//    public String root() {
-//        return "root page";
-//    }
-//
-//    @RequestMapping("/info")
-//    public String info() {
-//        return "info page";
-//    }
-//
-//    @RequestMapping("/user")
-//    public String userAccessible() {
-//        return "This page is available to User";
-//    }
-//
+    public static final String ADMIN_BODY = "This page is available to Admin";
+    public static final String ANONYMOUS_BODY = "This page is available to anyone";
+
     @RequestMapping("/admin")
     public String adminAccessible() {
         return ADMIN_BODY;
     }
 
-    public static final  String ADMIN_BODY = "This page is available to Admin";
-        public static final String ANONYMOUS_BODY = "This page is available to anyone";
     @RequestMapping("/anonymous")
     public String anonymous() {
         return ANONYMOUS_BODY;
     }
 
-//    @RequestMapping("/username")
-//    public String username() {
-//        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//
-//        String username;
-//        if (principal instanceof UserDetails) {
-//            username = ((UserDetails) principal).getUsername();
-//        } else {
-//            username = principal.toString();
-//        }
-//        return "username=" + username;
-//    }
-//
-//    @RequestMapping("/principal")
-//    public String principal() {
-//        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        return "principal=" + principal;
-//    }
 }
