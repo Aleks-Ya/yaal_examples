9cc0fe772f541ef286a958cf101679606dfff5ae
Aleksey Yablokov <ya_al@bk.ru>
2017-11-02 15:26:29 +0300

    add StringController

---
+++ Java+/Spring+/Spring4+/Web+/MVC+/Controller/src/test/java/controller/message/converter/StringControllerTest.java
@@ -0,0 +1,43 @@
+package controller.message.converter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = StringController.class)
+public class StringControllerTest {
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void requestParam() throws Exception {
+        mvc.perform(
+                post(StringController.ENDPOINT)
+                        .contentType("text/plain")
+                        .content("the_body")
+        )
+                .andExpect(status().isOk())
+                .andExpect(content().string("body=the_body"));
+    }
+}
