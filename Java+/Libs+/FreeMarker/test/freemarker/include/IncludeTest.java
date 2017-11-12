5444cc063e72be8d6388ffd3230d9d45384fe022
Aleksey Yablokov <ya_al@bk.ru>
2017-11-12 20:25:17 +0300

    add IncludeTest

---
+++ Java+/Libs+/FreeMarker/test/freemarker/include/IncludeTest.java
@@ -0,0 +1,36 @@
+package freemarker.include;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class IncludeTest {
+    private static Configuration cfg;
+
+    @BeforeClass
+    public static void setUp() {
+        cfg = new Configuration(Configuration.VERSION_2_3_22);
+        cfg.setClassForTemplateLoading(IncludeTest.class, "");
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+    }
+
+    @Test
+    public void include() throws TemplateException, IOException {
+        Template template = cfg.getTemplate("outer.ftl");
+        Writer out = new StringWriter();
+        template.process(null, out);
+        assertThat(out.toString(), equalTo("outer-inner"));
+    }
+
+}
