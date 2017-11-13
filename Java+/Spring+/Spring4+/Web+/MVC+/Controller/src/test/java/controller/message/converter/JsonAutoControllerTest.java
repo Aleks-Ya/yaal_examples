86931d067241ce146311d291a1d82f665dc3455e
Aleksey Yablokov <ya_al@bk.ru>
2017-11-13 10:15:58 +0300

    simplify JsonAutoControllerTest

--- Java+/Spring+/Spring4+/Web+/MVC+/Controller/src/test/java/controller/message/converter/JsonAutoControllerTest.java
+++ Java+/Spring+/Spring4+/Web+/MVC+/Controller/src/test/java/controller/message/converter/JsonAutoControllerTest.java
@@ -6,8 +6,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -15,9 +13,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -48,20 +43,11 @@ public class JsonAutoControllerTest {
                         .content("{ \"name\": \"abc\"}")
         )
                 .andExpect(status().isOk())
-                .andExpect(content().string("\"response=abc\""));
+                .andExpect(content().string("response=abc"));
     }
 
     @EnableWebMvc
     @Configuration
-    static class Config extends WebMvcConfigurerAdapter {
-
-        @Override
-        public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-            HttpMessageConverter<?> httpMessageConverter = converters.stream()
-                    .filter(converter -> converter instanceof MappingJackson2HttpMessageConverter)
-                    .findAny().get();
-            converters.remove(httpMessageConverter);
-            converters.add(0, httpMessageConverter);
-        }
+    static class Config {
     }
 }
