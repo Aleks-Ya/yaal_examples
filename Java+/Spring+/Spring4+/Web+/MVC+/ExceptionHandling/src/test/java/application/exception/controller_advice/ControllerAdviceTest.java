afc92636cfc0f904db1043e6443b159dd104b71c
Aleksey Yablokov <ya_al@bk.ru>
2017-11-15 14:59:46 +0300

    add @ControllerAdvice

---
+++ Java+/Spring+/Spring4+/Web+/MVC+/ExceptionHandling/src/test/java/application/exception/controller_advice/ControllerAdviceTest.java
@@ -0,0 +1,40 @@
+package application.exception.controller_advice;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {Config.class, ControllerAdviceComponent.class, TheController.class})
+public class ControllerAdviceTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void responseStatus() throws Exception {
+        mvc.perform(get(TheController.ENDPOINT))
+                .andExpect(content().string(TheController.EXCEPTION_MESSAGE))
+                .andExpect(status().is(HttpStatus.I_AM_A_TEAPOT.value()));
+    }
+}
