41fbd913fe6883af75fb0f2cdbca8715854004a1
Aleksey Yablokov <ya_al@bk.ru>
2017-11-15 06:50:33 +0300

    add pdf-test

---
+++ Java+/UnitTest+/Hamcrest+/PdfTest/test/pdf/PdfAssert.java
@@ -0,0 +1,21 @@
+package pdf;
+
+import com.codeborne.pdftest.PDF;
+import org.junit.Test;
+
+import java.io.File;
+import java.net.URL;
+
+import static com.codeborne.pdftest.PDF.containsText;
+import static org.junit.Assert.assertThat;
+
+public class PdfAssert {
+
+    @Test
+    public void canAssertThatPdfContainsText() {
+        URL pdfFile = PdfAssert.class.getResource("hello_world.pdf");
+        PDF pdf = new PDF(pdfFile);
+        assertThat(pdf, containsText("Hello World!"));
+    }
+
+}
