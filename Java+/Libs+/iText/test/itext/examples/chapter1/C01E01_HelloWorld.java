dcfd7e8e7a7d8b93060b79284e0bc82bdb23a1ef
Aleksey Yablokov <ya_al@bk.ru>
2017-11-15 06:30:00 +0300

    add iText

---
+++ Java+/Libs+/iText/test/itext/examples/chapter1/C01E01_HelloWorld.java
@@ -0,0 +1,43 @@
+package itext.examples.chapter1;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.test.annotations.WrapToTest;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Simple Hello World example.
+ * Src: https://developers.itextpdf.com/content/itext-7-jump-start-tutorial/examples/chapter-1#1723-c01e01_helloworld.java
+ */
+@WrapToTest
+public class C01E01_HelloWorld {
+
+    public static final String DEST = "results/chapter01/hello_world.pdf";
+
+    public static void main(String args[]) throws IOException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new C01E01_HelloWorld().createPdf(DEST);
+    }
+
+    public void createPdf(String dest) throws IOException {
+        //Initialize PDF writer
+        PdfWriter writer = new PdfWriter(dest);
+
+        //Initialize PDF document
+        PdfDocument pdf = new PdfDocument(writer);
+
+        // Initialize document
+        Document document = new Document(pdf);
+
+        //Add paragraph to the document
+        document.add(new Paragraph("Hello World!"));
+
+        //Close document
+        document.close();
+    }
+}
---
+++ Java+/Libs+/iText/test/itext/examples/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Official examples from https://developers.itextpdf.com/content/itext-7-jump-start-tutorial/examples
+ */
+package itext.examples;
