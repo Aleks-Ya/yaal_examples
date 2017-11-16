238d909d28bcc8242e79e837eada45924d333463
Aleksey Yablokov <ya_al@bk.ru>
2017-11-16 14:00:19 +0300

    add YAML Jackson

---
+++ Java+/Libs+/YAML+/JacksonFasterXml/test/yaml/ParseTest.java
@@ -0,0 +1,45 @@
+package yaml;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class ParseTest {
+
+    @Test
+    public void name() throws Exception {
+        String yaml = "name: John\n" +
+                "age: 30";
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        Person person = mapper.readValue(yaml, Person.class);
+
+        assertThat(person.getName(), equalTo("John"));
+        assertThat(person.getAge(), equalTo(30));
+    }
+
+    private static class Person {
+        private String name;
+        private Integer age;
+
+        String getName() {
+            return name;
+        }
+
+        @SuppressWarnings("unused")
+        void setName(String name) {
+            this.name = name;
+        }
+
+        Integer getAge() {
+            return age;
+        }
+
+        @SuppressWarnings("unused")
+        void setAge(Integer age) {
+            this.age = age;
+        }
+    }
+}
