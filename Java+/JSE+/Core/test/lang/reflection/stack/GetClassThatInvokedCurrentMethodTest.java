9bbd27c06251c6a99dee27e567a2a40c64a22533
Aleksey Yablokov <ya_al@bk.ru>
2017-12-04 15:05:10 +0300

    add GetClassThatInvokedCurrentMethodTest

---
+++ Java+/JSE+/Core/test/lang/reflection/stack/GetClassThatInvokedCurrentMethodTest.java
@@ -0,0 +1,41 @@
+package lang.reflection.stack;
+
+import org.hamcrest.Matchers;
+import org.junit.Ignore;
+import org.junit.Test;
+import sun.reflect.Reflection;
+
+import static org.junit.Assert.assertThat;
+
+/**
+ * Get class that invoked the current method.
+ */
+public class GetClassThatInvokedCurrentMethodTest {
+
+    private static Class<?> getCallerClass() {
+        return Reflection.getCallerClass(2);
+    }
+
+    @Test
+    @Ignore("Require Java 9")
+    public void stackWalker() {
+//        StackWalker walker = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
+//        Optional<Class<?>> callerClass = walker.walk(s ->
+//                s.map(StackWalker.StackFrame::getDeclaringClass)
+//                        .filter(interestingClasses::contains)
+//                        .findFirst());
+    }
+
+    @Test
+    @Ignore("doesn't work")
+    public void sunReflectReflection() {
+        Class<?> callerClass = TheCaller.call();
+        assertThat(callerClass, Matchers.is(TheCaller.class));
+    }
+
+    private static class TheCaller {
+        static Class<?> call() {
+            return getCallerClass();
+        }
+    }
+}
