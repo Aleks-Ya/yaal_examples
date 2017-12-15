fb865f1800923168ffb800fe0c548ee1fc3151e0
Aleksey Yablokov <ya_al@bk.ru>
2017-12-15 16:04:14 +0300

    add PerformingRequests#countSearch

--- BigData+/ElasticSearch+/JavaLowLevelRestClient/src/test/java/elastic/PerformingRequests.java
+++ BigData+/ElasticSearch+/JavaLowLevelRestClient/src/test/java/elastic/PerformingRequests.java
@@ -1,12 +1,16 @@
 package elastic;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.entity.ContentType;
+import org.apache.http.nio.entity.NStringEntity;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collections;
+import java.util.Map;
 
 import static java.lang.String.format;
 import static org.hamcrest.CoreMatchers.containsString;
@@ -44,7 +48,7 @@ public class PerformingRequests {
     }
 
     @Test
-    public void count() throws IOException {
+    public void countAll() throws IOException {
         String endpoint = format("%s/%s/_count", PEOPLE_INDEX, PERSONS_TYPE);
         Response response = client.performRequest("GET", endpoint);
         client.close();
@@ -55,7 +59,31 @@ public class PerformingRequests {
         InputStream contentIS = entity.getContent();
         String contentStr = StringHelper.inputStreamToString(contentIS);
         System.out.println("Body: " + contentStr);
-        assertThat(contentStr, containsString("\"total\":5"));
+        assertThat(contentStr, containsString("\"count\":4"));
+    }
+
+    @Test
+    public void countSearch() throws IOException {
+        String endpoint = format("%s/%s/_count", PEOPLE_INDEX, PERSONS_TYPE);
+        Map<String, String> params = Collections.emptyMap();
+        String body = "{\n" +
+                "  \"query\": {\n" +
+                "      \"term\": {\n" +
+                "          \"email\": \"john@mail.ru\"\n" +
+                "      }\n" +
+                "  }\n" +
+                "}";
+        HttpEntity requestEntity = new NStringEntity(body, ContentType.APPLICATION_JSON);
+        Response response = client.performRequest("GET", endpoint, params, requestEntity);
+        client.close();
+        assertThat(response.getStatusLine().getStatusCode(), equalTo(200));
+
+        System.out.println("Response: " + response);
+        HttpEntity responseEntity = response.getEntity();
+        InputStream contentIS = responseEntity.getContent();
+        String contentStr = StringHelper.inputStreamToString(contentIS);
+        System.out.println("Body: " + contentStr);
+        assertThat(contentStr, containsString("\"count\":1"));
     }
 
 }
