2a9a58857030cab49d67303d12ede9e7fa3c8088
Aleksey Yablokov <ya_al@bk.ru>
2017-12-15 15:48:53 +0300

    add PerformingRequests.count

--- BigData+/ElasticSearch+/JavaLowLevelRestClient/src/test/java/elastic/PerformingRequests.java
+++ BigData+/ElasticSearch+/JavaLowLevelRestClient/src/test/java/elastic/PerformingRequests.java
@@ -8,15 +8,21 @@ import org.junit.Test;
 import java.io.IOException;
 import java.io.InputStream;
 
+import static java.lang.String.format;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PerformingRequests {
+    private static final String PEOPLE_INDEX = "people";
+    private static final String PERSONS_TYPE = "persons";
+    private static final String EMAIL_FILED = "email";
+    private static final String JOHN_EMAIL = "john@mail.ru";
+    private static final String MARY_MAIL = "mary@mail.ru";
+    private final RestClient client = SecurityHelper.getLowLevelRestClient();
 
     @Test
     public void getRoot() throws IOException {
-        RestClient client = SecurityHelper.getLowLevelRestClient();
-
         Response response = client.performRequest("GET", "/");
         client.close();
 
@@ -26,8 +32,6 @@ public class PerformingRequests {
 
     @Test
     public void getMapping() throws IOException {
-        RestClient client = SecurityHelper.getLowLevelRestClient();
-
         Response response = client.performRequest("GET", "/_mapping");
         client.close();
         assertThat(response.getStatusLine().getStatusCode(), equalTo(200));
@@ -37,6 +41,21 @@ public class PerformingRequests {
         InputStream contentIS = entity.getContent();
         String contentStr = StringHelper.inputStreamToString(contentIS);
         System.out.println("Body: " + contentStr);
+    }
+
+    @Test
+    public void count() throws IOException {
+        String endpoint = format("%s/%s/_count", PEOPLE_INDEX, PERSONS_TYPE);
+        Response response = client.performRequest("GET", endpoint);
+        client.close();
+        assertThat(response.getStatusLine().getStatusCode(), equalTo(200));
+
+        System.out.println("Response: " + response);
+        HttpEntity entity = response.getEntity();
+        InputStream contentIS = entity.getContent();
+        String contentStr = StringHelper.inputStreamToString(contentIS);
+        System.out.println("Body: " + contentStr);
+        assertThat(contentStr, containsString("\"total\":5"));
     }
 
 }
