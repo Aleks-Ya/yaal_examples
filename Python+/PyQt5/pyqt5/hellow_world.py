a6cf21f0a3cd5f1bcac524ebac719e7c27ef35a2
Aleksey Yablokov <ya_al@bk.ru>
2018-01-02 12:15:28 +0300

    add PyQt5

---
+++ Python+/PyQt5/pyqt5/hellow_world.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget
+from PyQt5.QtCore import QSize
+
+
+class HelloWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        self.setMinimumSize(QSize(640, 480))
+        self.setWindowTitle("Hello world")
+
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+
+        grid_layout = QGridLayout(self)
+        central_widget.setLayout(grid_layout)
+
+        title = QLabel("Hello World from PyQt", self)
+        title.setAlignment(QtCore.Qt.AlignCenter)
+        grid_layout.addWidget(title, 0, 0)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = HelloWindow()
+    mainWin.show()
+    sys.exit(app.exec_())
