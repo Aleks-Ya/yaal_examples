efa0334bf97db920d110067566f07ff89c34ef8a
Aleksey Yablokov <ya_al@bk.ru>
2018-01-14 00:17:27 +0300

    add RandomStringUtilsTest

---
+++ Java+/Libs+/ApacheCommons+/Lang/test/RandomStringUtilsTest.java
@@ -0,0 +1,18 @@
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Test;
+
+public class RandomStringUtilsTest {
+
+    @Test
+    public void random() {
+        String random = RandomStringUtils.random(20);
+        System.out.println(random);
+    }
+
+    @Test
+    public void random2() {
+        String random = RandomStringUtils.random(20, true, true);
+        System.out.println(random);
+    }
+
+}
