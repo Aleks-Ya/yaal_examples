cd361bcd69c4b548e763cb58c175cca24fc73a95
Aleksey Yablokov <ya_al@bk.ru>
2018-02-15 10:08:16 +0300

    Java: add GetOrDefaultTest

---
+++ Java+/JSE+/Core/test/util/collection/map/GetOrDefaultTest.java
@@ -0,0 +1,34 @@
+package util.collection.map;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Using Map#getOrDefault.
+ */
+@SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
+public class GetOrDefaultTest {
+
+    @Test
+    public void getOrDefault() {
+        Map<String, Integer> map = new HashMap<>();
+        int expValue = 100;
+        Integer actValue = map.getOrDefault("a", expValue);
+        assertThat(actValue, equalTo(expValue));
+    }
+
+    @Test
+    public void getOrDefaultCapture() {
+        Map<String, ?> map = new HashMap<>();
+        Integer expValue = 100;
+//        Integer actValue = map.getOrDefault("a", expValue); // not compile
+        Object value = map.get("a");
+        Integer actValue = value != null ? (Integer) value : expValue;
+        assertThat(actValue, equalTo(expValue));
+    }
+}
