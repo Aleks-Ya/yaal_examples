aa6139b018a1fa07ba4f4e64809c4560f0417ca3
Aleksei Iablokov <aleksei_iablokov@epam.com>
2018-06-25 12:51:08 +0300

    Guava: add FileToStringTest

---
+++ Java+/Libs+/Guava/test/file/FileToStringTest.java
@@ -0,0 +1,27 @@
+package file;
+
+import com.google.common.io.Files;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class FileToStringTest {
+
+    @Test
+    public void readFileToString() throws IOException {
+        Path f = java.nio.file.Files.createTempFile(FileToStringTest.class.getName() + "_", ".tmp");
+        String expContent = "abcd";
+        java.nio.file.Files.write(f, expContent.getBytes());
+
+        File actFile = f.toFile();
+        String content = Files.asCharSource(actFile, Charset.defaultCharset()).read();
+
+        assertThat(content, equalTo(expContent));
+    }
+}
