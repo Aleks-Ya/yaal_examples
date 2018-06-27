f681816149b609a53ac2ac69652c357837e6795e
Aleksey Yablokov <ya_al@bk.ru>
2018-06-27 04:00:52 +0300

    ZooKeeper: improve ServerTest

--- BigData+/ZooKeeper+/ZooKeeperUnitTest/src/test/java/zookeeper/ServerTest.java
+++ BigData+/ZooKeeper+/ZooKeeperUnitTest/src/test/java/zookeeper/ServerTest.java
@@ -1,22 +1,34 @@
 package zookeeper;
 
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.server.ServerConfig;
 import org.apache.zookeeper.server.ZooKeeperServerMain;
 import org.apache.zookeeper.server.quorum.QuorumPeerConfig;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Properties;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+/**
+ * Use ZooKeeperServerMain to run ZooKeeper (without Curator).
+ */
 class ServerTest {
 
     @Test
-    void minimal() {
-        File dir = new File(System.getProperty("java.io.tmpdir"), "zookeeper").getAbsoluteFile();
+    void minimal() throws IOException, KeeperException, InterruptedException {
+        Path dir = Files.createTempDirectory("ZooKeeper_");
         ZooKeeperServerMain zooKeeperServer = new ZooKeeperServerMain();
         final ServerConfig configuration = new ServerConfig();
-        configuration.parse(new String[]{"2199", dir.getAbsolutePath()});
+        String port = "2199";
+        configuration.parse(new String[]{port, dir.toString()});
 
         new Thread(() -> {
             try {
@@ -25,14 +37,24 @@ class ServerTest {
                 System.err.println(("ZooKeeper Failed" + e));
             }
         }).start();
+        Thread.sleep(500);
+
+        Watcher watcher = event -> System.out.println("Watcher!");
+        ZooKeeper zk = new ZooKeeper("localhost:" + port, 3000, watcher);
+
+        String node = "/my";
+        byte[] expData = new byte[]{1, 2, 3};
+        zk.create(node, expData, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        byte[] actData = zk.getData(node, true, zk.exists(node, true));
+        assertArrayEquals(expData, actData);
     }
 
     @Test
-    void withQuorum() {
-        File dir = new File(System.getProperty("java.io.tmpdir"), "zookeeper").getAbsoluteFile();
+    void withQuorum() throws IOException {
+        Path dir = Files.createTempDirectory("ZooKeeper_");
 
         Properties startupProperties = new Properties();
-        startupProperties.setProperty("dataDir", dir.getAbsolutePath());
+        startupProperties.setProperty("dataDir", dir.toString());
         startupProperties.setProperty("clientPort", "21818");
 
         QuorumPeerConfig quorumConfiguration = new QuorumPeerConfig();
