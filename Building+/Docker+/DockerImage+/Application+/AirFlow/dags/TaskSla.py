3376398b74d9ce28b26c6adf20f9a374a4299d66
Aleksey Yablokov <ya_al@bk.ru>
2018-07-07 02:13:07 +0300

    AirFlow: add TaskTimeout and TaskSla

---
+++ Building+/Docker+/DockerImage+/Application+/AirFlow/dags/TaskSla.py
@@ -0,0 +1,32 @@
+"""
+NOT FINISHED
+Exceed a task SLA.
+API: https://airflow.apache.org/code.html#airflow.models.BaseOperator
+"""
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+import time
+
+default_args = {
+    'start_date': datetime.now()
+}
+
+def print_sla_miss(dag, task_list, blocking_task_list, slas, blocking_tis):
+    print("SLA was missed on DAG %(dag)s by task id %(blocking_tis)s with task list %(task_list)s which are blocking %(blocking_task_list)s")
+
+dag = DAG(
+    'task_sla',
+    default_args=default_args,
+    sla_miss_callback=print_sla_miss,
+    schedule_interval='@once')
+
+def sleep():
+    print("Spleeping 5 seconds...")
+    time.sleep(5)
+
+sla_exceed_task = PythonOperator(
+    task_id='sla_exceed_task',
+    python_callable=sleep,
+    dag=dag,
+    sla=timedelta(seconds=3))
---
+++ Building+/Docker+/DockerImage+/Application+/AirFlow/dags/TaskTimeout.py
@@ -0,0 +1,26 @@
+"""
+Exceed a task timeout.
+API: https://airflow.apache.org/code.html#airflow.models.BaseOperator
+"""
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+import time
+
+default_args = {
+    'start_date': datetime.now()
+}
+
+dag = DAG(
+    'task_timeout',
+    default_args=default_args)
+
+def sleep():
+    print("Spleeping 5 seconds...")
+    time.sleep(5)
+
+timeout_exceed_task = PythonOperator(
+    task_id='timeout_exceed_task',
+    python_callable=sleep,
+    dag=dag,
+    execution_timeout=timedelta(seconds=3))
