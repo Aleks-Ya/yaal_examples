c580d7ebd098b70b7d1c99153f63b443ea2425dd
Aleksei Iablokov <aleksei_iablokov@epam.com>
2018-08-06 17:35:07 +0300

    Python: fix string_to_date.py

--- Python+/Python3/module/datetime/string_to_date.py
+++ Python+/Python3/module/datetime/string_to_date.py
@@ -7,6 +7,6 @@ print(datetime_object)
 assert datetime_object == datetime.datetime(2005, 6, 1, 13, 33)
 
 # To date
-datetime_object = datetime.datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
-print(datetime_object)
-assert datetime_object == datetime.datetime(2005, 6, 1, 13, 33)
+date_object = datetime.datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p').date()
+print(date_object)
+assert date_object == datetime.date(2005, 6, 1)
