35f12bfc7cdc39a75bcbc5a8ef23bf0f4275c03b
Aleksei Iablokov <aleksei_iablokov@epam.com>
2018-08-28 15:16:06 +0300

    Python: convert_numbers.py, is.py

--- Python+/Python3/module/builtins/number/convert_numbers.py
+++ Python+/Python3/module/builtins/number/convert_numbers.py
@@ -3,3 +3,12 @@
 # float to int
 i = int(123.4)
 assert i == 123
+
+# Not a number exception
+success = False
+try:
+    n = 'abc'
+    int(n)
+except ValueError as e:
+    success = True
+assert success
--- Python+/Python3/module/builtins/number/is.py
+++ Python+/Python3/module/builtins/number/is.py
@@ -4,3 +4,12 @@
 f = 123.4
 res = f.is_integer()
 assert not res
+
+# Check variable contains a float
+f = 1.1
+is_float = isinstance(f, float)
+assert is_float
+
+i = 1
+is_float = isinstance(i, float)
+assert not is_float
