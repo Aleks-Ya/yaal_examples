7f7aa0d7227632fe7f91b6161ebee2c9704ecdd4
Aleksei Iablokov <aleksei_iablokov@epam.com>
2018-09-02 08:00:19 +0300

    Scala: Scalatest: SequencesTest, SeqInspectorTest

--- Scala+/Scalatest/src/test/scala/matcher/collections/SequencesTest.scala
+++ Scala+/Scalatest/src/test/scala/matcher/collections/SequencesTest.scala
@@ -5,13 +5,17 @@ import org.scalatest.{FlatSpec, Matchers}
 class SequencesTest extends FlatSpec with Matchers {
 
   "sequences with integers" should "work" in {
-    List(1, 2, 2, 3, 3, 3) should contain inOrderOnly(1, 2, 3)
-    List(1, 2, 2, 3, 3, 3) should contain(1)
+    val list = List(1, 2, 2, 3, 3, 3)
+    list should contain inOrderOnly(1, 2, 3)
+    list should contain(1)
+    list should contain allOf(1, 2)
   }
 
   "sequences with strings" should "work" in {
-    List("a", "b", "c") should contain inOrderOnly("a", "b", "c")
-    List("a", "b", "c") should contain("b")
+    val list = List("a", "b", "c")
+    list should contain inOrderOnly("a", "b", "c")
+    list should contain("b")
+    list should contain allOf("a", "b")
   }
 
 }
