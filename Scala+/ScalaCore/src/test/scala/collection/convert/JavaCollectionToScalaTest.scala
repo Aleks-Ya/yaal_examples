6c45fbffa9d065559d4f79bd4c8c7cc80f687cee
Aleksei Iablokov <aleksei_iablokov@epam.com>
2018-09-02 07:59:39 +0300

    Scala: JavaCollectionToScalaTest, MultiTraitsTest, ListCreateTest

---
+++ Scala+/ScalaCore/src/test/scala/collection/convert/JavaCollectionToScalaTest.scala
@@ -0,0 +1,15 @@
+package collection.convert
+
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.collection.JavaConverters._
+
+class JavaCollectionToScalaTest extends FlatSpec with Matchers {
+
+  it should "convert a Java list to a Scala list" in {
+    val javaList = java.util.Arrays.asList(1, 2)
+    val scalaList = javaList.asScala
+    scalaList should contain inOrder(1, 2)
+  }
+
+}
