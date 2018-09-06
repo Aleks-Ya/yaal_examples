20aba77818e297c1107728408561559559b9921b
Aleksei Iablokov <aleksei_iablokov@epam.com>
2018-09-06 15:36:55 +0300

    Java: add WrongDateComparing

---
+++ Java+/JSE+/Core/test/util/date/WrongDateComparing.java
@@ -0,0 +1,22 @@
+package util.date;
+
+import org.junit.Test;
+
+import java.sql.Date;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Different dates are equal.
+ */
+public class WrongDateComparing {
+
+
+    @Test
+    public void defaultLocal() {
+        Date d1 = Date.valueOf("1582-10-10");
+        Date d2 = Date.valueOf("1582-10-20");
+        assertEquals(d1, d2);
+    }
+
+}
