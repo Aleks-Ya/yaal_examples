6368ae98ab1b6fd7acf4496b5c3f6f0ddd845b7f
Aleksei Iablokov <aleksei_iablokov@epam.com>
2018-10-08 12:49:36 +0300

    Python: command_line_arguments.py

--- Python+/Python3/module/sys/command_line_arguments.py
+++ Python+/Python3/module/sys/command_line_arguments.py
@@ -1,3 +1,8 @@
+# Run: python3 command_line_arguments.py abc
 import sys
 
-print("Argument #0: " + sys.argv[0])
+script_name: str = sys.argv[0]
+assert script_name.endswith('command_line_arguments.py')
+
+arg_1: str = sys.argv[1]
+assert arg_1 == 'abc'
