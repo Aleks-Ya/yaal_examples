c3f944b62a3e504fb7011016df627c1f36eb8cd7
Aleksei Iablokov <aleksei_iablokov@epam.com>
2018-12-17 19:02:58 +0300

    Java: add MkDirsTest.java

---
+++ Java+/JSE+/Core/test/io/file/mkdir/MkDirsTest.java
@@ -0,0 +1,34 @@
+package io.file.mkdir;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.io.FileMatchers.anExistingFile;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class MkDirsTest {
+
+    /**
+     * If invoke File#mkdirs on a file, it creates a directory instead of file.
+     */
+    @Test
+    public void mkdirsOnFile() throws IOException {
+        File tmpDir = Files.createTempDirectory(getClass().getSimpleName()).toFile();
+        tmpDir.deleteOnExit();
+
+        File notExistsIntermediateDirs = new File(tmpDir, "a/b/file.txt");
+        assertThat(notExistsIntermediateDirs, not(anExistingFile()));
+
+        assertTrue(notExistsIntermediateDirs.mkdirs());
+
+        assertThat(notExistsIntermediateDirs, not(anExistingFile()));
+        assertTrue(notExistsIntermediateDirs.isDirectory());
+        assertFalse(notExistsIntermediateDirs.isFile());
+    }
+}
