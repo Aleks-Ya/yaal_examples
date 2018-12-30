811545642d03f2822b69069fd4b4cae5d00a2e0d
Aleksey Yablokov <ya_al@bk.ru>
2018-12-30 12:44:32 +0300

    Python: add lazy_variable.py

---
+++ Python+/Python3/core/variable/lazy_variable.py
@@ -0,0 +1,23 @@
+# Lazy variables
+
+
+class LazyWrapper(object):
+    def __init__(self, func):
+        self.func = func
+        self.value = None
+
+    def __call__(self):
+        if self.value is None:
+            self.value = self.func()
+        return self.value
+
+
+def initializer(number):
+    return number * 2
+
+
+lazy_wrapper = LazyWrapper(lambda: initializer(3))
+
+value = lazy_wrapper()
+
+assert value == 6
