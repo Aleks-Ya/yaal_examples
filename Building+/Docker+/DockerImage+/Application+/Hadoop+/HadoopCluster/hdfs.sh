7863a8fcfb63c96bee6d3866a65f827dc7e75ed4
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-01-02 23:21:27 +0300

    Hadoop: update HadoopCluster

--- Building+/Docker+/DockerImage+/Application+/Hadoop+/HadoopCluster/hdfs.sh
+++ Building+/Docker+/DockerImage+/Application+/Hadoop+/HadoopCluster/hdfs.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-docker build -t hadoop-cluster-client context
-docker run --rm -it --network hadoop-cluster-network --name hadoop-cluster-client hadoop-cluster-client bash
+HOST_DIR=/tmp/hadoop-cluster-master/
+CONTAINER_DIR=/tmp/host-bind/
+mkdir -p $HOST_DIR
+docker run --rm -it \
+  --network hadoop-cluster-network \
+  --name hadoop-cluster-client \
+  --mount type=bind,source=$HOST_DIR,target=$CONTAINER_DIR \
+  --workdir $CONTAINER_DIR \
+  hadoopcluster_master-service \
+  bash
