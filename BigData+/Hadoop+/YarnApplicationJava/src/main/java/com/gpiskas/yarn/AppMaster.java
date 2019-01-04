b761aea204bec02d901ca9801978931bbcadd906
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-01-04 14:41:13 +0300

    Hadoop: YarnApplicationJava: cleanup

--- BigData+/Hadoop+/YarnApplicationJava/src/main/java/com/gpiskas/yarn/AppMaster.java
+++ BigData+/Hadoop+/YarnApplicationJava/src/main/java/com/gpiskas/yarn/AppMaster.java
@@ -1,31 +1,4 @@
-/*
-* Simple YARN Application Skeleton
-* Copyright (C) 2015 George Piskas, Philémon Favrod
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License along
-* with this program; if not, write to the Free Software Foundation, Inc.,
-* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*
-* Contact: geopiskas@gmail.com
-*/
-
-
 package com.gpiskas.yarn;
-
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import org.apache.hadoop.yarn.api.ApplicationConstants;
 import org.apache.hadoop.yarn.api.records.Container;
@@ -42,126 +15,134 @@ import org.apache.hadoop.yarn.client.api.async.AMRMClientAsync;
 import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.hadoop.yarn.util.Records;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class AppMaster implements AMRMClientAsync.CallbackHandler {
 
-	private YarnConfiguration conf = new YarnConfiguration();
-	private NMClient nmClient;
-	private int containerCount = 3;
+    private YarnConfiguration conf = new YarnConfiguration();
+    private NMClient nmClient;
+    private int containerCount = 3;
 
-	public static void main(String[] args) {
-		System.out.println("AppMaster: Initializing");
-		try {
-			new AppMaster().run();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        System.out.println("AppMaster: Initializing");
+        try {
+            new AppMaster().run();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
-	public void run() throws Exception {
-		conf = new YarnConfiguration();
+    private void run() throws Exception {
+        conf = new YarnConfiguration();
 
-		// Create NM Client
-		nmClient = NMClient.createNMClient();
-		nmClient.init(conf);
-		nmClient.start();
-		
-		// Create AM - RM Client
-		AMRMClientAsync<ContainerRequest> rmClient = AMRMClientAsync.createAMRMClientAsync(1000, this);
-		rmClient.init(conf);
-		rmClient.start();
-		
-		// Register with RM
-		rmClient.registerApplicationMaster("", 0, "");
-		System.out.println("AppMaster: Registered");
+        // Create NM Client
+        nmClient = NMClient.createNMClient();
+        nmClient.init(conf);
+        nmClient.start();
 
-		// Priority for worker containers - priorities are intra-application
-		Priority priority = Records.newRecord(Priority.class);
-		priority.setPriority(0);
-		
-		// Resource requirements for worker containers
-		Resource capability = Records.newRecord(Resource.class);
-		capability.setMemory(128);
-		capability.setVirtualCores(1);
+        // Create AM - RM Client
+        AMRMClientAsync<ContainerRequest> rmClient = AMRMClientAsync.createAMRMClientAsync(1000, this);
+        rmClient.init(conf);
+        rmClient.start();
 
-		// Reqiest Containers from RM
-			System.out.println("AppMaster: Requesting " + containerCount + " Containers");
-		for (int i = 0; i < containerCount; ++i) {
-			rmClient.addContainerRequest(new ContainerRequest(capability, null, null, priority));
-		}
-		
-		while (!containersFinished()) {
-			Thread.sleep(100);
-		}
+        // Register with RM
+        rmClient.registerApplicationMaster("", 0, "");
+        System.out.println("AppMaster: Registered");
 
-		System.out.println("AppMaster: Unregistered");
-		rmClient.unregisterApplicationMaster(FinalApplicationStatus.SUCCEEDED, "", "");
-	}
+        // Priority for worker containers - priorities are intra-application
+        Priority priority = Records.newRecord(Priority.class);
+        priority.setPriority(0);
 
-	private boolean containersFinished() {
-		return containerCount == 0;
-	}
+        // Resource requirements for worker containers
+        Resource capability = Records.newRecord(Resource.class);
+        capability.setMemorySize(128);
+        capability.setVirtualCores(1);
 
-	@Override
-	public void onContainersAllocated(List<Container> containers) {
-		for (Container container : containers) {
-			try {
-				nmClient.startContainer(container, initContainer());
-				System.err.println("AppMaster: Container launched " + container.getId());
-			} catch (Exception ex) {
-				System.err.println("AppMaster: Container not launched " + container.getId());
-				ex.printStackTrace();
-			}
-		}
-	}
+        // Reqiest Containers from RM
+        System.out.println("AppMaster: Requesting " + containerCount + " Containers");
+        for (int i = 0; i < containerCount; ++i) {
+            rmClient.addContainerRequest(new ContainerRequest(capability, null, null, priority));
+        }
 
-	private ContainerLaunchContext initContainer() {
-		try {
-			// Create Container Context
-			ContainerLaunchContext cCLC = Records.newRecord(ContainerLaunchContext.class);
-			cCLC.setCommands(Collections.singletonList("$JAVA_HOME/bin/java"
-				+ " -Xmx256M"
-				+ " com.gpiskas.yarn.Container"
-				+ " 1>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stdout"
-				+ " 2>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stderr"));
+        while (!containersFinished()) {
+            Thread.sleep(100);
+        }
 
-			// Set Container jar
-			LocalResource jar = Records.newRecord(LocalResource.class);
-			Utils.setUpLocalResource(Utils.YARNAPP_JAR_PATH, jar, conf);
-			cCLC.setLocalResources(Collections.singletonMap(Utils.YARNAPP_JAR_NAME, jar));
-			
-			// Set Container CLASSPATH
-			Map<String, String> env = new HashMap<String, String>();
-			Utils.setUpEnv(env, conf);
-			cCLC.setEnvironment(env);
+        System.out.println("AppMaster: Unregistered");
+        rmClient.unregisterApplicationMaster(FinalApplicationStatus.SUCCEEDED, "", "");
+    }
 
-			return cCLC;
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			return null;
-		}
-	}
+    private boolean containersFinished() {
+        return containerCount == 0;
+    }
 
-	@Override
-	public void onContainersCompleted(List<ContainerStatus> statusOfContainers) {
-		for (ContainerStatus status : statusOfContainers) {
-			System.err.println("AppMaster: Container finished " + status.getContainerId());
-			synchronized (this) {
-				containerCount--;
-			}
-		}
-	}
+    @Override
+    public void onContainersAllocated(List<Container> containers) {
+        for (Container container : containers) {
+            try {
+                nmClient.startContainer(container, initContainer());
+                System.err.println("AppMaster: Container launched " + container.getId());
+            } catch (Exception ex) {
+                System.err.println("AppMaster: Container not launched " + container.getId());
+                ex.printStackTrace();
+            }
+        }
+    }
 
-	@Override
-	public void onError(Throwable e) {}
+    private ContainerLaunchContext initContainer() {
+        try {
+            // Create Container Context
+            ContainerLaunchContext cCLC = Records.newRecord(ContainerLaunchContext.class);
+            cCLC.setCommands(Collections.singletonList("$JAVA_HOME/bin/java"
+                    + " -Xmx256M"
+                    + " com.gpiskas.yarn.Container"
+                    + " 1>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stdout"
+                    + " 2>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stderr"));
 
-	@Override
-	public void onNodesUpdated(List<NodeReport> nodeReports) {}
+            // Set Container jar
+            LocalResource jar = Records.newRecord(LocalResource.class);
+            Utils.setUpLocalResource(Utils.YARNAPP_JAR_PATH, jar, conf);
+            cCLC.setLocalResources(Collections.singletonMap(Utils.YARNAPP_JAR_NAME, jar));
 
-	@Override
-	public void onShutdownRequest() {}
+            // Set Container CLASSPATH
+            Map<String, String> env = new HashMap<>();
+            Utils.setUpEnv(env, conf);
+            cCLC.setEnvironment(env);
 
-	@Override
-	public float getProgress() {
-		return 0;
-	}
+            return cCLC;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public void onContainersCompleted(List<ContainerStatus> statusOfContainers) {
+        for (ContainerStatus status : statusOfContainers) {
+            System.err.println("AppMaster: Container finished " + status.getContainerId());
+            synchronized (this) {
+                containerCount--;
+            }
+        }
+    }
+
+    @Override
+    public void onError(Throwable e) {
+    }
+
+    @Override
+    public void onNodesUpdated(List<NodeReport> nodeReports) {
+    }
+
+    @Override
+    public void onShutdownRequest() {
+    }
+
+    @Override
+    public float getProgress() {
+        return 0;
+    }
 }
