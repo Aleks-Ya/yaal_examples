a68c21ef65d6fa3ae2603ad05b528fc72da8bd47
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-01-06 23:12:20 +0300

    Scala: ScalaCore: add ListIterateTest.scala, IncrementNumberTest.scala

---
+++ Scala+/ScalaCore/src/test/scala/collection/list/ListIterateTest.scala
@@ -0,0 +1,14 @@
+package collection.list
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class ListIterateTest extends FlatSpec with Matchers {
+
+  it should "iterate elements of List" in {
+    val list = 1 :: 2 :: Nil
+    var sum = 0
+    list.foreach((i: Int) => sum += i)
+    sum shouldEqual 3
+  }
+
+}
