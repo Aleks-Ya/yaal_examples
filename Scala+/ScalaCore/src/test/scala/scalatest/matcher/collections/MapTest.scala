f8b2872985c467e0613bc3a9ae13b27f8ab7050f
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-01-08 03:32:04 +0300

    Scala: ScalaCore: update MapTest

--- Scala+/ScalaCore/src/test/scala/scalatest/matcher/collections/MapTest.scala
+++ Scala+/ScalaCore/src/test/scala/scalatest/matcher/collections/MapTest.scala
@@ -1,15 +1,29 @@
 package scalatest.matcher.collections
 
-import org.scalatest.{FlatSpec, Matchers}
+import java.util
+
+import org.scalatest.{Entry, FlatSpec, Matchers}
 
 class MapTest extends FlatSpec with Matchers {
 
-  "map" should "work" in {
-    val map = Map("x" -> 24, "y" -> 25)
-    map should contain key "x"
-    map should contain value 24
-    //    map should contain  (Entry("x", 24))
-    //    map should contain only (Entry("x", 24))
+  "scala map" should "work" in {
+    val scalaMap = Map("x" -> 24, "y" -> 25)
+    scalaMap should have size 2
+    scalaMap should contain key "x"
+    scalaMap should contain value 24
+  }
+
+  "java map" should "work" in {
+    val javaMap = new util.HashMap[String, Integer]()
+    javaMap.put("x", 24)
+    javaMap.put("y", 25)
+
+    javaMap should have size 2
+    javaMap should contain key "x"
+    javaMap should contain value 24
+    javaMap should contain(Entry("x", 24))
+    javaMap should contain allOf(Entry("x", 24), Entry("y", 25))
+    javaMap should contain oneOf(Entry("a", 1), Entry("x", 24))
   }
 
   it should "verify empty maps" in {
