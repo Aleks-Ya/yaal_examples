33230b36c47729991e56b7362cc7428a179c3fe4
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-01-09 01:41:08 +0300

    Hadoop: HadoopCluster: add hostnames to hosts file on the host machine

---
+++ Building+/Docker+/DockerImage+/Application+/Hadoop+/HadoopCluster/update_hosts.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Add hostnames of Docker containers to /etc/hosts.
+# Requires root privileges.
+
+set -e
+
+container_1=hadoop-cluster-master
+container_2=hadoop-cluster-slave-1
+container_3=hadoop-cluster-slave-2
+
+ip_1=$(docker exec ${container_1} hostname -I)
+ip_2=$(docker exec ${container_2} hostname -I)
+ip_3=$(docker exec ${container_3} hostname -I)
+
+hosts_file=/etc/hosts
+
+host_1=master-service
+host_2=slave-service-1
+host_3=slave-service-2
+
+sed -i "/${host_1}/d" ${hosts_file}
+sed -i "/${host_2}/d" ${hosts_file}
+sed -i "/${host_3}/d" ${hosts_file}
+
+echo "${ip_1}    ${host_1}" >> ${hosts_file}
+echo "${ip_2}    ${host_2}" >> ${hosts_file}
+echo "${ip_3}    ${host_3}" >> ${hosts_file}
