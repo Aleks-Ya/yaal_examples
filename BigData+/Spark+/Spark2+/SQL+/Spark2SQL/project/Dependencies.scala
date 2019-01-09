ef2def937b7de1d41d1c99d3b1a74425198b9bdf
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-01-09 22:02:18 +0300

    Spark: SparkSQL: update SBT version

--- BigData+/Spark+/Spark2+/SQL+/Spark2SQL/project/Dependencies.scala
+++ BigData+/Spark+/Spark2+/SQL+/Spark2SQL/project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val allDeps = Seq(
-    "org.apache.spark" % "spark-sql_2.11" % "2.3.1" exclude("org.scalatest", "scalatest_2.11"),
+    "org.apache.spark" % "spark-sql_2.11" % "2.4.0" exclude("org.scalatest", "scalatest_2.11"),
     "org.scalatest" % "scalatest_2.11" % "3.0.5" % Test,
     "com.h2database" % "h2" % "1.4.197" % Test
   )
