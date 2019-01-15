f67ed543e5e527fa6485e02b268606efe2ee0708
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-01-15 13:23:01 +0300

    Scala: ScalaCore: update SequencesTest

--- Scala+/ScalaCore/src/test/scala/scalatest/matcher/collections/SequencesTest.scala
+++ Scala+/ScalaCore/src/test/scala/scalatest/matcher/collections/SequencesTest.scala
@@ -6,6 +6,7 @@ class SequencesTest extends FlatSpec with Matchers {
 
   "sequences with integers" should "work" in {
     val list = List(1, 2, 2, 3, 3, 3)
+    list should have size 6
     list should contain inOrderOnly(1, 2, 3)
     list should contain(1)
     list should contain allOf(1, 2)
@@ -13,6 +14,7 @@ class SequencesTest extends FlatSpec with Matchers {
 
   "sequences with strings" should "work" in {
     val list = List("a", "b", "c")
+    list should have size 3
     list should contain inOrderOnly("a", "b", "c")
     list should contain("b")
     list should contain allOf("a", "b")
