08f791b62687c7d44a54059e0d72f6d5d8b21329
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-01-23 21:02:54 +0300

    Scala: ScalaCore: add CombiningMatchersTest

--- Scala+/ScalaCore/project/Dependencies.scala
+++ Scala+/ScalaCore/project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val allDeps = Seq(
+  val allDeps: Seq[ModuleID] = Seq(
     "org.scalatest" % "scalatest_2.12" % "3.0.5" % Test
   )
 }
--- Scala+/ScalaCore/src/test/scala/scalatest/matcher/NumbersTest.scala
+++ Scala+/ScalaCore/src/test/scala/scalatest/matcher/NumbersTest.scala
@@ -6,11 +6,15 @@ class NumbersTest extends FlatSpec with Matchers {
 
   "shouldEqual matcher" should "work" in {
     val result = 3
-    result should equal(3) // can customize equality
-    result should ===(3) // can customize equality and enforce type constraints
-    result should be(3) // cannot customize equality, so fastest to compile
-    result shouldEqual 3 // can customize equality, no parentheses required
-    result shouldBe 3 // cannot customize equality, so fastest to compile, no parentheses required
+    result should equal(3)
+    result should ===(3)
+    result should be(3)
+    result shouldEqual 3
+    result shouldBe 3
+    result shouldBe >(2)
+    result shouldBe >=(3)
+    result shouldBe <(4)
+    result shouldBe <=(3)
   }
 
   "NaN matcher" should "work" in {
