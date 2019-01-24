62242d4f328b622e3414c92cfea47bcad92e5c20
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-01-24 20:17:25 +0300

    Java: Jython: remove vars

--- Java+/Libs+/Jython/test/jython/Variable.java
+++ Java+/Libs+/Jython/test/jython/Variable.java
@@ -2,10 +2,15 @@ package jython;
 
 import org.junit.Test;
 import org.python.core.PyInteger;
+import org.python.core.PyObject;
+import org.python.core.PyString;
 import org.python.util.PythonInterpreter;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 
 public class Variable {
@@ -18,6 +23,27 @@ public class Variable {
         interp.exec("x = 2+2");
         PyInteger x = (PyInteger) interp.get("x");
         assertThat(x.getValue(), equalTo(4));
+    }
+
+    @Test
+    public void getNotExistVar() {
+        PyObject notExists = interp.get("not_exists");
+        assertNull(notExists);
+    }
+
+    /**
+     * PythonInterpreter#cleanup() doesn't remove variables.
+     */
+    @Test
+    public void removeVar() {
+        String varName = "a";
+        PyString varNamePy = new PyString(varName);
+
+        interp.set(varName, new PyInteger(42));
+        assertTrue(interp.getLocals().__contains__(varNamePy));
+
+        interp.set(varName, null);
+        assertFalse(interp.getLocals().__contains__(varNamePy));
     }
 
 }
