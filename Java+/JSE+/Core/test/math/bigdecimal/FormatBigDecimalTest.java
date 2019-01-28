6667c3e94d255c061112d991d9590cf81259af88
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-01-28 22:54:25 +0300

    Java: FormatBigDecimalTest

---
+++ Java+/JSE+/Core/test/math/bigdecimal/FormatBigDecimalTest.java
@@ -0,0 +1,31 @@
+package math.bigdecimal;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class FormatBigDecimalTest {
+    @Test
+    public void withPattern() {
+        BigDecimal num = BigDecimal.valueOf(10000000.2360000);
+        DecimalFormat df = new DecimalFormat("#,###.00");
+        String str = df.format(num);
+        assertThat(str, equalTo("10,000,000.24"));
+    }
+
+    @Test
+    public void withLocale() {
+        BigDecimal num = BigDecimal.valueOf(10000000.236000000);
+        Locale locale = new Locale("ru", "RU");
+        NumberFormat df = DecimalFormat.getInstance(locale);
+        String str = df.format(num);
+        assertThat(str, equalTo("10 000 000,236"));
+    }
+
+}
---
+++ Java+/Libs+/JSON+/Gson/test/serialization/MapToJson.java
@@ -0,0 +1,31 @@
+package serialization;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import org.junit.Test;
+
+import java.lang.reflect.Type;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class MapToJson {
+
+    @Test
+    public void compact() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("abc", 123);
+        map.put("Jonh", 30);
+
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Type typeOfMap = new TypeToken<Map<String, Integer>>() {
+        }.getType();
+
+        String jsonStr = gson.toJson(map, typeOfMap);
+        assertEquals("{\"abc\":123,\"Jonh\":30}", jsonStr);
+    }
+}
