c89adf76cd4c4377b6ef7bcc060d6be3dc1a0aa5
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-02-10 01:18:05 +0300

    Java: Jython: Russian

--- Java+/Libs+/Jython/test/jython/Russian.java
+++ Java+/Libs+/Jython/test/jython/Russian.java
@@ -3,15 +3,19 @@ package jython;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.python.core.Py;
 import org.python.core.PyBoolean;
 import org.python.core.PyObject;
+import org.python.core.PyString;
 import org.python.util.PythonInterpreter;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class Russian {
+
     @Rule
     public ExpectedException expectedEx = ExpectedException.none();
     private static final PythonInterpreter interp = new PythonInterpreter();
@@ -35,6 +39,21 @@ public class Russian {
         interp.exec("result = 'ый' == 'ый'");
         PyBoolean result = (PyBoolean) interp.get("result");
         assertTrue(result.getBooleanValue());
+    }
+
+    @Test
+    public void compareStrings2() {
+        PyString code = Py.newStringOrUnicode("result = 'аб' == 'ый'");
+        interp.exec(code);
+        PyBoolean result = (PyBoolean) interp.get("result");
+        assertFalse(result.getBooleanValue());
+    }
+
+    @Test
+    public void evalCompareStrings() {
+        PyString code = Py.newStringOrUnicode("'аб' == 'ый'");
+        PyBoolean result = (PyBoolean) interp.eval(code);
+        assertFalse(result.getBooleanValue());
     }
 
     @Test
