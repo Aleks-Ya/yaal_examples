ededb440601903b2dc7981c07557a2aa05a591ac
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-02-09 19:22:55 +0300

    Java: H2: add CredentialsTest

---
+++ Java+/Database+/H2+/H2Jdbc/test/connection/mode/embedded/CredentialsTest.java
@@ -0,0 +1,31 @@
+package connection.mode.embedded;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class CredentialsTest {
+
+    /**
+     * User you are connecting is created automatically.
+     */
+    @Test
+    public void autoCreatedUser() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:h2:mem:", "my_user", "my_pass");
+
+        Statement select = conn.createStatement();
+        ResultSet resultSet = select.executeQuery("SELECT * FROM INFORMATION_SCHEMA.USERS");
+        if (resultSet.next()) {
+            assertThat(resultSet.getString("NAME"), equalTo("MY_USER"));
+        }
+
+        conn.close();
+    }
+}
