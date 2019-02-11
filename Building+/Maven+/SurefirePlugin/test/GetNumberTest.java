895e6a9b0550e2c1cac2f654ebd623a0c1d7efaa
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-02-11 17:08:34 +0300

    Maven: add SurefirePlugin

---
+++ Building+/Maven+/SurefirePlugin/test/GetNumberTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GetNumberTest {
+
+    @Test
+    public void testGetNubmer() throws Exception {
+        assertEquals(1, GetNumber.getNubmer());
+    }
+}
