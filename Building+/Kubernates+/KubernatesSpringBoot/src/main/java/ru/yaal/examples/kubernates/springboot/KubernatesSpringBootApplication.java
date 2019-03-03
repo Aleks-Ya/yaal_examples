86fc7e605caf5829bb949ff0b6d765fca14b977e
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-03-03 15:57:10 +0300

    Kubernates: add KubernatesSpringBoot

---
+++ Building+/Kubernates+/KubernatesSpringBoot/src/main/java/ru/yaal/examples/kubernates/springboot/KubernatesSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.yaal.examples.kubernates.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernatesSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernatesSpringBootApplication.class, args);
+	}
+
+}
