d90becd5197e14f3639ffe5ff37e0931e855e76b
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-03-17 17:25:04 +0300

    Spring: Spring Boot 2

--- Java+/Spring+/SpringBoot2+/WithParentPom+/HelloWorldWebWithParent/src/test/java/com/example/DemoApplicationTests.java
+++ Java+/Spring+/SpringBoot2+/WithParentPom+/HelloWorldWebWithParent/src/test/java/com/example/DemoApplicationTests.java
@@ -2,17 +2,15 @@ package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DemoApplication.class)
-@WebAppConfiguration
+@SpringBootTest
 public class DemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
