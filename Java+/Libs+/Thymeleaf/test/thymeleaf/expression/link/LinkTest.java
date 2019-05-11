a424e32b06f30a0b46e60985627dac7de0744c0a
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-05-11 09:57:16 +0300

    Java: Thymeleaf: add LinkTest

---
+++ Java+/Libs+/Thymeleaf/test/thymeleaf/expression/link/LinkTest.java
@@ -0,0 +1,28 @@
+package thymeleaf.expression.link;
+
+import org.junit.Test;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import util.ResourceUtil;
+
+import static org.junit.Assert.assertEquals;
+
+public class LinkTest {
+    @Test
+    public void test() {
+        ITemplateResolver resolver = new ClassLoaderTemplateResolver();
+
+        TemplateEngine engine = new TemplateEngine();
+        engine.setTemplateResolver(resolver);
+
+        Context context = new Context();
+
+        String template = "thymeleaf/expression/link/link_template.html";
+        String result = engine.process(template, context);
+
+        String expContent = ResourceUtil.resourceToString(LinkTest.class, "link_expected.html");
+        assertEquals(expContent, result);
+    }
+}
