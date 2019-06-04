70676c5ed0452b919ae6cb53e405fea00a238282
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-06-04 08:21:54 +0300

    Java: Java8: GroupingBy

--- Java+/JSE+/Java8/test/stream/collector/GroupingBy.java
+++ Java+/JSE+/Java8/test/stream/collector/GroupingBy.java
@@ -2,6 +2,7 @@ package stream.collector;
 
 import org.junit.Test;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -15,10 +16,10 @@ import static org.junit.Assert.assertThat;
  * Grouping elements of a List to a Map.
  */
 public class GroupingBy {
-    private final Stream<Integer> stream = Stream.of(1, 2, 3, -1);
 
     @Test
-    public void sum() {
+    public void groupBy() {
+        Stream<Integer> stream = Stream.of(1, 2, 3, -1);
         String positiveKey = "positive";
         String negativeKey = "negative";
 
@@ -28,5 +29,34 @@ public class GroupingBy {
         assertThat(grouped, aMapWithSize(2));
         assertThat(grouped.get(positiveKey), contains(1, 2, 3));
         assertThat(grouped.get(negativeKey), contains(-1));
+    }
+
+
+    /**
+     * Keys of outcome map are ordered (used LinkedHashMap).
+     */
+    @Test
+    public void groupByOrdered() {
+        Stream<Integer> stream = Stream.of(1, 2, 0, 3, -1);
+        String positiveKey = "positive";
+        String zeroKey = "zero";
+        String negativeKey = "negative";
+
+        Map<String, List<Integer>> grouped = stream
+                .collect(Collectors.groupingBy(
+                        num -> {
+                            if (num == 0) {
+                                return zeroKey;
+                            }
+                            return num > 0 ? positiveKey : negativeKey;
+                        },
+                        LinkedHashMap::new,
+                        Collectors.toList()));
+
+        assertThat(grouped, aMapWithSize(3));
+        assertThat(grouped.get(positiveKey), contains(1, 2, 3));
+        assertThat(grouped.get(zeroKey), contains(0));
+        assertThat(grouped.get(negativeKey), contains(-1));
+        assertThat(grouped.keySet(), contains(positiveKey, zeroKey, negativeKey));
     }
 }
