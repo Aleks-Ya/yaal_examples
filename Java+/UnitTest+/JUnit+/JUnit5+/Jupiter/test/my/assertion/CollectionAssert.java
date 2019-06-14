2bfa6c4ad4778672fdfe2a81e71c93ebab462583
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-06-14 07:47:59 +0300

    Java: JUnit5: add CollectionAssert

---
+++ Java+/UnitTest+/JUnit+/JUnit5+/Jupiter/test/my/assertion/CollectionAssert.java
@@ -0,0 +1,18 @@
+package my.assertion;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+
+class CollectionAssert {
+
+    @Test
+    void iterableEquals() {
+        Iterable<Integer> act = Arrays.asList(1, 2, 3);
+        Iterable<Integer> exp = Arrays.asList(1, 2, 3);
+        assertIterableEquals(exp, act);
+    }
+
+}
