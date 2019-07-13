87f3be88f1fef9ea48363a24b6a0595525f1694a
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-07-13 22:40:08 +0300

    Android: improve AndroidExamples

--- Android/AndroidExamples/app/src/main/java/ru/yaal/examples/android/MainActivity.java
+++ Android/AndroidExamples/app/src/main/java/ru/yaal/examples/android/MainActivity.java
@@ -1,14 +1,32 @@
 package ru.yaal.examples.android;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * Called when the user taps the Send button
+     */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
 }
