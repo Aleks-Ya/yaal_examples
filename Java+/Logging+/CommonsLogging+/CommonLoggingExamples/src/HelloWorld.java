36ce9ed921fe15df8b8c1ba4e5f172b069bfb9dc
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-07-31 14:39:44 +0300

    Java: add CommonsLogging

---
+++ Java+/Logging+/CommonsLogging+/CommonLoggingExamples/src/HelloWorld.java
@@ -0,0 +1,10 @@
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        Log log = LogFactory.getLog(HelloWorld.class);
+        log.info("Hello, CommonsLogging!");
+    }
+}
