924c04220ccc1fd5cecb909d615f54ba5b8a8147
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-08-22 22:08:49 +0300

    Phoenix: add PhoenixSyntax

---
+++ BigData+/Phoenix+/PhoenixDocker/Index/CoveredIndex.md
@@ -0,0 +1,4 @@
+# Covered Index
+
+
+
---
+++ BigData+/Phoenix+/PhoenixSyntax/General/Scripts/Index/covered_index.sql
@@ -0,0 +1,30 @@
+-- Covered Indexes
+-- Documentation: https://phoenix.apache.org/secondary_indexing.html
+
+DROP TABLE IF EXISTS person;
+CREATE TABLE person (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR,
+    surname VARCHAR,
+    age INTEGER
+);
+
+UPSERT INTO person(id, name, surname, age) VALUES(1, 'Jonh', 'Will', 25);
+UPSERT INTO person(id, name, surname, age) VALUES(2, 'Mary', 'Smith', 20);
+UPSERT INTO person(id, name, surname, age) VALUES(3, 'Mike', 'Grey', 30);
+
+SELECT * FROM person;
+
+EXPLAIN SELECT * FROM person WHERE name = 'Mike';
+
+DROP INDEX IF EXISTS person_name_index_1 ON person;
+CREATE INDEX person_name_index_1 ON person(name);
+EXPLAIN SELECT name FROM person WHERE name = 'Mike';
+
+DROP INDEX IF EXISTS person_name_index_2 ON person;
+CREATE INDEX person_name_index_2 ON person(name, age);
+EXPLAIN SELECT name, age FROM person WHERE name = 'Mike' AND age > 25;
+
+DROP INDEX IF EXISTS person_name_index_3 ON person;
+CREATE INDEX person_name_index_3 ON person(name, age) INCLUDE (surname);
+EXPLAIN SELECT name, surname, age FROM person WHERE name = 'Mike' AND age > 25;
