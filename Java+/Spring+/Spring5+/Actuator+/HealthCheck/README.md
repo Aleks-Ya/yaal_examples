c4ac3161e64947dd1173be7257b26db57f37a447
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-08-30 20:42:31 +0300

    Spring Actuator: add Loggers

---
+++ Java+/Spring+/Spring5+/Actuator+/HealthCheck/README.md
@@ -0,0 +1,7 @@
+# Spring Boot Actuator "/health" endpoint
+
+## Run 
+Main class: `actuator.HealthStatusApp` 
+
+ ## Endpoints
+1. Health status: http://localhost:8080/actuator/health
--- Java+/Spring+/Spring5+/Actuator+/Loggers/src/actuator/LoggersApp.java
+++ Java+/Spring+/Spring5+/Actuator+/Loggers/src/actuator/LoggersApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-class HealthStatusApp {
+class LoggersApp {
     public static void main(String[] args) {
-        SpringApplication.run(HealthStatusApp.class, args);
+        SpringApplication.run(LoggersApp.class, args);
     }
 }
