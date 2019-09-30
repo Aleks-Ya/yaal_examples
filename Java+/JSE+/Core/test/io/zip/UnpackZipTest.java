ef7661fda0f38c7cb8b1985574c7e71dca353263
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-09-30 14:11:24 +0300

    Java: add UnpackZipTest.java

---
+++ Java+/JSE+/Core/test/io/zip/UnpackZipTest.java
@@ -0,0 +1,56 @@
+package io.zip;
+
+import org.junit.Test;
+import util.ResourceUtil;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import static org.hamcrest.Matchers.aMapWithSize;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class UnpackZipTest {
+
+    @Test
+    public void unpack() throws IOException {
+        try (InputStream is = ResourceUtil.resourceToInputStream(UnpackZipTest.class, "UnpackZip.zip");
+             ZipInputStream zis = new ZipInputStream(is)) {
+            Map<String, EntryData> dataList = new HashMap<>();
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                int size = (int) entry.getSize();
+                byte[] entryContent = new byte[size];
+                int readBytes = zis.read(entryContent, 0, size);
+                assertThat(readBytes, equalTo(size));
+                dataList.put(entry.getName(), new EntryData(entry, entryContent));
+                zis.closeEntry();
+            }
+
+            assertThat(dataList, aMapWithSize(2));
+            assertEntry(dataList, "a.txt", "aaa");
+            assertEntry(dataList, "b.txt", "bbb");
+        }
+    }
+
+    private static void assertEntry(Map<String, EntryData> dataList, String entryName, String expContent) {
+        EntryData data = dataList.get(entryName);
+        assertThat(data.zipEntry.getName(), equalTo(entryName));
+        assertThat(new String(data.content), equalTo(expContent));
+        assertThat(data.content, equalTo(expContent.getBytes()));
+    }
+
+    private static class EntryData {
+        ZipEntry zipEntry;
+        byte[] content;
+
+        public EntryData(ZipEntry zipEntry, byte[] content) {
+            this.zipEntry = zipEntry;
+            this.content = content;
+        }
+    }
+}
