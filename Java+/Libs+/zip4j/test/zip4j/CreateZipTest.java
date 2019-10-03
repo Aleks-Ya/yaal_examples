070e18faf43a31b09342325f966918af04d24d78
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-10-03 21:08:39 +0300

    Java: add zip4j

---
+++ Java+/Libs+/zip4j/test/zip4j/CreateZipTest.java
@@ -0,0 +1,24 @@
+package zip4j;
+
+import net.lingala.zip4j.ZipFile;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class CreateZipTest {
+    @Test
+    public void create() throws IOException {
+        File notExistZipFile = Paths.get(
+                Files.createTempDirectory(CreateZipTest.class.getSimpleName()).toString(), "my.zip").toFile();
+
+        File txtFile1 = Files.createTempFile(CreateZipTest.class.getSimpleName(), "txt").toFile();
+        File txtFile2 = Files.createTempFile(CreateZipTest.class.getSimpleName(), "txt").toFile();
+
+        ZipFile zip = new ZipFile(notExistZipFile);
+        zip.addFile(txtFile1);
+        zip.addFile(txtFile2);
+    }
+}
