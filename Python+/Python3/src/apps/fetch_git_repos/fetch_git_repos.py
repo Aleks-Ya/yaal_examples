4f2e72d461459f58a8e5c43e394663285d165d69
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-10-14 23:57:23 +0300

    Python: change fetch_git_repos.py settings

--- Python+/Python3/src/apps/fetch_git_repos/fetch_git_repos.py
+++ Python+/Python3/src/apps/fetch_git_repos/fetch_git_repos.py
@@ -1,5 +1,6 @@
 # Execute "git fetch" for all Git repos starting from "root_dir" (1st argument) recursively
 # Run example: "python fetch_git_repos.py /home/aleks/pr/home"
+# Add Linux alias: alias fetch_all='python3 /home/aleks/pr/home/yaal_examples/Python+/Python3/src/apps/fetch_git_repos/fetch_git_repos.py /home/aleks/pr'
 import os
 import sys
 from subprocess import Popen, PIPE, TimeoutExpired
@@ -19,12 +20,12 @@ git_repos: List[str] = [tup[0] for tup in os.walk(root_dir, onerror=on_error) if
 repo_number = len(git_repos)
 print(f'Git repositories ({repo_number}): {git_repos}')
 
-cmd: str = 'git fetch'
-timeout_sec = 2 * 60
+cmd: str = 'git fetch --tags'
+timeout_sec = 1 * 60
 print(f'Command: "{cmd}"')
 print(f'Timeout (seconds): {timeout_sec}')
 
-batch_size = 5
+batch_size = 3
 slices = [git_repos[i:i + batch_size] for i in range(0, len(git_repos), batch_size)]
 print(f'Batch size: {batch_size}')
 print(f'Slice number: {len(slices)}')
@@ -41,7 +42,7 @@ for repo_slice in slices:
                       bufsize=1024000)) for repo in repo_slice])
     for repo, process in processes.items():
         counter = counter + 1
-        print(f'[{counter} of {repo_number}] {repo}... ', end='', flush=True)
+        print(f'[{counter} of {repo_number}] [PID {process.pid}] {repo}... ', end='', flush=True)
         time_spent_sec = 0
         outs: bytearray = bytearray()
         errs: bytearray = bytearray()
