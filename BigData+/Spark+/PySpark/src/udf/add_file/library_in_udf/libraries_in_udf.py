994c518ea6dda368175624fd36cf7de5bf503c45
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-10-18 11:24:34 +0300

    PySpark: add libraries_in_udf.py

---
+++ BigData+/Spark+/PySpark/src/udf/add_file/library_in_udf/libraries_in_udf.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+
+import pandas as pd
+from pyspark.sql.functions import udf
+from pyspark.sql.types import IntegerType
+
+from udf.add_file.library_in_udf.my_functions import square
+from udf.add_file.library_in_udf.my_lib.my_lib import to_upper_case
+from util import create_spark_context
+
+spark = create_spark_context(__file__)
+# spark.sparkContext.addPyFile('')
+
+df_pd = pd.DataFrame(
+    data={'integers': [1, 2, 3]}
+)
+df = spark.createDataFrame(df_pd)
+df.printSchema()
+df.show()
+
+
+def my_udf(num: int):
+    print(to_upper_case('a'))
+    return square(num) * 3
+
+
+square_udf_int = udf(my_udf, IntegerType())
+
+converted_df = df.select('integers', square_udf_int('integers').alias('int_squared'))
+
+converted_df.show()
+
+spark.stop()
---
+++ BigData+/Spark+/PySpark/src/udf/add_file/library_in_udf/my_lib/my_lib.py
@@ -0,0 +1,2 @@
+def to_upper_case(s: str) -> str:
+    return s.upper()
