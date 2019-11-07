7ace53f8048b047f67deee67e0950e401b5f2985
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-11-07 17:22:11 +0800

    Kotlin: add KotlinSpringExample

---
+++ Kotlin+/KotlinSpringExample/src/main/kotlin/ru/yaal/examples/kotlin/spring/kotlinspringexample/KotlinSpringExampleApplication.kt
@@ -0,0 +1,11 @@
+package ru.yaal.examples.kotlin.spring.kotlinspringexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringExampleApplication>(*args)
+}
---
+++ Kotlin+/KotlinSpringExample/src/test/kotlin/ru/yaal/examples/kotlin/spring/kotlinspringexample/KotlinSpringExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.yaal.examples.kotlin.spring.kotlinspringexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
