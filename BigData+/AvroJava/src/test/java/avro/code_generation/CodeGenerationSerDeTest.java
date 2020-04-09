b51b8d6b00c1472a8284f204b97d0042c6991247
Aleksei Iablokov <aleksei_iablokov@epam.com>
2020-04-09 09:53:27 +0300

    Avro: add AvroJava

---
+++ BigData+/AvroJava/src/test/java/avro/code_generation/CodeGenerationSerDeTest.java
@@ -0,0 +1,55 @@
+package avro.code_generation;
+
+import example.avro.User;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Serialize and deserialize objects without code generation.
+ * Source: https://avro.apache.org/docs/current/gettingstartedjava.html
+ */
+public class CodeGenerationSerDeTest {
+
+    @Test
+    public void serDe() throws IOException {
+        // Create users
+        User user1 = new User();
+        user1.setName("Alyssa");
+        user1.setFavoriteNumber(256);
+
+        User user2 = new User("Ben", 7, "red");
+
+        User user3 = User.newBuilder()
+                .setName("Charlie")
+                .setFavoriteColor("blue")
+                .setFavoriteNumber(null)
+                .build();
+
+        // Serialize user1, user2 and user3 to disk
+        File file = File.createTempFile("users", ".avro");
+        DatumWriter<User> userDatumWriter = new SpecificDatumWriter<>(User.class);
+        DataFileWriter<User> dataFileWriter = new DataFileWriter<>(userDatumWriter);
+        dataFileWriter.create(user1.getSchema(), file);
+        dataFileWriter.append(user1);
+        dataFileWriter.append(user2);
+        dataFileWriter.append(user3);
+        dataFileWriter.close();
+
+        // Deserialize Users from disk
+        DatumReader<User> userDatumReader = new SpecificDatumReader<>(User.class);
+        DataFileReader<User> dataFileReader = new DataFileReader<>(file, userDatumReader);
+        User user = null;
+        while (dataFileReader.hasNext()) {
+            user = dataFileReader.next(user);
+            System.out.println(user);
+        }
+    }
+}
---
+++ BigData+/AvroJava/src/test/java/avro/no_code_generation/NoCodeGenerationSerDeTest.java
@@ -0,0 +1,56 @@
+package avro.no_code_generation;
+
+import org.apache.avro.Schema;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Serialize and deserialize objects without code generation.
+ * Source: https://avro.apache.org/docs/current/gettingstartedjava.html
+ */
+public class NoCodeGenerationSerDeTest {
+
+    @Test
+    public void serDe() throws IOException {
+        InputStream schemaIS = NoCodeGenerationSerDeTest.class.getResourceAsStream("user.avsc");
+        Schema schema = new Schema.Parser().parse(schemaIS);
+
+        // Create users
+        GenericRecord user1 = new GenericData.Record(schema);
+        user1.put("name", "Alyssa");
+        user1.put("favorite_number", 256);
+
+        GenericRecord user2 = new GenericData.Record(schema);
+        user2.put("name", "Ben");
+        user2.put("favorite_number", 7);
+        user2.put("favorite_color", "red");
+
+        // Serialize user1 and user2 to disk
+        File file = File.createTempFile("users", ".avro");
+        DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<>(schema);
+        DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<>(datumWriter);
+        dataFileWriter.create(schema, file);
+        dataFileWriter.append(user1);
+        dataFileWriter.append(user2);
+        dataFileWriter.close();
+
+        DatumReader<GenericRecord> datumReader = new GenericDatumReader<>(schema);
+        DataFileReader<GenericRecord> dataFileReader = new DataFileReader<>(file, datumReader);
+        GenericRecord user = null;
+        while (dataFileReader.hasNext()) {
+            user = dataFileReader.next(user);
+            System.out.println(user);
+        }
+    }
+}
