533a77b87aa190e3f5665c5a8cc407bf9400a76a
Aleksey Yablokov <ya_al@bk.ru>
2020-06-14 16:52:35 +0300

    Java: add XMLEncoder/XMLDecoder example

---
+++ Java+/JSE+/Core/test/beans/xml/XmlTest.java
@@ -0,0 +1,34 @@
+package beans.xml;
+
+import org.junit.Test;
+
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.sameInstance;
+
+public class XmlTest {
+
+    @Test
+    public void codeDecode() {
+        Object expObj = "abc";
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try (XMLEncoder encoder = new XMLEncoder(out)) {
+            encoder.writeObject(expObj);
+        }
+        byte[] bytes = out.toByteArray();
+
+        try (XMLDecoder decoder = new XMLDecoder(new ByteArrayInputStream(bytes))) {
+            Object actObj = decoder.readObject();
+            assertThat(actObj, not(sameInstance(expObj)));
+            assertThat(actObj, equalTo(expObj));
+        }
+    }
+
+}
