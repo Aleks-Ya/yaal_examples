878dd89b9769394021e7ab1e489725e2cb25b8ca
Aleksey Yablokov <ya_al@bk.ru>
2020-09-02 10:52:21 +0300

    Java: S3: add ObjectTest

---
+++ Java+/Libs+/AWS+/s3/test/aws/ObjectTest.java
@@ -0,0 +1,125 @@
+package aws;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.emptyString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
+
+/**
+ * Write and read an object in a bucket.
+ * Requires aws.key.access and aws.key.secret Java properties.
+ */
+public class ObjectTest {
+    private static AmazonS3 s3;
+
+    @BeforeClass
+    public static void setUp() {
+        String accessKey = System.getProperty("aws.key.access");
+        String secretKey = System.getProperty("aws.key.secret");
+        assertThat(accessKey, allOf(not(emptyString()), notNullValue()));
+        assertThat(secretKey, allOf(not(emptyString()), notNullValue()));
+
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        AWSCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(credentials);
+        s3 = AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(credentialsProvider)
+                .withRegion(Regions.EU_CENTRAL_1)
+                .build();
+    }
+
+    @Test
+    public void uploadListDeleteObject() {
+        Bucket bucket = createBucket();
+
+        String contentExp = "abc";
+        String keyName = uploadObject(bucket, contentExp);
+
+        listObjects(bucket, keyName);
+
+        String contentAct = downloadObject(bucket, keyName);
+        assertThat(contentAct, equalTo(contentExp));
+
+        deleteObject(bucket, keyName);
+        deleteBucket(bucket);
+    }
+
+    private static String downloadObject(Bucket bucket, String keyName) {
+        try {
+            S3Object o = s3.getObject(bucket.getName(), keyName);
+            S3ObjectInputStream s3is = o.getObjectContent();
+            OutputStream os = new ByteArrayOutputStream();
+            byte[] read_buf = new byte[1024];
+            int read_len;
+            while ((read_len = s3is.read(read_buf)) > 0) {
+                os.write(read_buf, 0, read_len);
+            }
+            s3is.close();
+            os.close();
+            return os.toString();
+        } catch (AmazonServiceException e) {
+            System.err.println(e.getErrorMessage());
+            throw e;
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void deleteBucket(Bucket bucket) {
+        s3.deleteBucket(bucket.getName());
+    }
+
+    private static Bucket createBucket() {
+        String bucketName = UUID.randomUUID().toString();
+        return s3.createBucket(bucketName);
+    }
+
+    private static void deleteObject(Bucket bucket, String keyName) {
+        s3.deleteObject(bucket.getName(), keyName);
+    }
+
+    private static void listObjects(Bucket bucket, String keyName) {
+        ListObjectsV2Result result = s3.listObjectsV2(bucket.getName());
+        List<S3ObjectSummary> objects = result.getObjectSummaries();
+        assertThat(objects, hasSize(1));
+        S3ObjectSummary os = objects.get(0);
+        assertThat(os.getKey(), equalTo(keyName));
+    }
+
+    private static String uploadObject(Bucket bucket, String content) {
+        String keyName = "ObjectTest.txt";
+        try {
+            s3.putObject(bucket.getName(), keyName, content);
+        } catch (AmazonServiceException e) {
+            System.err.println(e.getErrorMessage());
+            System.exit(1);
+        }
+        return keyName;
+    }
+}
