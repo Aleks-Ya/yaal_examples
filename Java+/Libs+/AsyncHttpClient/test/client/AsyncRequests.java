f5f37563293bbfd2ea1d942cefd79620564e0497
Aleksey Yablokov <ya_al@bk.ru>
2020-09-29 09:09:11 +0300

    AsyncHttpClient: add project

---
+++ Java+/Libs+/AsyncHttpClient/test/client/AsyncRequests.java
@@ -0,0 +1,11 @@
+package client;
+
+import org.junit.Test;
+
+public class AsyncRequests {
+
+    @Test
+    public void async() {
+    }
+
+}
---
+++ Java+/Libs+/AsyncHttpClient/test/client/MultipartRequest.java
@@ -0,0 +1,11 @@
+package client;
+
+import org.junit.Test;
+
+public class MultipartRequest {
+
+    @Test
+    public void multipart() {
+    }
+
+}
