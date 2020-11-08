a47b596c9ac218bbfe1aaf5d2b85ead2f6ae5c21
Aleksey Yablokov <ya_al@bk.ru>
2020-11-08 14:25:53 +0300

    Kotlin: iterate map

--- Kotlin+/kotlin-examples/src/test/kotlin/kotlins/core/collection/Maps.kt
+++ Kotlin+/kotlin-examples/src/test/kotlin/kotlins/core/collection/Maps.kt
@@ -10,4 +10,12 @@ class Maps {
         val map = mapOf("a" to 2, "b" to 7)
         assertThat(map).containsEntry("a", 2).containsEntry("b", 7)
     }
+
+    @Test
+    fun `iterate map entries`() {
+        val str = mapOf("a" to 2, "b" to 7)
+                .map { it.key + it.value }
+                .joinToString(", ") { it.toUpperCase() }
+        assertThat(str).isEqualTo("A2, B7")
+    }
 }
