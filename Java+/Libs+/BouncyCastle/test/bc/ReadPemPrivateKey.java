961d2e701026e8d0d5ede6b42e2016f0750ee7b0
Aleksey Yablokov <ya_al@bk.ru>
2020-11-13 13:10:57 +0300

    Java: BouncyCastle: ReadPemPrivateKey#readKeyPairPKCS8EncryptedPrivateKeyInfo()

--- Java+/Libs+/BouncyCastle/test/bc/ReadPemPrivateKey.java
+++ Java+/Libs+/BouncyCastle/test/bc/ReadPemPrivateKey.java
@@ -4,6 +4,7 @@ import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
 import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 import org.bouncycastle.openssl.PEMKeyPair;
 import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.pkcs.PKCS8EncryptedPrivateKeyInfo;
 import org.junit.Test;
 import util.ResourceUtil;
 
@@ -12,6 +13,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
 public class ReadPemPrivateKey {
@@ -25,6 +27,15 @@ public class ReadPemPrivateKey {
         SubjectPublicKeyInfo publicKeyInfo = keyPair.getPublicKeyInfo();
         assertThat(privateKeyInfo, notNullValue());
         assertThat(publicKeyInfo, notNullValue());
+    }
+
+    @Test
+    public void readKeyPairPKCS8EncryptedPrivateKeyInfo() throws IOException {
+        File keyFile = ResourceUtil.resourceToFile(ReadPemPrivateKey.class, "rsa-pkcs8-private.pem");
+        PEMParser pemParser = new PEMParser(new FileReader(keyFile));
+        PKCS8EncryptedPrivateKeyInfo keyPair = (PKCS8EncryptedPrivateKeyInfo) pemParser.readObject();
+        byte[] encoded = keyPair.getEncoded();
+        assertThat(encoded.length, equalTo(2481));
     }
 
 }
