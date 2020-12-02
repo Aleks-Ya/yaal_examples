56e115aec5a30285eac1d3819f6e04b578340e4a
Aleksey Yablokov <ya_al@bk.ru>
2020-12-02 10:17:56 +0300

    Java: HttpClient: add Http file download

--- Java+/JSE+/Java9/test/java9/http/Http.java
+++ Java+/JSE+/Java9/test/java9/http/Http.java
@@ -1,22 +1,34 @@
 package java9.http;
 
+import okhttp3.HttpUrl;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
+import static java.lang.String.format;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class Http {
+
     @Test
-    public void get() throws URISyntaxException, IOException, InterruptedException {
+    public void get() throws IOException, InterruptedException {
+        MockWebServer server = new MockWebServer();
+        String body = "hello, world!";
+        server.enqueue(new MockResponse().setBody(body));
+        server.start();
+        HttpUrl baseUrl = server.url("/");
+
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI("https://postman-echo.com/get"))
+                .uri(baseUrl.uri())
                 .GET()
                 .build();
 
@@ -24,5 +36,37 @@ public class Http {
                 .send(request, HttpResponse.BodyHandlers.ofString());
         int statusCode = response.statusCode();
         assertThat(statusCode, equalTo(200));
+
+        server.shutdown();
     }
+
+    @Test
+    public void getFile() throws IOException, InterruptedException {
+        MockWebServer server = new MockWebServer();
+        String body = "hello, world!";
+        String outputFilename = "message.txt";
+        server.enqueue(new MockResponse()
+                .addHeader("Content-Disposition", format("attachment; filename=\"%s\"", outputFilename))
+                .setBody(body));
+        server.start();
+        HttpUrl baseUrl = server.url("/");
+
+        Path outputDir = Files.createTempDirectory(Http.class.getSimpleName());
+        HttpRequest request = HttpRequest.newBuilder().uri(baseUrl.uri()).GET().build();
+        HttpResponse.BodyHandler<Path> bodyHandler = HttpResponse.BodyHandlers.ofFileDownload(outputDir,
+                StandardOpenOption.CREATE_NEW, StandardOpenOption.WRITE);
+        HttpResponse<Path> response = HttpClient.newHttpClient().send(request, bodyHandler);
+
+        int statusCode = response.statusCode();
+        assertThat(statusCode, equalTo(200));
+
+        Path outputFile = response.body();
+        assertThat(outputFile, equalTo(outputDir.resolve(outputFilename)));
+
+        String actContent = Files.readString(outputFile);
+        assertThat(actContent, equalTo(body));
+
+        server.shutdown();
+    }
+
 }
