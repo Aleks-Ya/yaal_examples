6729079da10ef4066e4f5d2069dfc710e1d3d46c
Aleksey Yablokov <ya_al@bk.ru>
2020-12-03 19:03:47 +0300

    Java: Java9: add HttpAsync

--- Java+/JSE+/Java9/test/java9/http/HttpSync.java
+++ Java+/JSE+/Java9/test/java9/http/HttpSync.java
@@ -1,6 +1,5 @@
 package java9.http;
 
-import okhttp3.HttpUrl;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import org.junit.Test;
@@ -10,31 +9,30 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
 import static java.lang.String.format;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-public class Http {
+public class HttpSync {
 
     @Test
     public void get() throws IOException, InterruptedException {
-        MockWebServer server = new MockWebServer();
-        String body = "hello, world!";
+        var server = new MockWebServer();
+        var body = "hello, world!";
         server.enqueue(new MockResponse().setBody(body));
         server.start();
-        HttpUrl baseUrl = server.url("/");
+        var baseUrl = server.url("/");
 
-        HttpRequest request = HttpRequest.newBuilder()
+        var request = HttpRequest.newBuilder()
                 .uri(baseUrl.uri())
                 .GET()
                 .build();
 
-        HttpResponse<String> response = HttpClient.newHttpClient()
+        var response = HttpClient.newHttpClient()
                 .send(request, HttpResponse.BodyHandlers.ofString());
-        int statusCode = response.statusCode();
+        var statusCode = response.statusCode();
         assertThat(statusCode, equalTo(200));
 
         server.shutdown();
@@ -42,28 +40,28 @@ public class Http {
 
     @Test
     public void getFile() throws IOException, InterruptedException {
-        MockWebServer server = new MockWebServer();
-        String body = "hello, world!";
-        String outputFilename = "message.txt";
+        var server = new MockWebServer();
+        var body = "hello, world!";
+        var outputFilename = "message.txt";
         server.enqueue(new MockResponse()
                 .addHeader("Content-Disposition", format("attachment; filename=\"%s\"", outputFilename))
                 .setBody(body));
         server.start();
-        HttpUrl baseUrl = server.url("/");
+        var baseUrl = server.url("/");
 
-        Path outputDir = Files.createTempDirectory(Http.class.getSimpleName());
-        HttpRequest request = HttpRequest.newBuilder().uri(baseUrl.uri()).GET().build();
-        HttpResponse.BodyHandler<Path> bodyHandler = HttpResponse.BodyHandlers.ofFileDownload(outputDir,
+        var outputDir = Files.createTempDirectory(HttpSync.class.getSimpleName());
+        var request = HttpRequest.newBuilder().uri(baseUrl.uri()).GET().build();
+        var bodyHandler = HttpResponse.BodyHandlers.ofFileDownload(outputDir,
                 StandardOpenOption.CREATE_NEW, StandardOpenOption.WRITE);
-        HttpResponse<Path> response = HttpClient.newHttpClient().send(request, bodyHandler);
+        var response = HttpClient.newHttpClient().send(request, bodyHandler);
 
-        int statusCode = response.statusCode();
+        var statusCode = response.statusCode();
         assertThat(statusCode, equalTo(200));
 
-        Path outputFile = response.body();
+        var outputFile = response.body();
         assertThat(outputFile, equalTo(outputDir.resolve(outputFilename)));
 
-        String actContent = Files.readString(outputFile);
+        var actContent = Files.readString(outputFile);
         assertThat(actContent, equalTo(body));
 
         server.shutdown();
