5f8cdb24588400a69d898d2e1174eb6488404cd9
Aleksey Yablokov <ya_al@bk.ru>
2020-12-23 03:11:19 +0200

    Java: AWS: add TransferManagerTest

--- Java+/Libs+/AWS+/S3+/S3UnitTest+/AdobeS3Mock/test/mock/AdobeBaseTest.java
+++ Java+/Libs+/AWS+/S3+/S3UnitTest+/AdobeS3Mock/test/mock/AdobeBaseTest.java
@@ -1,12 +1,24 @@
 package mock;
 
 import com.adobe.testing.s3mock.S3MockApplication;
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.UUID;
 
 public abstract class AdobeBaseTest {
     protected static S3MockApplication server;
@@ -28,6 +40,38 @@ public abstract class AdobeBaseTest {
     public static void tearDown() {
         s3.shutdown();
         server.stop();
+    }
+
+    protected Bucket createRandomBucket(AmazonS3 s3) {
+        String bucketName = UUID.randomUUID().toString();
+        return s3.createBucket(bucketName);
+    }
+
+    protected String downloadObject(Bucket bucket, String keyName) {
+        try {
+            S3Object o = s3.getObject(bucket.getName(), keyName);
+            S3ObjectInputStream s3is = o.getObjectContent();
+            OutputStream os = new ByteArrayOutputStream();
+            byte[] read_buf = new byte[1024];
+            int read_len;
+            while ((read_len = s3is.read(read_buf)) > 0) {
+                os.write(read_buf, 0, read_len);
+            }
+            s3is.close();
+            os.close();
+            return os.toString();
+        } catch (AmazonServiceException e) {
+            System.err.println(e.getErrorMessage());
+            throw e;
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected List<S3ObjectSummary> listObjects(Bucket bucket) {
+        ListObjectsV2Result result = s3.listObjectsV2(bucket.getName());
+        return result.getObjectSummaries();
     }
 
 }
--- Java+/Libs+/AWS+/S3+/S3UnitTest+/AdobeS3Mock/test/mock/AdobeObjectTest.java
+++ Java+/Libs+/AWS+/S3+/S3UnitTest+/AdobeS3Mock/test/mock/AdobeObjectTest.java
@@ -3,17 +3,10 @@ package mock;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.Bucket;
-import com.amazonaws.services.s3.model.ListObjectsV2Result;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.List;
-import java.util.UUID;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -26,58 +19,30 @@ public class AdobeObjectTest extends AdobeBaseTest {
 
     @Test
     public void uploadListDeleteObject() {
-        Bucket bucket = createBucket(s3);
+        Bucket bucket = createRandomBucket(s3);
 
         String contentExp = "abc";
         String keyName = uploadObject(s3, bucket, contentExp);
 
-        listObjects(s3, bucket, keyName);
+        assertObjects(bucket, keyName);
 
-        String contentAct = downloadObject(s3, bucket, keyName);
+        String contentAct = downloadObject(bucket, keyName);
         assertThat(contentAct, equalTo(contentExp));
 
         deleteObject(s3, bucket, keyName);
         deleteBucket(s3, bucket);
     }
 
-    private static String downloadObject(AmazonS3 s3, Bucket bucket, String keyName) {
-        try {
-            S3Object o = s3.getObject(bucket.getName(), keyName);
-            S3ObjectInputStream s3is = o.getObjectContent();
-            OutputStream os = new ByteArrayOutputStream();
-            byte[] read_buf = new byte[1024];
-            int read_len;
-            while ((read_len = s3is.read(read_buf)) > 0) {
-                os.write(read_buf, 0, read_len);
-            }
-            s3is.close();
-            os.close();
-            return os.toString();
-        } catch (AmazonServiceException e) {
-            System.err.println(e.getErrorMessage());
-            throw e;
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-            throw new RuntimeException(e);
-        }
-    }
-
     private static void deleteBucket(AmazonS3 s3, Bucket bucket) {
         s3.deleteBucket(bucket.getName());
-    }
-
-    private static Bucket createBucket(AmazonS3 s3) {
-        String bucketName = UUID.randomUUID().toString();
-        return s3.createBucket(bucketName);
     }
 
     private static void deleteObject(AmazonS3 s3, Bucket bucket, String keyName) {
         s3.deleteObject(bucket.getName(), keyName);
     }
 
-    private static void listObjects(AmazonS3 s3, Bucket bucket, String keyName) {
-        ListObjectsV2Result result = s3.listObjectsV2(bucket.getName());
-        List<S3ObjectSummary> objects = result.getObjectSummaries();
+    private void assertObjects(Bucket bucket, String keyName) {
+        List<S3ObjectSummary> objects = listObjects(bucket);
         assertThat(objects, hasSize(1));
         S3ObjectSummary os = objects.get(0);
         assertThat(os.getKey(), equalTo(keyName));
