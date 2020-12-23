00c82096fad576d850030f06173117c815cb8db1
Aleksey Yablokov <ya_al@bk.ru>
2020-12-23 03:50:11 +0200

    Java: AWS: add S3MockTransferManagerTest

--- Java+/Libs+/AWS+/S3+/S3UnitTest+/s3mock/test/mock/BucketTest.java
+++ Java+/Libs+/AWS+/S3+/S3UnitTest+/s3mock/test/mock/BucketTest.java
@@ -1,14 +1,6 @@
 package mock;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.AnonymousAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.Bucket;
-import io.findify.s3mock.S3Mock;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.List;
@@ -21,49 +13,27 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class BucketTest {
-    private static AmazonS3 s3client;
-    private static S3Mock api;
-
-    @BeforeClass
-    public static void setUp() {
-        api = new S3Mock.Builder().withPort(8001).withInMemoryBackend().build();
-        api.start();
-        String region = "us-west-2";
-        String serviceEndpoint = "http://localhost:8001";
-        AwsClientBuilder.EndpointConfiguration endpoint = new AwsClientBuilder.EndpointConfiguration(serviceEndpoint, region);
-        s3client = AmazonS3ClientBuilder
-                .standard()
-                .withPathStyleAccessEnabled(true)
-                .withEndpointConfiguration(endpoint)
-                .withCredentials(new AWSStaticCredentialsProvider(new AnonymousAWSCredentials()))
-                .build();
-    }
+public class BucketTest extends S3MockBaseTest {
 
     @Test
     public void listBuckets() {
         String bucketName = "abc";
-        s3client.createBucket(bucketName);
-        List<String> buckets = s3client.listBuckets().stream().map(Bucket::getName).collect(Collectors.toList());
+        s3.createBucket(bucketName);
+        List<String> buckets = s3.listBuckets().stream().map(Bucket::getName).collect(Collectors.toList());
         assertThat(buckets, contains(bucketName));
     }
 
     @Test
     public void createBucket() {
         String bucketName = UUID.randomUUID().toString();
-        assertFalse(s3client.doesBucketExistV2(bucketName));
+        assertFalse(s3.doesBucketExistV2(bucketName));
 
-        Bucket bucket = s3client.createBucket(bucketName);
+        Bucket bucket = s3.createBucket(bucketName);
         assertThat(bucket.getName(), equalTo(bucketName));
-        assertTrue(s3client.doesBucketExistV2(bucketName));
+        assertTrue(s3.doesBucketExistV2(bucketName));
 
-        s3client.deleteBucket(bucketName);
-        assertFalse(s3client.doesBucketExistV2(bucketName));
+        s3.deleteBucket(bucketName);
+        assertFalse(s3.doesBucketExistV2(bucketName));
 
-    }
-
-    @AfterClass
-    public static void afterClass() {
-        api.shutdown(); // kills the underlying actor system. Use api.stop() to just unbind the port.
     }
 }
---
+++ Java+/Libs+/AWS+/S3+/S3UnitTest+/s3mock/test/mock/S3MockBaseTest.java
@@ -0,0 +1,79 @@
+package mock;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.AnonymousAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import io.findify.s3mock.S3Mock;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.UUID;
+
+public abstract class S3MockBaseTest {
+    protected static AmazonS3 s3;
+    private static S3Mock api;
+
+    @BeforeClass
+    public static void setUp() {
+        api = new S3Mock.Builder().withPort(8001).withInMemoryBackend().build();
+        api.start();
+        String region = "us-west-2";
+        String serviceEndpoint = "http://localhost:8001";
+        AwsClientBuilder.EndpointConfiguration endpoint = new AwsClientBuilder.EndpointConfiguration(serviceEndpoint, region);
+        s3 = AmazonS3ClientBuilder
+                .standard()
+                .withPathStyleAccessEnabled(true)
+                .withEndpointConfiguration(endpoint)
+                .withCredentials(new AWSStaticCredentialsProvider(new AnonymousAWSCredentials()))
+                .build();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        api.shutdown(); // kills the underlying actor system. Use api.stop() to just unbind the port.
+    }
+
+    protected Bucket createRandomBucket(AmazonS3 s3) {
+        String bucketName = UUID.randomUUID().toString();
+        return s3.createBucket(bucketName);
+    }
+
+    protected String downloadObject(Bucket bucket, String keyName) {
+        try {
+            S3Object o = s3.getObject(bucket.getName(), keyName);
+            S3ObjectInputStream s3is = o.getObjectContent();
+            OutputStream os = new ByteArrayOutputStream();
+            byte[] read_buf = new byte[1024];
+            int read_len;
+            while ((read_len = s3is.read(read_buf)) > 0) {
+                os.write(read_buf, 0, read_len);
+            }
+            s3is.close();
+            os.close();
+            return os.toString();
+        } catch (AmazonServiceException e) {
+            System.err.println(e.getErrorMessage());
+            throw e;
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected List<S3ObjectSummary> listObjects(Bucket bucket) {
+        ListObjectsV2Result result = s3.listObjectsV2(bucket.getName());
+        return result.getObjectSummaries();
+    }
+}
