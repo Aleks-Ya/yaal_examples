cebfb2a40c5a04c018267b6714db843b72fd9d29
Aleksey Yablokov <ya_al@bk.ru>
2020-12-28 14:15:24 +0200

    Java: Guice: ad NamedAnnotationBindingTest

---
+++ Java+/Libs+/Guice/test/guice/binding/annotation/NamedAnnotationBindingTest.java
@@ -0,0 +1,74 @@
+package guice.binding.annotation;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Provides;
+import com.google.inject.name.Names;
+import org.junit.Test;
+
+import javax.inject.Named;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ * Use {@link Named} annotation to distinguish implementations.
+ */
+public class NamedAnnotationBindingTest {
+    private static final String COUNTRY_NAME = "country";
+    private static final String CITY_NAME = "city";
+    private static final String DISTRICT_NAME = "district";
+    private static final String STREET_NAME = "street";
+
+    private static class DemoModule extends AbstractModule {
+        @Override
+        protected void configure() {
+            bind(String.class).annotatedWith(Names.named(COUNTRY_NAME)).toInstance("Russia");
+            bind(String.class).annotatedWith(Names.named(CITY_NAME)).toInstance("Moscow");
+        }
+
+        @Provides
+        @Named(DISTRICT_NAME)
+        @SuppressWarnings("unused")
+        public String district() {
+            return "Central District";
+        }
+
+        @Provides
+        @Named(STREET_NAME)
+        @SuppressWarnings("unused")
+        public String street() {
+            return "Lenina St.";
+        }
+    }
+
+    static class Location {
+        private final String country;
+        private final String city;
+        private final String district;
+        private final String street;
+
+        @Inject
+        Location(@Named(COUNTRY_NAME) String country, @Named(CITY_NAME) String city,
+                 @Named(DISTRICT_NAME) String district, @Named(STREET_NAME) String street) {
+            this.country = country;
+            this.city = city;
+            this.district = district;
+            this.street = street;
+        }
+
+        String getMessage() {
+            return street + ", " + district + ", " + city + ", " + country;
+        }
+    }
+
+    @Test
+    public void bind() {
+        Injector injector = Guice.createInjector(new DemoModule());
+        Location location = injector.getInstance(Location.class);
+        String message = location.getMessage();
+        assertThat(message, equalTo("Lenina St., Central District, Moscow, Russia"));
+    }
+}
