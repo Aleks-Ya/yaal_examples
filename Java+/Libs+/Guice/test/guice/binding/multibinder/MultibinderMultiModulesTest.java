e942382e87b4b2ef9536c21bb8f0dfb08fbab0db
Aleksey Yablokov <ya_al@bk.ru>
2020-12-30 06:58:51 +0200

    Java: Guice: add Multibinder

---
+++ Java+/Libs+/Guice/test/guice/binding/multibinder/MultibinderMultiModulesTest.java
@@ -0,0 +1,74 @@
+package guice.binding.multibinder;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.multibindings.Multibinder;
+import org.junit.Test;
+
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+
+public class MultibinderMultiModulesTest {
+
+    @Test
+    public void bind() {
+        var injector = Guice.createInjector(new RootModule());
+        var processor = injector.getInstance(SourceProcessor.class);
+        var source = processor.getSources();
+        assertThat(source, hasSize(3));
+    }
+
+    private static class RootModule extends AbstractModule {
+        @Override
+        protected void configure() {
+            var multibinder = Multibinder.newSetBinder(binder(), Source.class);
+            multibinder.addBinding().to(HttpSource.class);
+            install(new ModuleA());
+            install(new ModuleB());
+        }
+    }
+
+    private static class ModuleA extends AbstractModule {
+        @Override
+        protected void configure() {
+            var multibinder = Multibinder.newSetBinder(binder(), Source.class);
+            multibinder.addBinding().to(DatabaseSource.class);
+        }
+    }
+
+    private static class ModuleB extends AbstractModule {
+        @Override
+        protected void configure() {
+            var multibinder = Multibinder.newSetBinder(binder(), Source.class);
+            multibinder.addBinding().toInstance(new FileSource());
+        }
+    }
+
+    interface Source {
+    }
+
+    static class HttpSource implements Source {
+    }
+
+    static class DatabaseSource implements Source {
+    }
+
+    static class FileSource implements Source {
+    }
+
+    static class SourceProcessor {
+        private final Set<Source> sources;
+
+        @Inject
+        SourceProcessor(Set<Source> sources) {
+            this.sources = sources;
+        }
+
+        public Set<Source> getSources() {
+            return sources;
+        }
+    }
+}
---
+++ Java+/Libs+/Guice/test/guice/binding/multibinder/MultibinderSingleModuleTest.java
@@ -0,0 +1,59 @@
+package guice.binding.multibinder;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.multibindings.Multibinder;
+import org.junit.Test;
+
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+
+public class MultibinderSingleModuleTest {
+
+    @Test
+    public void bind() {
+        var injector = Guice.createInjector(new DemoModule());
+        var processor = injector.getInstance(SourceProcessor.class);
+        var source = processor.getSources();
+        assertThat(source, hasSize(3));
+    }
+
+    private static class DemoModule extends AbstractModule {
+        @Override
+        protected void configure() {
+            var multibinder = Multibinder.newSetBinder(binder(), Source.class);
+            multibinder.addBinding().to(HttpSource.class);
+            multibinder.addBinding().to(DatabaseSource.class);
+            multibinder.addBinding().toInstance(new FileSource());
+        }
+    }
+
+    interface Source {
+    }
+
+    static class HttpSource implements Source {
+    }
+
+    static class DatabaseSource implements Source {
+    }
+
+    static class FileSource implements Source {
+    }
+
+    static class SourceProcessor {
+        private final Set<Source> sources;
+
+        @Inject
+        SourceProcessor(Set<Source> sources) {
+            this.sources = sources;
+        }
+
+        public Set<Source> getSources() {
+            return sources;
+        }
+
+    }
+}
