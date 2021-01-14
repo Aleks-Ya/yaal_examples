f05b28f7f6dbbc04eff34ea59c2fe9ce1df466fe
Aleksey Yablokov <ya_al@bk.ru>
2021-01-14 11:19:55 +0200

    Java: append ErrorHandlingInCompletableFuture

--- Java+/JSE+/Java8/test/java8/concurrent/future/completable/ErrorHandlingInCompletableFuture.java
+++ Java+/JSE+/Java8/test/java8/concurrent/future/completable/ErrorHandlingInCompletableFuture.java
@@ -13,23 +13,57 @@ import static org.junit.Assert.assertThrows;
  * Handle error in {@link CompletableFuture}s.
  */
 public class ErrorHandlingInCompletableFuture {
+    private Boolean errorHappened;
+
+    @Test
+    public void notHandleExceptions() {
+        var expException = new IllegalArgumentException();
+        var cf = CompletableFuture
+                .supplyAsync(() -> {
+                    throw expException;
+                });
+        var actException = assertThrows(ExecutionException.class, cf::get);
+        assertThat(cf.isDone(), equalTo(true));
+        assertThat(actException.getCause(), equalTo(expException));
+    }
 
     @Test
     public void handle() {
-        String message = null;
-        var cf = CompletableFuture.supplyAsync(() -> {
-            if (message == null) {
-                throw new IllegalArgumentException();
-            }
-            return message.toUpperCase();
-        }).handle((s, t) -> s != null ? s : "Hello Exception");
+        var cf = CompletableFuture
+                .supplyAsync(() -> {
+                    throw new IllegalArgumentException();
+                })
+                .handle((s, t) -> s != null ? s : "Hello Exception");
         assertThat(cf.join(), equalTo("Hello Exception"));
         assertThat(cf.isDone(), equalTo(true));
     }
 
     @Test
+    public void whenComplete() {
+        var cf = CompletableFuture
+                .supplyAsync(() -> {
+                    throw new IllegalArgumentException();
+                })
+                .whenComplete((msg, t) -> {
+                    errorHappened = t != null;
+                });
+        assertThat(errorHappened, equalTo(true));
+        assertThat(cf.isDone(), equalTo(true));
+    }
+
+    @Test
+    public void exceptionally() {
+        var cf = CompletableFuture
+                .supplyAsync(() -> {
+                    throw new IllegalArgumentException();
+                }).exceptionally(t -> errorHappened = true);
+        assertThat(errorHappened, equalTo(true));
+        assertThat(cf.isDone(), equalTo(true));
+    }
+
+    @Test
     public void completeExceptionally() {
-        var cf = new CompletableFuture<String>();
+        var cf = CompletableFuture.supplyAsync(() -> "The result");
         var expException = new RuntimeException("Computation failed");
         cf.completeExceptionally(expException);
         assertThat(cf.isDone(), equalTo(true));
