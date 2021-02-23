0157852c430e100d7efbcfd15912e81f1af08029
Aleksey Yablokov <ya_al@bk.ru>
2021-02-23 22:26:44 +0200

    Java: Azure: simplify requestOboAccessToken()

--- Java+/Libs+/Azure+/MSAL/src/azure/flow/authcode/common/HttpClientHelper.java
+++ Java+/Libs+/Azure+/MSAL/src/azure/flow/authcode/common/HttpClientHelper.java
@@ -8,6 +8,7 @@ import com.microsoft.aad.msal4j.UserAssertion;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -33,20 +34,18 @@ public class HttpClientHelper {
     }
 
     public static String requestOboAccessToken(String clientId, String clientSecret,
-                                               String authority, Set<String> scopes, String userAccessToken)
-            throws java.net.MalformedURLException {
-        var clientCredential = ClientCredentialFactory.createFromSecret(clientSecret);
-        var app = ConfidentialClientApplication.builder(clientId, clientCredential)
-                .authority(authority)
-                .build();
-        var userAssertion = new UserAssertion(userAccessToken);
-        var params = OnBehalfOfParameters.builder(scopes, userAssertion).build();
-        IAuthenticationResult result;
+                                               String authority, Set<String> scopes, String userAccessToken) {
         try {
-            result = app.acquireToken(params).get();
-        } catch (InterruptedException | ExecutionException e) {
+            var clientCredential = ClientCredentialFactory.createFromSecret(clientSecret);
+            var app = ConfidentialClientApplication.builder(clientId, clientCredential)
+                    .authority(authority)
+                    .build();
+            var userAssertion = new UserAssertion(userAccessToken);
+            var params = OnBehalfOfParameters.builder(scopes, userAssertion).build();
+            var result = app.acquireToken(params).get();
+            return result.accessToken();
+        } catch (MalformedURLException | InterruptedException | ExecutionException e) {
             throw new RuntimeException(e);
         }
-        return result.accessToken();
     }
 }
