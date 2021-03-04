3f491b609e3c66e469f7b3153317680c5ca7987c
Aleksey Yablokov <ya_al@bk.ru>
2021-03-04 13:24:09 +0200

    Scala: ScalaSbt: fix SslSpec

--- Scala+/ScalaSbt/PlayFramework+/play-framework-examples/test/controllers/ssl/SslSpec.scala
+++ Scala+/ScalaSbt/PlayFramework+/play-framework-examples/test/controllers/ssl/SslSpec.scala
@@ -1,8 +1,9 @@
 package controllers.ssl
 
 import controllers.ssl.SslController.BODY
+import org.apache.http.client.RedirectStrategy
 import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.HttpClients
+import org.apache.http.impl.client.{DefaultRedirectStrategy, HttpClients}
 import org.scalatest.TestData
 import org.scalatest.time.SpanSugar.convertIntToGrainOfTime
 import org.scalatestplus.play._
@@ -11,6 +12,7 @@ import play.api.{Application, Configuration}
 import play.api.http.Status.OK
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.ws.WSClient
+import play.api.test.Helpers.baseApplicationBuilder.build
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
@@ -18,19 +20,21 @@ import scala.io.Source
 
 class SslSpec extends PlaySpec with GuiceOneServerPerTest {
 
-//  implicit override def newAppForTest(testData: TestData): Application = {
-//    new GuiceApplicationBuilder().configure(Map(
-//      "a" -> "b"
-//      "play.server.https.keyStore.path" -> "/home/aleks/pr/home/yaal_examples/Java+/JEE+/Jetty/resources/handler/ssl/server_keystore.jks",
-//      "play.server.https.keyStore.type" -> "JKS",
-//      "play.server.https.keyStore.password" -> "098765",
-//      "http.port" -> "disabled",
-//      "https.port" -> "9443",
-//      "play.ws.ssl.trustManager.stores.0.type" -> "JKS",
-//      "play.ws.ssl.trustManager.stores.0.path" -> "/home/aleks/pr/home/yaal_examples/Java+/JEE+/Jetty/resources/handler/ssl/client_truststore.jks",
-//      "play.ws.ssl.trustManager.stores.0.password" -> "654321",
-//    )).build()
-//  }
+  implicit override def newAppForTest(testData: TestData): Application = {
+    new GuiceApplicationBuilder().configure(Map(
+      "play.server.https.keyStore.path" -> "/tmp/jetty/certpath/server_keystore.jks",
+      "play.server.https.keyStore.type" -> "JKS",
+      "play.server.https.keyStore.password" -> "098765",
+      "play.server.https.keyStore.algorithm" -> "RSA",
+      "https.keyStore" -> "/tmp/jetty/certpath/server_keystore.jks",
+      "https.keyStorePassword" -> "098765",
+      "http.port" -> "disabled",
+      "https.port" -> "9443",
+      "play.ws.ssl.trustManager.stores.0.type" -> "JKS",
+      "play.ws.ssl.trustManager.stores.0.path" -> "/tmp/jetty/certpath/client_truststore.jks",
+      "play.ws.ssl.trustManager.stores.0.password" -> "654321",
+    )).build()
+  }
 
   "Test" should {
 //    "override FakeApplication" in {
@@ -38,13 +42,12 @@ class SslSpec extends PlaySpec with GuiceOneServerPerTest {
 //    }
 
     "setup SSL connection" in {
-      val endpoint = runningServer.endpoints.httpEndpoint.get
-      println("Endpoint port: " + endpoint.port)
-//      Thread.sleep(60*1000)
+      val endpoint = runningServer.endpoints.httpsEndpoint.get
 //      val wsClient = app.injector.instanceOf[WSClient]
-      val url = s"http://localhost:${endpoint.port}/ssl"
+      val url = s"https://localhost:${endpoint.port}/ssl"
       println("URL: " + url)
-//      val future = wsClient.url(url)
+      Thread.sleep(60*1000)
+      //      val future = wsClient.url(url)
 //        .withRequestTimeout(10000.millis)
 //        .withFollowRedirects(true)
 //        .get()
