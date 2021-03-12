24e0565976999150c0885ee9a40af23122763c6f
Aleksey Yablokov <ya_al@bk.ru>
2021-03-12 16:23:35 +0200

    Java: Typesafe Config: improve YamlToJson

--- Java+/Libs+/TypesafeConfig/test/config/LoadFromJsonFile.java
+++ Java+/Libs+/TypesafeConfig/test/config/LoadFromJsonFile.java
@@ -10,11 +10,9 @@ public class LoadFromJsonFile extends BaseTest {
     @Test
     public void loadFromJson() {
         var conf = ConfigFactory.load("config/LoadFromJsonFile.json");
-
-        var host = conf.getString("parameters.host");
-        var port = conf.getInt("parameters.port");
-
-        assertThat(host, equalTo("localhost"));
-        assertThat(port, equalTo(8080));
+        assertThat(conf.getString("parameters.host"), equalTo("localhost"));
+        assertThat(conf.getInt("parameters.port"), equalTo(8080));
+        assertThat(conf.hasPath("version.major"), equalTo(false));
+        assertThat(conf.hasPath("version.minor"), equalTo(false));
     }
 }
