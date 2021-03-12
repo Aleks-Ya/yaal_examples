d2abcd7aacabe800481572299fbcd27c95d891b3
Aleksey Yablokov <ya_al@bk.ru>
2021-03-12 11:08:07 +0200

    Java: Typesafe Config: add GetProperty

---
+++ Java+/Libs+/TypesafeConfig/test/config/GetProperty.java
@@ -0,0 +1,22 @@
+package config;
+
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigUtil;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class GetProperty extends BaseTest {
+
+    @Test
+    public void joinPath() {
+        var json = "{\"kafka\":{\"common\":{\"bootstrap.servers\":\"kfk.awseuc1.tst.edh.cnb:9093\"}}}";
+        var conf = ConfigFactory.parseString(json);
+        assertThat(conf.getValue("kafka"), notNullValue());
+        assertThat(conf.getValue("kafka.common"), notNullValue());
+        var path = ConfigUtil.joinPath("kafka", "common", "bootstrap.servers");
+        assertThat(conf.getString(path), equalTo("kfk.awseuc1.tst.edh.cnb:9093"));
+    }
+}
