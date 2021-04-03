e1b03d67e70988295649fcc38cdf8868fe91a416
Aleksey Yablokov <ya_al@bk.ru>
2021-04-03 06:27:02 +0200

    Java: GCP: improve DownloadObject

--- Java+/Libs+/Cloud+/GCP+/GoogleCloudStorage/test/cloud/DownloadObject.java
+++ Java+/Libs+/Cloud+/GCP+/GoogleCloudStorage/test/cloud/DownloadObject.java
@@ -2,51 +2,37 @@ package cloud;
 
 
 import com.google.auth.oauth2.ServiceAccountCredentials;
-import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobId;
-import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 
 public class DownloadObject {
     @Test
-    public void name() throws IOException {
-
-        // The ID of your GCP project
-        String projectId = "enterprise-data-hub-dev";
-
-        // The ID of your GCS bucket
-        String bucketName = "iablokov-test-bucket";
-
-        // The ID of your GCS object
-        String objectName = "file1.txt";
-
-        // The path to which the file should be downloaded
-        String destFilePath = Files.createTempFile(DownloadObject.class.getSimpleName(), ".txt").toString();
-        System.out.println("Destination file: " + destFilePath);
-
-        Storage storage = StorageOptions.newBuilder()
-//                .setCredentials(ServiceAccountCredentials.fromStream(new FileInputStream(configs.googleCredentials)))
+    public void download() throws IOException {
+        var projectId = "enterprise-data-hub-dev";
+        var bucketName = "iablokov-test-bucket";
+        var objectName = "file1.txt";
+        var credentialsFile = System.getProperty("credentialsFile");
+        if (credentialsFile == null) {
+            throw new RuntimeException("'credentialsFile' system property absents");
+        }
+        var storage = StorageOptions.newBuilder()
+                .setCredentials(ServiceAccountCredentials.fromStream(new FileInputStream(credentialsFile)))
                 .setProjectId(projectId)
-                .build().getService();
-
-        Blob blob = storage.get(BlobId.of(bucketName, objectName));
-        blob.downloadTo(Paths.get(destFilePath));
-
-        System.out.println(
-                "Downloaded object "
-                        + objectName
-                        + " from bucket name "
-                        + bucketName
-                        + " to "
-                        + destFilePath);
-
+                .build()
+                .getService();
+        var os = new ByteArrayOutputStream();
+        var blob = storage.get(BlobId.of(bucketName, objectName));
+        blob.downloadTo(os);
+        assertThat(os.toString(), equalTo("abc"));
     }
 
 }
