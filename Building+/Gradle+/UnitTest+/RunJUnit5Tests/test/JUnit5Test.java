e41106b9a4b4f7e65788e5d120981ce0fbaa6528
Aleksey Yablokov <ya_al@bk.ru>
2021-04-18 20:46:03 +0200

    Gradle: add RunJUnit5Tests

---
+++ Building+/Gradle+/UnitTest+/RunJUnit5Tests/test/JUnit5Test.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JUnit5Test {
+    @Test
+    public void test1() {
+        assertTrue(true);
+    }
+}
