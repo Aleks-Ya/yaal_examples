51acd1539ac48b3757bc7ede5d3d820c07d7368a
Aleksey Yablokov <ya_al@bk.ru>
2021-05-09 21:44:17 +0300

    Java: JSE: format CountDownLatchTest

--- Java+/JSE+/Core/test/util/concurrent/latch/CountDownLatchTest.java
+++ Java+/JSE+/Core/test/util/concurrent/latch/CountDownLatchTest.java
@@ -2,41 +2,37 @@ package util.concurrent.latch;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 
 class CountDownLatchTest {
     @Test
-    void latch() throws InterruptedException, ExecutionException {
+    void latch() {
         var log = new StringBuffer();
         var latch = new CountDownLatch(2);
-        Callable<Void> t1 = () -> {
-            log.append("t1 is waiting for t2 and t3\n");
-            latch.await();
-            log.append("t1 finished waiting\n");
-            return null;
+        Runnable r1 = () -> {
+            try {
+                log.append("r1 is waiting for r2 and r3\n");
+                latch.await();
+                log.append("r1 finished waiting\n");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         };
-        Callable<Void> t2 = () -> {
-            log.append("t2 is ready to count down the latch\n");
+        Runnable r2 = () -> {
+            log.append("r2 is ready to count down the latch\n");
             latch.countDown();
-            log.append("t2 has counted down the latch\n");
-            return null;
+            log.append("r2 has counted down the latch\n");
         };
-        Callable<Void> t3 = () -> {
-            log.append("t3 is ready to count down the latch\n");
+        Runnable r3 = () -> {
+            log.append("r3 is ready to count down the latch\n");
             latch.countDown();
-            log.append("t3 has counted down the latch\n");
-            return null;
+            log.append("r3 has counted down the latch\n");
         };
         var executor = Executors.newFixedThreadPool(3);
-        var f1 = executor.submit(t1);
-        var f2 = executor.submit(t2);
-        var f3 = executor.submit(t3);
-        f1.get();
-        f2.get();
-        f3.get();
+        executor.execute(r1);
+        executor.execute(r2);
+        executor.execute(r3);
         System.out.println(log);
     }
 }
