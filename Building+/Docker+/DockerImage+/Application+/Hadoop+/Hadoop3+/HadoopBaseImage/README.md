6bc30730bc7b6ba63162babd583faea9b3d89fe0
Aleksey Yablokov <ya_al@bk.ru>
2021-06-07 14:00:06 +0300

    Hadoop: add Hadoop3+

---
+++ Building+/Docker+/DockerImage+/Application+/Hadoop+/Hadoop3+/HadoopBaseImage/README.md
@@ -0,0 +1,11 @@
+# Hadoop Single Node Cluster
+
+## Documentation
+https://hadoop.apache.org/docs/r2.9.2/hadoop-project-dist/hadoop-common/SingleCluster.html
+
+## Build image
+Hadoop version: 2.10.1, 3.2.2 (version list: http://apache-mirror.rbc.ru/pub/apache/hadoop/common/)
+
+Image version: 1.0 (tag for created Docker image)
+
+`./build.sh 3.2.2 1.0`
---
+++ Building+/Docker+/DockerImage+/Application+/Hadoop+/Hadoop3+/SparkBaseImage/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export HADOOP_VERSION=$1
+export HADOOP_IMAGE_VERSION=$2
+export SPARK_VERSION=$3
+export IMAGE_VERSION=$4
+
+if [[ -z ${HADOOP_VERSION} ]] || [[ -z ${HADOOP_IMAGE_VERSION} ]] || [[ -z ${SPARK_VERSION} ]] || [[ -z ${IMAGE_VERSION} ]]; then
+  echo 'Error: wrong parameters. Example: "./build.sh 3.2.2 1.0 3.1.2 1.0"'
+  exit 1
+fi
+if [[ $HADOOP_VERSION == 3* ]];  then
+	export HADOOP_BUILD_VERSION=3.2
+else
+	export HADOOP_BUILD_VERSION=2.7
+fi
+
+echo "Hadoop image: ${HADOOP_IMAGE}"
+echo "Spark version: ${SPARK_VERSION}"
+echo "Hadoop build version: ${HADOOP_BUILD_VERSION}"
+echo "This image version: ${IMAGE_VERSION}"
+
+docker build \
+  --build-arg HADOOP_IMAGE=hadoop-base-image-${HADOOP_VERSION}:${HADOOP_IMAGE_VERSION} \
+  --build-arg SPARK_VERSION=${SPARK_VERSION} \
+  --build-arg HADOOP_BUILD_VERSION=${HADOOP_BUILD_VERSION} \
+  -t spark-base-image-${SPARK_VERSION}-hadoop-${HADOOP_VERSION}:${IMAGE_VERSION} \
+  .
