64a0de5793302966b90282703bd8c1e2b245e4a4
Aleksey Yablokov <ya_al@bk.ru>
2021-07-02 08:24:49 +0300

    PySpark: add write_empty_df_to_csv.py

---
+++ BigData+/Spark+/PySpark/src/dataframe/write_empty_df_to_csv.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+
+import os
+import tempfile
+
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, LongType
+
+from src.util import create_spark_and_sql_contexts
+
+sparkContext, sqlContext = create_spark_and_sql_contexts(__file__)
+
+schema = StructType([
+    StructField("id", LongType(), False),
+    StructField("name", StringType(), False),
+    StructField("age", IntegerType(), True)])
+values = []
+
+df = sqlContext.createDataFrame(values, schema)
+df.show()
+
+temp_dir: str = tempfile.mkdtemp()
+output_file = os.path.join(temp_dir, 'utf-8.csv')
+df.write.option('header', 'true').csv(output_file)
+sparkContext.stop()
+
+print(f'Output file: "{output_file}"')
