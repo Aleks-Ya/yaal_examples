6def50c0499f108b41b2fda957d830609fc29ba8
Aleksey Yablokov <ya_al@bk.ru>
2021-07-16 07:21:54 +0300

    PySpark: find Python executable

--- BigData+/Spark+/PySpark/src/util/__init__.py
+++ BigData+/Spark+/PySpark/src/util/__init__.py
@@ -5,9 +5,18 @@ from pyspark.sql.context import SQLContext
 from pyspark.sql.session import SparkSession
 
 
+def find_python() -> str:
+    names = ['python3', 'python']
+    for name in names:
+        python = shutil.which(name)
+        if python is not None:
+            return python
+    raise Exception('Python executable not found')
+
+
 def create_spark_session(app_name: str) -> SparkSession:
     # os.environ['JAVA_HOME'] = '/home/aleks/.sdkman/candidates/java/8.0.222-zulu'
-    python = shutil.which('python')
+    python = find_python()
     os.environ['PYSPARK_PYTHON'] = python
     os.environ['PYSPARK_DRIVER_PYTHON'] = python
     return SparkSession \
@@ -18,5 +27,5 @@ def create_spark_session(app_name: str) -> SparkSession:
 
 def create_spark_session_and_sql_contexts(app_name: str) -> (SparkSession, SQLContext):
     ss = create_spark_session(app_name)
-    sql = SQLContext(ss)
+    sql = SQLContext(ss.sparkContext)
     return ss, sql
