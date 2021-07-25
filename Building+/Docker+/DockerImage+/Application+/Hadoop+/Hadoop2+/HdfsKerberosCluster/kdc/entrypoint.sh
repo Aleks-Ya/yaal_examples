0a82e5e5f1bfe7b1768e349fdd8f3e1699616930
Aleksey Yablokov <ya_al@bk.ru>
2021-07-26 00:08:24 +0300

    Hadoop: HdfsKerberosCluster: format KDC files

--- Building+/Docker+/DockerImage+/Application+/Hadoop+/Hadoop2+/HdfsKerberosCluster/kdc/entrypoint.sh
+++ Building+/Docker+/DockerImage+/Application+/Hadoop+/Hadoop2+/HdfsKerberosCluster/kdc/entrypoint.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
 set -e
 
-export KERBEROS_SHARED=/tmp/kerberos
+KERBEROS_SHARED=/tmp/kerberos
 cp "$KRB5_CONFIG" "$KERBEROS_SHARED"
 
-kadmin.local -q "xst -norandkey -k $KERBEROS_SHARED/sserver.keytab $SSERVER_PRINCIPAL"
-kadmin.local -q "xst -norandkey -k $KERBEROS_SHARED/sclient.keytab $SCLIENT_PRINCIPAL"
+NN_KEYTAB="$KERBEROS_SHARED/nn.keytab"
+DN2_KEYTAB="$KERBEROS_SHARED/dn2.keytab"
+DN3_KEYTAB="$KERBEROS_SHARED/dn3.keytab"
+CLIENT_KEYTAB="$KERBEROS_SHARED/client.keytab"
+SSERVER_KEYTAB="$KERBEROS_SHARED/sserver.keytab"
+SCLIENT_KEYTAB="$KERBEROS_SHARED/sclient.keytab"
 
-kadmin.local -q "xst -norandkey -k $KERBEROS_SHARED/nn.keytab nn/hdfs-master.hdfs.yaal.ru@HADOOPCLUSTER.LOCAL"
-kadmin.local -q "ktadd -k $KERBEROS_SHARED/nn.keytab dn/hdfs-master.hdfs.yaal.ru@HADOOPCLUSTER.LOCAL"
-kadmin.local -q "ktadd -k $KERBEROS_SHARED/nn.keytab host/hdfs-master.hdfs.yaal.ru@HADOOPCLUSTER.LOCAL"
+kadmin.local -q "xst -norandkey -k $NN_KEYTAB $NN_MASTER_PRINCIPAL"
+kadmin.local -q "ktadd -k $NN_KEYTAB $DN_MASTER_PRINCIPAL"
+kadmin.local -q "ktadd -k $NN_KEYTAB $HOST_MASTER_PRINCIPAL"
 
-kadmin.local -q "xst -norandkey -k $KERBEROS_SHARED/dn2.keytab dn/hdfs-slave1.hdfs.yaal.ru@HADOOPCLUSTER.LOCAL"
-kadmin.local -q "ktadd -k $KERBEROS_SHARED/dn2.keytab host/hdfs-slave1.hdfs.yaal.ru@HADOOPCLUSTER.LOCAL"
+kadmin.local -q "xst -norandkey -k $DN2_KEYTAB $DN_SLAVE1_PRINCIPAL"
+kadmin.local -q "ktadd -k $DN2_KEYTAB $HOST_SLAVE1_PRINCIPAL"
 
-kadmin.local -q "xst -norandkey -k $KERBEROS_SHARED/dn3.keytab dn/hdfs-slave2.hdfs.yaal.ru@HADOOPCLUSTER.LOCAL"
-kadmin.local -q "ktadd -k $KERBEROS_SHARED/dn3.keytab host/hdfs-slave2.hdfs.yaal.ru@HADOOPCLUSTER.LOCAL"
+kadmin.local -q "xst -norandkey -k $DN3_KEYTAB $DN_SLAVE2_PRINCIPAL"
+kadmin.local -q "ktadd -k $DN3_KEYTAB $HOST_SLAVE2_PRINCIPAL"
 
-kadmin.local -q "xst -norandkey -k $KERBEROS_SHARED/client.keytab client@HADOOPCLUSTER.LOCAL"
+kadmin.local -q "xst -norandkey -k $CLIENT_KEYTAB $CLIENT_PRINCIPAL"
+
+kadmin.local -q "xst -norandkey -k $SSERVER_KEYTAB $SSERVER_PRINCIPAL"
+kadmin.local -q "xst -norandkey -k $SCLIENT_KEYTAB $SCLIENT_PRINCIPAL"
 
 cp "$KRB5_CONFIG" "$KERBEROS_SHARED"
 chmod a+r "$KERBEROS_SHARED"/*.keytab #TODO use group permission
 ls -l "$KERBEROS_SHARED"
 
-klist -kte "$KERBEROS_SHARED/nn.keytab"
-klist -kte "$KERBEROS_SHARED/dn2.keytab"
-klist -kte "$KERBEROS_SHARED/dn3.keytab"
-klist -kte "$KERBEROS_SHARED/sserver.keytab"
-klist -kte "$KERBEROS_SHARED/sclient.keytab"
-klist -kte "$KERBEROS_SHARED/client.keytab"
+klist -kte "$NN_KEYTAB"
+klist -kte "$DN2_KEYTAB"
+klist -kte "$DN3_KEYTAB"
+klist -kte "$CLIENT_KEYTAB"
+klist -kte "$SSERVER_KEYTAB"
+klist -kte "$SCLIENT_KEYTAB"
 
 bash
