7038de7d66a1f7c0fbf723f301884cc615b7c655
Aleksey Yablokov <ya_al@bk.ru>
2021-07-25 07:11:28 +0300

    Kerberos: SserverSclient: simplify

--- Building+/Docker+/DockerImage+/Application+/Kerberos+/SserverSclient/kdc/entrypoint.sh
+++ Building+/Docker+/DockerImage+/Application+/Kerberos+/SserverSclient/kdc/entrypoint.sh
@@ -7,7 +7,7 @@ cp "$KRB5_CONFIG" "$KERBEROS_SHARED"
 kadmin.local -q "xst -norandkey -k $KERBEROS_SHARED/sserver.keytab $SSERVER_PRINCIPAL"
 kadmin.local -q "xst -norandkey -k $KERBEROS_SHARED/sclient.keytab $SCLIENT_PRINCIPAL"
 
-klist -kt "$KERBEROS_SHARED/sserver.keytab"
-klist -kt "$KERBEROS_SHARED/sclient.keytab"
+klist -kte "$KERBEROS_SHARED/sserver.keytab"
+klist -kte "$KERBEROS_SHARED/sclient.keytab"
 
 bash
