4d8bec4d38e66a31f3ebd8176afd438bc124e7e3
Aleksey Yablokov <ya_al@bk.ru>
2021-07-27 22:23:54 +0300

    Hadoop: YarnHdfsKerberosCluster: add run_yarn_example.sh

---
+++ Building+/Docker+/DockerImage+/Application+/Hadoop+/Hadoop2+/YarnHdfsKerberosCluster/hadoop/run_yarn_example.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+kinit -kt /tmp/kerberos/client.keytab client@HADOOPCLUSTER.LOCAL
+
+USER="$(whoami)"
+echo "User: $USER"
+
+LOCAL_DIR=/tmp/app
+echo "Local working dir: ${LOCAL_DIR}"
+rm -rf ${LOCAL_DIR}
+mkdir -p ${LOCAL_DIR}/input
+mkdir -p ${LOCAL_DIR}/output
+cp "${HADOOP_PREFIX}"/etc/hadoop/*.xml "${LOCAL_DIR}"/input
+
+HDFS_DIR=/tmp/hadoop-cluster-master
+echo "HDFS working dir: ${HDFS_DIR}"
+hdfs dfs -rm -r -f ${HDFS_DIR}
+hdfs dfs -mkdir -p ${HDFS_DIR}/input
+hdfs dfs -put "${HADOOP_PREFIX}"/etc/hadoop/* "${HDFS_DIR}"/input/
+
+hdfs dfs -mkdir -p /user/"${USER}"
+
+echo "Running YARN application..."
+hadoop jar "${HADOOP_PREFIX}"/share/hadoop/mapreduce/hadoop-mapreduce-examples-"${HADOOP_VERSION}".jar \
+  grep "${HDFS_DIR}"/input "${HDFS_DIR}"/output 'dfs[a-z.]+'
+
+hdfs dfs -get ${HDFS_DIR}/output/* ${LOCAL_DIR}/output/
+cat ${LOCAL_DIR}/output/*
