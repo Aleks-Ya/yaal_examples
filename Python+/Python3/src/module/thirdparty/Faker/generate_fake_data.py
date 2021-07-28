512f90bb6d54264b35773b57858ea464b04ded4a
Aleksey Yablokov <ya_al@bk.ru>
2021-07-28 14:45:55 +0300

    Python: Faker: add timestamp field

--- Python+/Python3/src/module/thirdparty/Faker/generate_fake_data.py
+++ Python+/Python3/src/module/thirdparty/Faker/generate_fake_data.py
@@ -7,10 +7,16 @@ Faker.seed(4321)  # Repeat the same result each test
 name = fake.name()
 address = fake.address()
 text = fake.text()
+date_time = fake.date_time()
+unix_time = fake.unix_time()
+iso8601 = fake.iso8601()
 
-print(name)
-print(address)
-print(text)
+print(f"name={name}")
+print(f"address={address}")
+print(f"text={text}")
+print(f"date_time={date_time}")
+print(f"unix_time={unix_time}")
+print(f"iso8601={iso8601}")
 
 assert name == 'Jason Brown'
 assert address == """02907 Jenkins Fort Suite 672
