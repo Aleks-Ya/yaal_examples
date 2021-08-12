fb20cecc766aaff6770a4ab83a966ca973f3efb9
Aleksey Yablokov <ya_al@bk.ru>
2021-08-12 19:52:12 +0300

    Java: Spring: format SpringRestTemplate

--- Java+/Spring+/Spring5+/Web+/SpringRestTemplate/test/get/GetForObjectTest.java
+++ Java+/Spring+/Spring5+/Web+/SpringRestTemplate/test/get/GetForObjectTest.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,40 +16,40 @@ import static org.springframework.test.web.client.match.MockRestRequestMatchers.
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
-public class GetForObjectTest {
+class GetForObjectTest {
 
     @Test
-    public void getForObjectTest() {
-        RestTemplate restTemplate = new RestTemplate();
+    void getForObjectTest() {
+        var restTemplate = new RestTemplate();
 
-        final String url = "/localhost";
-        final MockRestServiceServer server = MockRestServiceServer.bindTo(restTemplate).build();
+        var url = "/localhost";
+        var server = MockRestServiceServer.bindTo(restTemplate).build();
         server.expect(once(), requestTo(url))
                 .andExpect(method(HttpMethod.GET))
                 .andRespond(withSuccess("{}", MediaType.APPLICATION_JSON));
 
-        Quote response = restTemplate.getForObject(url, Quote.class);
+        var response = restTemplate.getForObject(url, Quote.class);
         System.out.println(response);
     }
 
     @Test
-    public void withHeaders() {
-        RestTemplate restTemplate = new RestTemplate();
-        String headerName = "myheader";
-        String headerValue = "abc";
-        final String url = "/localhost";
-        final MockRestServiceServer server = MockRestServiceServer.bindTo(restTemplate).build();
+    void withHeaders() {
+        var restTemplate = new RestTemplate();
+        var headerName = "myheader";
+        var headerValue = "abc";
+        var url = "/localhost";
+        var server = MockRestServiceServer.bindTo(restTemplate).build();
         server.expect(once(), requestTo(url))
                 .andExpect(method(HttpMethod.GET))
                 .andExpect(header(headerName, equalTo(headerValue)))
                 .andRespond(withSuccess("{}", MediaType.APPLICATION_JSON));
 
-        HttpHeaders headers = new HttpHeaders();
+        var headers = new HttpHeaders();
         headers.add(headerName, headerValue);
 
-        HttpEntity<String> entity = new HttpEntity<>("body", headers);
+        var entity = new HttpEntity<>("body", headers);
 
-        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        var responseEntity = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
         System.out.println(responseEntity.getBody());
     }
 }
--- Java+/Spring+/Spring5+/Web+/SpringRestTemplate/test/post/PostForObjectTest.java
+++ Java+/Spring+/Spring5+/Web+/SpringRestTemplate/test/post/PostForObjectTest.java
@@ -17,48 +17,48 @@ import static org.springframework.test.web.client.match.MockRestRequestMatchers.
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
-public class PostForObjectTest {
+class PostForObjectTest {
 
     @Test
-    public void customHeader() {
-        RestTemplate restTemplate = new RestTemplate();
+    void customHeader() {
+        var restTemplate = new RestTemplate();
 
-        String headerName = "myheader";
-        String headerValue = "abc";
-        final String url = "/localhost/path2";
-        final MockRestServiceServer server = MockRestServiceServer.bindTo(restTemplate).build();
+        var headerName = "myheader";
+        var headerValue = "abc";
+        var url = "/localhost/path2";
+        var server = MockRestServiceServer.bindTo(restTemplate).build();
         server.expect(once(), requestTo(url))
                 .andExpect(method(HttpMethod.POST))
                 .andExpect(header(headerName, equalTo(headerValue)))
                 .andRespond(withSuccess("{}", MediaType.APPLICATION_JSON));
 
 
-        HttpHeaders headers = new HttpHeaders();
+        var headers = new HttpHeaders();
         headers.add(headerName, headerValue);
-        HttpEntity<String> entity = new HttpEntity<>("body", headers);
+        var entity = new HttpEntity<>("body", headers);
 
-        Quote quote = restTemplate.postForObject(url, entity, Quote.class);
+        var quote = restTemplate.postForObject(url, entity, Quote.class);
         System.out.println(quote);
     }
 
     @Test
-    public void bodyTest() {
-        final RestTemplate restTemplate = new RestTemplate();
+    void bodyTest() {
+        final var restTemplate = new RestTemplate();
 
-        HttpHeaders headers = new HttpHeaders();
+        var headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        String body = "{key: value}";
-        HttpEntity<String> entity = new HttpEntity<>(body, headers);
+        var body = "{key: value}";
+        var entity = new HttpEntity<>(body, headers);
 
-        final String URL = "/localhost/path";
-        final MockRestServiceServer server = MockRestServiceServer.bindTo(restTemplate).build();
+        var URL = "/localhost/path";
+        var server = MockRestServiceServer.bindTo(restTemplate).build();
         server.expect(once(), requestTo(URL)).andExpect(method(HttpMethod.POST))
                 .andExpect(jsonPath("key", equalTo("value")))
                 .andExpect(header("content-type", equalTo(MediaType.APPLICATION_JSON_VALUE)))
                 .andRespond(withSuccess("{}", MediaType.APPLICATION_JSON));
 
-        Quote quote = restTemplate.postForObject(URL, entity, Quote.class);
+        var quote = restTemplate.postForObject(URL, entity, Quote.class);
         System.out.println(quote);
 
         server.verify();
