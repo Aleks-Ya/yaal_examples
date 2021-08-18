16bc5d00cc204ad1ffee0838defb4eddebe7dbbe
Aleksey Yablokov <ya_al@bk.ru>
2021-08-18 15:45:28 +0300

    Java: add WireMock

---
+++ Java+/UnitTest+/WireMock/test/wiremock/WireMockTest.java
@@ -0,0 +1,47 @@
+package wiremock;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WireMockTest {
+
+    @Test
+    void hello() throws IOException, InterruptedException {
+        var server = new WireMockServer();
+        server.start();
+
+        var host = "localhost";
+        var port = server.port();
+        configureFor(host, port);
+        var expBody = "Hello, World!";
+        var path = "/hello";
+        stubFor(get(urlEqualTo(path)).willReturn(aResponse().withBody(expBody)));
+
+        var url = URI.create(server.url(path));
+        var request = HttpRequest.newBuilder().uri(url).GET().build();
+
+        var response = HttpClient.newHttpClient()
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        var actBody = response.body();
+
+        verify(getRequestedFor(urlEqualTo(path)));
+        assertThat(expBody).isEqualTo(actBody);
+
+        server.stop();
+    }
+}
