4ae337305e3ddf6832860be991f1189cf6c48cfd
Aleksey Yablokov <ya_al@bk.ru>
2021-09-01 15:05:17 +0300

    Gradle: Sonar: add Jacoco

---
+++ Building+/Gradle+/SonarQube+/SingleModule/src/test/java/coverage/PersonServiceTest.java
@@ -0,0 +1,16 @@
+package coverage;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PersonServiceTest {
+    @Test
+    void getListPersons() {
+        var service = new PersonService();
+        var persons = service.getPersons();
+        assertEquals(List.of("JOHN", "MARY", "MARK"), persons);
+    }
+}
