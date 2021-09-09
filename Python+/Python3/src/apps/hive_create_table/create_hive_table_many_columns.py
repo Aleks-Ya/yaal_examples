40504dd983a224af0300403d64b83e022242353a
Yablokov Aleksey <AAYablokov@sberbank.ru>
2021-09-09 07:43:48 +0300

    Python: add create_hive_table_many_columns.py

---
+++ Python+/Python3/src/apps/hive_create_table/create_hive_table_many_columns.py
@@ -0,0 +1,33 @@
+# Generate create script for a Hive table having specified number of columns
+import tempfile
+
+column_number = 100
+row_number = 10
+
+output_file = tempfile.mkstemp(suffix=".sql")[1]
+table_name = f'yablokov_many_columns_{column_number}'
+
+drop = f'DROP TABLE IF EXISTS {table_name};\n'
+
+column_list = [f'col{column} string' for column in range(column_number)]
+column_str = ','.join(column_list)
+create = f"CREATE TABLE {table_name} (\n" \
+         f"{column_str}" \
+         f"\n) " \
+         f"row format delimited fields terminated by ',' " \
+         f"stored as TextFile;\n"
+
+values_list = []
+for row in range(row_number):
+    row_values = [f"'r{row}c{column}'" for column in range(column_number)]
+    row_str = ','.join(row_values)
+    values_list.append(row_str)
+
+inserts = [f"INSERT INTO {table_name} VALUES({value});" for value in values_list]
+insert_str = '\n'.join(inserts)
+
+print(f'Output file: {output_file}')
+with open(output_file, 'w') as f:
+    f.write(drop)
+    f.write(create)
+    f.write(insert_str)
--- Python+/Python3/src/module/standard/csv/generate_many_columns_csv.py
+++ Python+/Python3/src/module/standard/csv/generate_many_columns_csv.py
@@ -2,8 +2,10 @@ import csv
 import tempfile
 
 column_number = 100
-column_digit_capacity = len(str(column_number - 1))
 row_number = 50
+add_header = True
+
+column_digit_capacity = len(str(column_number - 1))
 row_digit_capacity = len(str(row_number - 1))
 
 _, file = tempfile.mkstemp(suffix=".csv")
@@ -11,8 +13,9 @@ print(file)
 
 with open(file, 'w', newline='') as csv_file:
     writer = csv.writer(csv_file)
-    column_name_template = 'col{:0' + str(column_digit_capacity) + '}'
-    writer.writerow([column_name_template.format(column) for column in range(column_number)])
+    if add_header:
+        column_name_template = 'col{:0' + str(column_digit_capacity) + '}'
+        writer.writerow([column_name_template.format(column) for column in range(column_number)])
     for row in range(row_number):
         row_value_template = 'r{:0' + str(row_digit_capacity) + '}c{:0' + str(column_digit_capacity) + '}'
         row_values = [row_value_template.format(row, column) for column in range(column_number)]
