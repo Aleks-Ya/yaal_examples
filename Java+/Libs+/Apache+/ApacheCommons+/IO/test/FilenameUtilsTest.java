be8bb4cc0b528e301affa817450e4f36cc25fd98
Aleksey Yablokov <ya_al@bk.ru>
2021-09-16 16:08:26 +0300

    Java: add FilenameUtilsTest

---
+++ Java+/Libs+/Apache+/ApacheCommons+/IO/test/FilenameUtilsTest.java
@@ -0,0 +1,17 @@
+import org.apache.commons.io.FilenameUtils;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class FilenameUtilsTest {
+
+    @Test
+    void concat() {
+        var file = FilenameUtils.concat("tmp", "file.txt");
+        var expPath = Paths.get("tmp", "file.txt");
+        assertThat(file).isEqualTo(expPath.toString());
+    }
+}
