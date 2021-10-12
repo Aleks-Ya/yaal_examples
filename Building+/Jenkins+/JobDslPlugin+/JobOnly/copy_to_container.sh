6d2aef59fd4773f9b6540d06bcb6d3af524500cc
Aleksey Yablokov <ya_al@bk.ru>
2021-10-12 08:30:11 +0300

    Jenkins: JobOnly: move example pipelines to "examples" dir

--- Building+/Jenkins+/JobDslPlugin+/JobOnly/copy_to_container.sh
+++ Building+/Jenkins+/JobDslPlugin+/JobOnly/copy_to_container.sh
@@ -3,4 +3,5 @@ container=jenkins2
 docker exec $container rm -rf $workspace
 docker exec $container mkdir -p $workspace
 docker cp jobs ${container}:${workspace}
+docker cp examples ${container}:${workspace}
 docker cp gradle ${container}:${workspace}
