947dca49bdb94bace1072c354a6d46a4b2449724
Yablokov Aleksey <AAYablokov@sberbank.ru>
2021-10-13 12:38:33 +0300

    Python: add schema to create_hive_table_many_columns.py

--- Python+/Python3/src/apps/hive_create_table/create_hive_table_many_columns.py
+++ Python+/Python3/src/apps/hive_create_table/create_hive_table_many_columns.py
@@ -1,17 +1,19 @@
 # Generate create script for a Hive table having specified number of columns
 import tempfile
 
+schema='the_schema'
 column_number = 100
 row_number = 10
 
 output_file = tempfile.mkstemp(suffix=".sql")[1]
 table_name = f'yablokov_many_columns_{column_number}'
+table_full_name=f'{schema}.{table_name}'
 
-drop = f'DROP TABLE IF EXISTS {table_name};\n'
+drop = f'DROP TABLE IF EXISTS {table_full_name};\n'
 
 column_list = [f'col{column} string' for column in range(column_number)]
 column_str = ','.join(column_list)
-create = f"CREATE TABLE {table_name} (\n" \
+create = f"CREATE TABLE {table_full_name} (\n" \
          f"{column_str}" \
          f"\n) " \
          f"row format delimited fields terminated by ',' " \
@@ -23,7 +25,7 @@ for row in range(row_number):
     row_str = ','.join(row_values)
     values_list.append(row_str)
 
-inserts = [f"INSERT INTO {table_name} VALUES({value});" for value in values_list]
+inserts = [f"INSERT INTO {table_full_name} VALUES({value});" for value in values_list]
 insert_str = '\n'.join(inserts)
 
 print(f'Output file: {output_file}')
