b0be457ccb714c3a93d7ccbdfad3aa5c9dec370f
Aleksey Yablokov <ya_al@bk.ru>
2021-12-09 14:10:35 +0300

    Python: add base64

---
+++ Python+/Python3/src/module/standard/base64/decode_base64.py
@@ -0,0 +1,8 @@
+# Decode Base64 string to raw string
+import base64
+
+base64_str: str = 'YWJj'
+base64_bytes: bytes = base64_str.encode("utf-8")
+raw_bytes: bytes = base64.b64decode(base64_bytes)
+raw_str: str = str(raw_bytes, "utf-8")
+assert raw_str == "abc"
