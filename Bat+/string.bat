b555090f98a99bb777a27fb8ae08421ad2389ce0
Aleksey Yablokov <ya_al@bk.ru>
2021-12-15 13:32:12 +0300

    Bat: add string.bat

---
+++ Bat+/string.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+REM Docs: https://www.dostips.com/DtTipsStringManipulation.php
+
+REM Remove spaces from a string
+SETLOCAL
+set var= a b  c 
+set var=%var: =%
+echo No spaces: '%var%'
+ENDLOCAL
+
+REM Replace space with zero (for time)
+SETLOCAL
+set var= 2:27:55,30
+set var=%var: =0%
+echo Spaces to zero: '%var%'
+ENDLOCAL
