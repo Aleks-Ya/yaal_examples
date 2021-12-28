ade1ea50b622eb275a39d5d7ff9e97ad3ebad09e
Aleksey Yablokov <ya_al@bk.ru>
2021-12-28 14:14:32 +0300

    Java: AssertJ: add ReflectionAssert

---
+++ Java+/UnitTest+/AssertJ/test/assertj/ReflectionAssert.java
@@ -0,0 +1,21 @@
+package assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ReflectionAssert {
+
+    /**
+     * @see <a href="https://github.com/assertj/assertj-core/issues/1693">add constructor assertions for ClassAssert</a>
+     */
+    @Test
+    void hasConstructor() throws NoSuchMethodException {
+        assertThat(Person.class.getDeclaredConstructor(String.class, Integer.class)).isNotNull();
+    }
+
+    static class Person {
+        Person(String name, Integer age) {
+        }
+    }
+}
