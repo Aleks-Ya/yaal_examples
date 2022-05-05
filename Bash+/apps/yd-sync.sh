2c47760dc9db52f7343671a72e7f7f4c57efe7d0
Aleksei Iablokov <aleksei.iablokov@sap.com>
2022-05-05 08:21:49 +0300

    Bash: update yd-sync.sh

--- Bash+/apps/yd-sync.sh
+++ Bash+/apps/yd-sync.sh
@@ -8,7 +8,7 @@ if [ ! -z "$(ls -A $dir)" ]; then
 		exit 1
 	fi		
 	echo "Running Yandex Disk sync..."
-	yandex-disk sync --dir=$dir --exclude-dirs=Lubov,Lubov_2021-07-16
+	yandex-disk sync --dir=$dir --exclude-dirs=Lubov,Lubov_2021-07-16,no_sync
 	echo "Yandex Disk synced." 
 else 
 	echo "Target dir '$dir' is not mounted (empty)"
