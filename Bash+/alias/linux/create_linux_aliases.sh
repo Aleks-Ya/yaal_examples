1f52c37014d04845705fb992efb14cd9990fedbd
Aleksei Iablokov <aleksei.iablokov@sap.com>
2022-05-06 09:52:22 +0300

    Java: add SoftReferenceTest

--- Bash+/alias/linux/create_linux_aliases.sh
+++ Bash+/alias/linux/create_linux_aliases.sh
@@ -1,3 +1,32 @@
 # run this file from .~/bashrc
 
-alias mvn-tree='mvn dependency:tree -DoutputFile=tree.txt'
+alias yd-sync='~/pr/home/yaal_examples/Bash+/apps/yd-sync.sh'
+alias yd-status='yandex-disk status --dir=~/yandex-disk/'
+
+alias mtree='mvn dependency:tree -DoutputFile=target/tree.txt'
+alias fetch_all='python3 /home/aleks/pr/home/yaal_examples/Python+/Python3/src/apps/fetch_git_repos/fetch_git_repos.py /home/aleks/pr'
+alias upgrade='sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y'
+
+alias backup_cryptomator='duplicity incremental --encrypt-key alex.yablokov@protonmail.com /home/aleks/Vault file:///home/aleks/yandex-disk/backup/CryptomatorVaultBackup'
+
+alias vtb-report='
+(VERSION=2.13; eval /home/aleks/.sdkman/candidates/java/16-open/bin/java -jar \
+    -Dreport.source.dir=/home/aleks/Vault/Finances/VTB_Broker/VTB_Broker_Report/Origin \
+    -Dexporter.dealxls.output.dir=/home/aleks/Vault/Finances/VTB_Broker/VTB_Broker_Report/Generated \
+    /home/aleks/.m2/repository/ru/yaal/vtb/vtb-broker-report/${VERSION}/vtb-broker-report-${VERSION}.jar)'
+
+alias gpc='globalprotect connect -u aleksei_iablokov@epam.com --portal vpn.epam.com'
+alias gpd='globalprotect disconnect'
+
+alias madata='mkdir -p /mnt/adata && sudo mount $(lsblk -o label,path --noheadings | grep ADATA | sed 's/ADATA\s*//') /mnt/adata/'
+alias uadata='sudo umount /mnt/adata/'
+
+alias load_cam_video='python3 /home/aleks/pr/home/yaal_examples/Python+/Python3/src/apps/mts2mp4/mts2mp4.py /media/aleks/JVCCAM_SD/PRIVATE/AVCHD/BDMV/STREAM /media/aleks/ADATA/tmp/Wakeboarding'
+
+export PATH=$PATH:/media/aleks/ADATA/installed/crc_open_shift
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="/home/aleks/.sdkman"
+[[ -s "/home/aleks/.sdkman/bin/sdkman-init.sh" ]] && source "/home/aleks/.sdkman/bin/sdkman-init.sh"
+export SDKMAN_OFFLINE_MODE=false
+alias gps='globalprotect show --status'
---
+++ Java+/JSE+/Core/test/lang/ref/SoftReferenceTest.java
@@ -0,0 +1,23 @@
+package lang.ref;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SoftReferenceTest {
+    @Test
+    void createAndClear() {
+        var referent = "abc";
+
+        var reference = new SoftReference<>(referent);
+        assertThat(reference.get()).isSameAs(referent);
+        assertThat(reference.refersTo(referent)).isTrue();
+
+        reference.clear();
+        assertThat(reference.get()).isNull();
+        assertThat(reference.refersTo(referent)).isFalse();
+    }
+}
