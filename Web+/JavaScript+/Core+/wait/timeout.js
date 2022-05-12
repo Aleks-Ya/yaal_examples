9a77bb3861db165a9462b291f4d2d7d64bcdf450
Aleksei Iablokov <aleksei.iablokov@sap.com>
2022-05-12 09:55:08 +0300

    JavaScript: move files

---
+++ Web+/JavaScript+/Core+/wait/timeout.js
@@ -0,0 +1,9 @@
+console.log("Executed now");
+
+
+setTimeout(function(){
+    console.log("Executed after 1 second");
+}, 1000);
+
+
+console.log("Executed before the delay, but after the 1st console.log");
