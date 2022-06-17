8383d35959c7f95e706be126889cbe0ebb4f8fdf
Aleksei Iablokov <aleksei.iablokov@sap.com>
2022-06-17 14:33:20 +0300

    Java: Spring: add KafkaListenerErrorHandlerTest

---
+++ Java+/Spring+/SpringKafka/src/kafka/consumer/error/kafka_listener_error_handler/ConsumerProperties.java
@@ -0,0 +1,6 @@
+package kafka.consumer.error.kafka_listener_error_handler;
+
+import java.util.Map;
+
+record ConsumerProperties(Map<String, Object> consumerProperties) {
+}
---
+++ Java+/Spring+/SpringKafka/src/kafka/consumer/error/kafka_listener_error_handler/Person.java
@@ -0,0 +1,4 @@
+package kafka.consumer.error.kafka_listener_error_handler;
+
+record Person(Long id, String name) {
+}
