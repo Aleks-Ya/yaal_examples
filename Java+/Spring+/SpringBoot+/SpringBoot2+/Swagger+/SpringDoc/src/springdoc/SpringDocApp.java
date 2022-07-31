ca63002436d8331a6b5455bb5b9b7d6cb4fb239e
Aleksey Yablokov <ya_al@bk.ru>
2022-07-31 22:23:15 +0300

    Java: Spring: add Swagger

---
+++ Java+/Spring+/SpringBoot+/SpringBoot2+/Swagger+/SpringDoc/src/springdoc/SpringDocApp.java
@@ -0,0 +1,14 @@
+package springdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Swagger UI: http://localhost:8080/swagger-ui/index.html
+ */
+@SpringBootApplication
+public class SpringDocApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDocApp.class, args);
+    }
+}
---
+++ Java+/Spring+/SpringBoot+/SpringBoot2+/Swagger+/SpringFox/src/springfox/SpringFoxApp.java
@@ -0,0 +1,11 @@
+package springfox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFoxApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFoxApp.class, args);
+    }
+}
