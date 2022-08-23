8a7e1adeb1aed19fec89d044b8819e6562b41965
Aleksey Yablokov <ya_al@bk.ru>
2022-08-23 19:57:09 +0300

    Java: add MicroStream

---
+++ Java+/Libs+/MicroStream/src/microstream/DataRoot.java
@@ -0,0 +1,27 @@
+package microstream;
+
+public class DataRoot
+{
+    private String content;
+
+    public DataRoot()
+    {
+        super();
+    }
+
+    public String getContent()
+    {
+        return this.content;
+    }
+
+    public void setContent(final String content)
+    {
+        this.content = content;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Root: " + this.content;
+    }
+}
---
+++ Java+/Libs+/MicroStream/test/microstream/GettingStartedTest.java
@@ -0,0 +1,47 @@
+package microstream;
+
+import one.microstream.storage.types.EmbeddedStorage;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Paths;
+import java.util.Date;
+
+/**
+ * Source: https://manual.docs.microstream.one/data-store/getting-started
+ */
+class GettingStartedTest {
+
+    @Test
+    void test1() {
+        // Initialize a storage manager ("the database") with purely defaults.
+        var storageManager = EmbeddedStorage.start();
+
+        // print the last loaded root instance,
+        // replace it with a current version and store it
+        System.out.println(storageManager.root());
+        storageManager.setRoot("Hello World! @ " + new Date());
+        storageManager.storeRoot();
+
+        // shutdown storage
+        storageManager.shutdown();
+    }
+
+    @Test
+    void test2() {
+        // Application-specific root instance
+        var root = new DataRoot();
+
+        // Initialize a storage manager ("the database") with the given directory.
+        var storageManager = EmbeddedStorage.start(
+                root,             // root object
+                Paths.get("data") // storage directory
+        );
+        // Set content data to the root element, including the time to visualize
+        // changes on the next execution.
+        root.setContent("Hello World! @ " + new Date());
+
+        // Store the modified root and its content.
+        storageManager.storeRoot();
+        storageManager.shutdown();
+    }
+}
