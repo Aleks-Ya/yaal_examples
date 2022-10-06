c00061c472ec880de094bdb1675e1bf392315189
Aleksey Yablokov <ya_al@bk.ru>
2022-10-06 17:25:02 +0800

    Java: Spring: work with properties

--- Java+/Spring+/Spring5+/Context+/Core/test/property/source/custom/CustomPropertyTest.java
+++ Java+/Spring+/Spring5+/Context+/Core/test/property/source/custom/CustomPropertyTest.java
@@ -1,7 +1,11 @@
 package property.source.custom;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Map;
 
@@ -10,6 +14,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Use custom PropertySource.
  */
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = CustomPropertyTest.Config.class)
 class CustomPropertyTest {
     @Test
     void custom() {
@@ -23,5 +29,9 @@ class CustomPropertyTest {
         sources.addFirst(customPropertySource);
 
         assertThat(ctx.getEnvironment().getProperty(propertyName)).isEqualTo(propertyValue);
+    }
+
+    @Configuration
+    static class Config {
     }
 }
