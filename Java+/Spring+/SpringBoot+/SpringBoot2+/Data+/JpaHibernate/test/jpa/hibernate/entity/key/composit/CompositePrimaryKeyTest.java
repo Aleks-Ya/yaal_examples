83c01db8f6e2f122c50d90540994162b0efd878d
Aleksey Yablokov <ya_al@bk.ru>
2023-01-11 12:14:11 +0800

    Java: Spring: add CompositePrimaryKeyTest

---
+++ Java+/Spring+/SpringBoot+/SpringBoot2+/Data+/JpaHibernate/test/jpa/hibernate/entity/key/composit/CompositePrimaryKeyTest.java
@@ -0,0 +1,25 @@
+package jpa.hibernate.entity.key.composit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@EnableAutoConfiguration
+@ContextConfiguration(classes = {Person.class, PersonRepository.class})
+class CompositePrimaryKeyTest {
+    @Autowired
+    private PersonRepository repo;
+
+    @Test
+    void findByName() {
+        var name = "John";
+        repo.save(new Person(name));
+        var actPerson = repo.findByName(name);
+        assertThat(actPerson).isEqualTo(new Person(1, name));
+    }
+}
