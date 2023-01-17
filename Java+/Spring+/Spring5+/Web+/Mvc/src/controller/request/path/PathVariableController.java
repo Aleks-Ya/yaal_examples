ab20c0ccb19a2675efe722ecad64cb37cfc5cf3f
Aleksey Yablokov <ya_al@bk.ru>
2023-01-17 13:32:06 +0800

    Java: Spring: improve PathVariableControllerTest

--- Java+/Spring+/Spring5+/Web+/Mvc/src/controller/request/path/PathVariableController.java
+++ Java+/Spring+/Spring5+/Web+/Mvc/src/controller/request/path/PathVariableController.java
@@ -5,14 +5,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
 class PathVariableController {
-    static final String ENDPOINT = "/case/upper/";
+    static final String ENDPOINT_SINGLE = "/case/upper/single/";
+    static final String ENDPOINT_SINGLE_SPLIT = "/case/upper/singleSplit/";
+    static final String ENDPOINT_ARRAY = "/case/upper/array/";
+    static final String ENDPOINT_LIST = "/case/upper/list/";
 
     @ResponseBody
-    @GetMapping(value = ENDPOINT + "{str}")
-    public String upperCase(@PathVariable String str) {
+    @GetMapping(value = ENDPOINT_SINGLE + "{str}")
+    public String upperCaseSingle(@PathVariable String str) {
         return str.toUpperCase();
+    }
+
+    @ResponseBody
+    @GetMapping(value = ENDPOINT_SINGLE_SPLIT + "{str}")
+    public String upperCaseSingleSplit(@PathVariable String str) {
+        var elements = str.split(",");
+        return String.join("+", elements);
+    }
+
+    @ResponseBody
+    @GetMapping(value = ENDPOINT_ARRAY + "{strArray}")
+    public String upperCaseArray(@PathVariable String[] strArray) {
+        return Arrays.stream(strArray).map(String::toUpperCase).collect(Collectors.joining("_"));
+    }
+
+    @ResponseBody
+    @GetMapping(value = ENDPOINT_LIST + "{strList}")
+    public String upperCaseList(@PathVariable List<String> strList) {
+        return strList.stream().map(String::toUpperCase).collect(Collectors.joining("-"));
     }
 
 }
--- Java+/Spring+/Spring5+/Web+/Mvc/test/controller/request/path/PathVariableControllerTest.java
+++ Java+/Spring+/Spring5+/Web+/Mvc/test/controller/request/path/PathVariableControllerTest.java
@@ -11,9 +11,30 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ContextConfiguration(classes = PathVariableController.class)
 class PathVariableControllerTest extends BaseTest {
     @Test
-    void pathVariable() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get(PathVariableController.ENDPOINT + "abc"))
+    void single() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get(PathVariableController.ENDPOINT_SINGLE + "abc"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("ABC"));
+    }
+
+    @Test
+    void singleSplit() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get(PathVariableController.ENDPOINT_SINGLE_SPLIT + "abc,efg,xyz"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("abc+efg+xyz"));
+    }
+
+    @Test
+    void array_NotWork() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get(PathVariableController.ENDPOINT_ARRAY + "abc,efg,xyz"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("ABC,EFG,XYZ"));//Should be "ABC_EFG_XYZ"
+    }
+
+    @Test
+    void list_NotWork() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get(PathVariableController.ENDPOINT_LIST + "abc,efg,xyz"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("ABC,EFG,XYZ"));//Should be "ABC-EFG-XYZ"
     }
 }
