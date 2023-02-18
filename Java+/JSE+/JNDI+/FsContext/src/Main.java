943c75128b352eef43a56a4ed84f99858646b93f
Aleksey Yablokov <ya_al@bk.ru>
2023-02-18 10:33:53 +0300

    Java: include JNDI+ project

--- Java+/JSE+/JNDI+/FsContext/src/Main.java
+++ Java+/JSE+/JNDI+/FsContext/src/Main.java
@@ -1,23 +1,21 @@
 import com.sun.jndi.fscontext.RefFSContext;
 
-import javax.naming.Binding;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NameClassPair;
-import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
 import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) throws NamingException {
-        Properties props = new Properties();
+        var props = new Properties();
         props.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.fscontext.RefFSContextFactory");
 
         Context initialContext = new InitialContext(props);
 
         {
             //Ищем в папке, заданной в свойстве Context.PROVIDER_URL
-            NamingEnumeration<Binding> list = initialContext.listBindings("");
+            var list = initialContext.listBindings("");
             while (list.hasMoreElements()) {
                 NameClassPair classPair = list.next();
                 System.out.printf("%20s - %s\n", classPair.getName(), classPair.getClassName());
@@ -25,10 +23,10 @@ public class Main {
         }
         {
             //Ищем в папке, заданной во время поиска
-            RefFSContext tmpFolder = (RefFSContext) initialContext.lookup(System.getProperty("java.io.tmpdir"));
-            NamingEnumeration list = tmpFolder.listBindings("");
+            var tmpFolder = (RefFSContext) initialContext.lookup(System.getProperty("java.io.tmpdir"));
+            var list = tmpFolder.listBindings("");
             while (list.hasMoreElements()) {
-                NameClassPair classPair = (NameClassPair) list.next();
+                var classPair = (NameClassPair) list.next();
                 System.out.printf("%80s - %s\n", classPair.getName(), classPair.getClassName());
             }
         }
