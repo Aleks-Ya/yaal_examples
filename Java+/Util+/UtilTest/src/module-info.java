200e703ae3366f4456645ccba1a639b3ef94876a
Aleksey Yablokov <ya_al@bk.ru>
2023-09-02 13:26:06 +0800

    Java: split Util module into UtilSrc and UtilTest

---
+++ Java+/Util+/UtilTest/src/module-info.java
@@ -0,0 +1,6 @@
+module Java.Util.test {
+    requires org.assertj.core;
+    requires org.junit.jupiter.api;
+    requires Java.Util.main;
+    exports utilt;
+}
--- Java+/Util+/UtilTest/src/utilt/NetAsserts.java
+++ Java+/Util+/UtilTest/src/utilt/NetAsserts.java
@@ -2,8 +2,7 @@ package utilt;
 
 import util.NetUtil;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class NetAsserts {
     private NetAsserts() {
@@ -11,6 +10,6 @@ public class NetAsserts {
 
     public static void assertUrlContent(String urlStr, String expectedContent) {
         var content = NetUtil.urlContentToString(urlStr);
-        assertThat(content, containsString(expectedContent));
+        assertThat(content).contains(expectedContent);
     }
 }
