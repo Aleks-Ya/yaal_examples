d18eb97b88c959d7e67747508cace70fbe8be50a
Aleksey Yablokov <ya_al@bk.ru>
2023-11-11 23:10:57 +0800

    Lua: improve mpv_replay.lua

--- Lua/mpv_replay.lua
+++ Lua/mpv_replay.lua
@@ -1,10 +1,15 @@
--- By Ctrl-Shift-Left replays previous 3 seconds with 50% speed
+-- By Ctrl-1, Ctrl-2, etc replays previous N seconds with 50% speed
+-- By Ctrl-ESC restore normal speed immediately
 -- Install: ln -s ~/pr/home/yaal_examples/Lua/mpv_replay.lua ~/.config/mpv/scripts/mpv_replay.lua
-function custom_replay()
-    local jump_seconds = 3
+
+function set_normal_speed()
+    local normal_speed = 1.0
+    mp.set_property_number("speed", normal_speed)
+end
+
+function replay(jump_seconds)
     local overlap_seconds = jump_seconds * 5
     local slow_speed = 0.5
-    local normal_speed = 1.0
 
     -- Jump backward
     mp.commandv("seek", -jump_seconds, "relative", "keyframes")
@@ -13,11 +18,14 @@ function custom_replay()
     mp.set_property_number("speed", slow_speed)
 
     -- Wait for the specified duration
-    mp.add_timeout(jump_seconds + overlap_seconds, function()
-        -- Set normal speed
-        mp.set_property_number("speed", normal_speed)
-    end)
+    mp.add_timeout(jump_seconds + overlap_seconds, set_normal_speed)
 end
 
--- Bind the function to a key combination, e.g., Ctrl+Shift+Left
-mp.add_key_binding("Ctrl+Shift+LEFT", "custom_replay", custom_replay)
+for i = 1, 5 do
+    f = function()
+        replay(i)
+    end
+    mp.add_key_binding("Ctrl+" .. i, f)
+end
+
+mp.add_key_binding("Ctrl+ESC", set_normal_speed)
