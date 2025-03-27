7873f188840b87cbacfaa9eb9ffc3ef9506e99c9
Aleksey Yablokov <ya_al@bk.ru>
2025-03-27 15:38:36 +0700

    Scala: Databricks: improve DbUtils

--- Scala+/ScalaSbt/Libs+/Databricks+/DbUtils/src/test/scala/dbutils/FileSystemIT.scala
+++ Scala+/ScalaSbt/Libs+/Databricks+/DbUtils/src/test/scala/dbutils/FileSystemIT.scala
@@ -4,6 +4,7 @@ import com.databricks.dbutils_v1.DBUtilsHolder
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.scalatest.flatspec.AnyFlatSpec
 
+import java.io.FileNotFoundException
 import java.nio.file.Files
 import java.util.UUID
 
@@ -38,6 +39,30 @@ class FileSystemIT extends AnyFlatSpec {
     dbfs.createNewFile(file)
     val exists = dbfs.exists(file)
     assert(exists)
+  }
+
+  it should "get status of a DBFS file (exists)" in {
+    val file = absentDbfsFile
+    val dos = dbfs.create(file)
+    dos.writeBytes("abc")
+    dos.close()
+    val fileStatus = dbfs.getFileStatus(file)
+    println(fileStatus)
+    val fileSize = fileStatus.getLen
+    val isFile = fileStatus.isFile
+    val isDirectory = fileStatus.isDirectory
+    assert(fileSize == 3)
+    assert(isFile)
+    assert(!isDirectory)
+  }
+
+  it should "get status of a DBFS file (absent)" in {
+    try {
+      dbfs.getFileStatus(absentDbfsFile)
+      throw new AssertionError("Expected FileNotFoundException")
+    } catch {
+      case e: FileNotFoundException => println(e)
+    }
   }
 
   it should "copy a local file to DBFS" in {
