e11f4b3fcb5e0748128555a37461a3696d42dcbc
Aleksey Yablokov <alex_ya@mailbox.org>
2025-05-12 10:51:21 +0700

    Java: GptUI: upgrade to gemini-2.5-pro

--- Java+/App+/GptUi/src/gptui/model/question/gcp/GcpApiImpl.java
+++ Java+/App+/GptUi/src/gptui/model/question/gcp/GcpApiImpl.java
@@ -22,7 +22,8 @@ import static java.math.RoundingMode.HALF_UP;
 class GcpApiImpl implements GcpApi {
     private static final Logger log = LoggerFactory.getLogger(GcpApiImpl.class);
     private static final Gson gson = new Gson();
-    private static final URI endpoint = URI.create("https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent");
+    private static final URI endpoint = URI.create(
+            "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-pro-preview-05-06:generateContent");
     private final String apiKey;
 
     @Inject
