30021b0f51ff51d97b13b19d4a484c7d776c2675
Aleksey Yablokov <alex_ya@mailbox.org>
2025-06-15 14:07:24 +0700

    Python: improve manipulate_test.py

--- Python+/Python3/tests/module/thirdparty/pandas/conftest.py
+++ Python+/Python3/tests/module/thirdparty/pandas/conftest.py
@@ -5,3 +5,21 @@ from pandas import DataFrame
 @pytest.fixture
 def people_df() -> DataFrame:
     return DataFrame({'Name': ['John', 'Mary'], 'Age': [30, 25]})
+
+
+@pytest.fixture
+def people_extended_df() -> DataFrame:
+    return DataFrame({'Name': ['John', 'Mary'], 'Age': [30, 25], 'Male': [True, False]})
+
+
+@pytest.fixture
+def people_nested_df() -> DataFrame:
+    return DataFrame({
+        'Name': [
+            'John',
+            'Mary'
+        ],
+        'Location': [
+            {'Country': 'UK', 'City': 'London'},
+            {'Country': 'France', 'City': 'Paris'}
+        ]})
--- Python+/Python3/tests/module/thirdparty/pandas/manipulate_test.py
+++ Python+/Python3/tests/module/thirdparty/pandas/manipulate_test.py
@@ -7,9 +7,20 @@ def test_get_cell_value(people_df: DataFrame):
     assert people_df['Age'][0] == 30
 
 
-def test_select_column(people_df: DataFrame):
+def test_select_one_column(people_df: DataFrame):
     name_df: DataFrame = people_df[['Name']]
     assert name_df.to_string() == """   Name\n0  John\n1  Mary"""
+
+
+def test_select_two_columns(people_extended_df: DataFrame):
+    name_df: DataFrame = people_extended_df[['Name', 'Male']]
+    assert name_df.to_string() == """   Name   Male\n0  John   True\n1  Mary  False"""
+
+
+def test_select_nested_column(people_nested_df: DataFrame):
+    people_nested_df['City'] = people_nested_df['Location'].apply(lambda loc: loc['City'])
+    name_df: DataFrame = people_nested_df[['Name', 'City']]
+    assert name_df.to_string() == """   Name    City\n0  John  London\n1  Mary   Paris"""
 
 
 def test_explode():
