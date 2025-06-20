2103eefc955f768e6b0cb9abe210a4ba798ae1a3
Aleksey Yablokov <alex_ya@mailbox.org>
2025-06-20 09:48:36 +0700

    Spark: improve tests

--- Scala+/ScalaSbt/Spark+/Spark3+/Spark3Sql/src/test/scala/dataframe/function/builtin/TrimFunction.scala
+++ Scala+/ScalaSbt/Spark+/Spark3+/Spark3Sql/src/test/scala/dataframe/function/builtin/TrimFunction.scala
@@ -8,12 +8,14 @@ import org.scalatest.matchers.should.Matchers
 
 class TrimFunction extends AnyFlatSpec with Matchers {
   it should "use trim function" in {
-    val df = Factory.createDf("country STRING", Row("  England "), Row("Germany"))
+    val df = Factory.createDf("country STRING", Row("  England "), Row("Germany"), Row(null))
     val updatedDf = df.select(
       col("country"),
       trim(col("country")) as "trimmed")
     updatedDf.toJSON.collect should contain inOrderOnly(
       """{"country":"  England ","trimmed":"England"}""",
-      """{"country":"Germany","trimmed":"Germany"}""")
+      """{"country":"Germany","trimmed":"Germany"}""",
+      """{"country":null,"trimmed":null}"""
+    )
   }
 }
