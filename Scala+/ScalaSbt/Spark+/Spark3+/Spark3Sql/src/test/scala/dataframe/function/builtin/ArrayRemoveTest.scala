c51fd7721fe3cdf0228f6383de9cbbfebb3a5be9
Aleksey Yablokov <alex_ya@mailbox.org>
2025-08-20 05:19:56 +0700

    Spark: improve ArrayRemoveTest

--- Scala+/ScalaSbt/Spark+/Spark3+/Spark3Sql/src/test/scala/dataframe/function/builtin/ArrayRemoveTest.scala
+++ Scala+/ScalaSbt/Spark+/Spark3+/Spark3Sql/src/test/scala/dataframe/function/builtin/ArrayRemoveTest.scala
@@ -7,7 +7,8 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class ArrayRemoveTest extends AnyFlatSpec with Matchers {
-  it should "remove an array element" in {
+
+  it should "remove a literal from an array" in {
     val df = Factory.createDf("numbers ARRAY<INT>",
       Row(Seq(10, 15, 20)),
       Row(Seq(11, 16, 15)))
@@ -15,6 +16,16 @@ class ArrayRemoveTest extends AnyFlatSpec with Matchers {
     updatedDf.toJSON.collect should contain inOrderOnly(
       """{"numbers":[10,15,20],"clean":[10,20]}""",
       """{"numbers":[11,16,15],"clean":[11,16]}""")
+  }
+
+  it should "remove an another column element from an array" in {
+    val df = Factory.createDf("numbers ARRAY<INT>, bad_number INT",
+      Row(Seq(10, 15, 13), 13),
+      Row(Seq(11, 666, 15), 666))
+    val updatedDf = df.withColumn("clean", array_remove(col("numbers"), col("bad_number")))
+    updatedDf.toJSON.collect should contain inOrderOnly(
+      """{"numbers":[10,15,13],"bad_number":13,"clean":[10,15]}""",
+      """{"numbers":[11,666,15],"bad_number":666,"clean":[11,15]}""")
   }
 
   //Use `array_compact` instead
